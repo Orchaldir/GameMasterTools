f4d8918e13ad9559066d11ed9645d45970df2be8
Orchaldir <orchaldir@gmail.com>
2024-05-09 16:57:08 +0200

    Add logging

--- src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
@@ -3,10 +3,14 @@ package at.orchaldir.gm.app.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 fun Application.configureRouting() {
     routing {
         get("/") {
+            logger.info { "Root" }
             call.respondText("Hello World!")
         }
     }
