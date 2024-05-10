91e0cb7a4a033b493fd5634e4a7410878bc7ed0b
Orchaldir <orchaldir@gmail.com>
2024-05-10 08:57:12 +0200

    Add Storage

--- src/main/kotlin/at/orchaldir/gm/utils/Storage.kt
+++ src/main/kotlin/at/orchaldir/gm/utils/Storage.kt
@@ -4,10 +4,23 @@ interface Storage<ID, ELEMENT> {
 
     fun create(f: (ID) -> ELEMENT): ID
 
-    fun getAll(): List<ELEMENT>
+    fun getAll(): Collection<ELEMENT>
 
-    fun get(id: ID): ELEMENT
+    fun get(id: ID): ELEMENT?
 
-    fun  delete(id: ID): Boolean
+    fun  delete(id: ID): ELEMENT?
+
+}
+
+data class MapStorage<ID, ELEMENT>(private val elements: MutableMap<ID,ELEMENT> = mutableMapOf()): Storage<ID,ELEMENT> {
+    override fun create(f: (ID) -> ELEMENT): ID {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAll() = elements.values
+
+    override fun get(id: ID) = elements.get(id)
+
+    override fun delete(id: ID) = elements.remove(id)
 
 }
