1cfe53d7f8415149c90384b8bd9b75a03fa7a4c2
Orchaldir <orchaldir@gmail.com>
2024-05-10 08:17:16 +0200

    Add css

--- src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
@@ -3,9 +3,11 @@ package at.orchaldir.gm.app.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
 import mu.KotlinLogging
+import java.io.File
 
 private val logger = KotlinLogging.logger {}
 
@@ -13,12 +15,14 @@ private const val TITLE = "Orchaldir's Game Master Tools"
 
 fun Application.configureRouting() {
     routing {
+        staticFiles("/static", File("static"))
         get("/") {
             logger.info { "Root" }
 
             call.respondHtml(HttpStatusCode.OK) {
                 head {
                     title { +TITLE }
+                    link(rel = "stylesheet", href = "/static/style.css", type = "text/css")
                 }
                 body {
                     h1 { +TITLE }
