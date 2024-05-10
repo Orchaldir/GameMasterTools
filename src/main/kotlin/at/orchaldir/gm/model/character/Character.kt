09340716939de73f91a787d0321593de03528ea3
Orchaldir <orchaldir@gmail.com>
2024-05-10 08:33:13 +0200

    Add id

--- src/main/kotlin/at/orchaldir/gm/model/character/Character.kt
+++ src/main/kotlin/at/orchaldir/gm/model/character/Character.kt
@@ -1,6 +1,10 @@
 package at.orchaldir.gm.model.character
 
+@JvmInline
+value class CharacterId(private val id: Int)
+
 data class Character(
+    val id: CharacterId,
     val name: String,
     val gender: Gender,
 )
