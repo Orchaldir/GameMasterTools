5bdbe8f46872cb0d7c39e7685e0c726f861abab5
Orchaldir <orchaldir@gmail.com>
2024-05-19 23:02:01 +0200

    [#13] CI (#24)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# GameMasterTools
+# Orchaldir's Game Master Tools
+
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=Orchaldir_GameMasterTools&metric=coverage)](https://sonarcloud.io/summary/new_code?id=Orchaldir_GameMasterTools)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "1.9.23"
+    id("org.sonarqube") version "4.0.0.2929"
+    id("jacoco")
 }
 
 group = "at.orchaldir.gm"
@@ -8,6 +10,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 val ktorVersion = "2.3.10"
@@ -24,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("io.github.microutils:kotlin-logging-jvm:$loggingVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.0.0.2929")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
     testImplementation(kotlin("test"))
@@ -31,10 +35,32 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(file("build/test-results/test/text.xml"))
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "Orchaldir_GameMasterTools")
+        property("sonar.organization", "orchaldir-github")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.language", "kotlin")
+        property("sonar.sources", "src/main/kotlin")
+        property("sonar.tests", "src/test/kotlin")
+        property("sonar.junit.reportPaths", "build/test-results/test/text.xml")
+        property("sonar.jacoco.reportPaths", "build/reports/jacoco/test")
+    }
 }
 
 tasks.wrapper {
-    gradleVersion = "8.5"
+    gradleVersion = "8.0.2"
 }
 
 kotlin {
