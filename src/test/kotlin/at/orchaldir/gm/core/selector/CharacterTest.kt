4e8dcf0e8a8e0876da3a15ff673925ec4233c93c
Orchaldir <orchaldir@gmail.com>
2024-05-20 12:09:35 +0200

    test

--- src/test/kotlin/at/orchaldir/gm/core/selector/CharacterTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/CharacterTest.kt
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-private val CHILD_ID0 = CharacterId(0)
+private val DAUGHTER_ID = CharacterId(0)
 private val MOTHER_ID = CharacterId(1)
 private val FATHER_ID = CharacterId(2)
-private val CHILD_ID1 = CharacterId(3)
+private val SON_ID = CharacterId(3)
 private val RACE0 = RaceId(0)
 
-private val CHILD0 = Character(CHILD_ID0, origin = Born(MOTHER_ID, FATHER_ID))
+private val DAUGHTER = Character(DAUGHTER_ID, gender = Gender.Female, origin = Born(MOTHER_ID, FATHER_ID))
 private val MOTHER = Character(MOTHER_ID, gender = Gender.Female)
 private val FATHER = Character(FATHER_ID, gender = Gender.Male)
-private val CHILD1 = Character(CHILD_ID1, origin = Born(MOTHER_ID, FATHER_ID))
+private val SON = Character(SON_ID, gender = Gender.Male, origin = Born(MOTHER_ID, FATHER_ID))
 
 class CharacterTest {
     @Nested
@@ -25,19 +25,24 @@ class CharacterTest {
         private val state = State(
             characters = Storage(
                 listOf(
-                    CHILD0,
+                    DAUGHTER,
                     MOTHER,
                     FATHER,
-                    CHILD1,
+                    SON,
                 )
             ),
             races = Storage(listOf(Race(RACE0)))
         )
 
         @Test
+        fun `Get the parents of an unknown character`() {
+            assertTrue(state.getParents(CharacterId(100)).isEmpty())
+        }
+
+        @Test
         fun `Get the parents`() {
-            assertEquals(setOf(MOTHER, FATHER), state.getParents(CHILD_ID0).toSet())
-            assertEquals(setOf(MOTHER, FATHER), state.getParents(CHILD_ID1).toSet())
+            assertEquals(setOf(MOTHER, FATHER), state.getParents(DAUGHTER_ID).toSet())
+            assertEquals(setOf(MOTHER, FATHER), state.getParents(SON_ID).toSet())
         }
 
         @Test
@@ -47,18 +52,18 @@ class CharacterTest {
 
         @Test
         fun `Get the children of the mother`() {
-            assertEquals(setOf(CHILD0, CHILD1), state.getChildren(MOTHER_ID).toSet())
+            assertEquals(setOf(DAUGHTER, SON), state.getChildren(MOTHER_ID).toSet())
         }
 
         @Test
         fun `Get the children of the father`() {
-            assertEquals(setOf(CHILD0, CHILD1), state.getChildren(FATHER_ID).toSet())
+            assertEquals(setOf(DAUGHTER, SON), state.getChildren(FATHER_ID).toSet())
         }
 
         @Test
         fun `Get the siblings`() {
-            assertEquals(setOf(CHILD1), state.getSiblings(CHILD_ID0).toSet())
-            assertEquals(setOf(CHILD0), state.getSiblings(CHILD_ID1).toSet())
+            assertEquals(setOf(SON), state.getSiblings(DAUGHTER_ID).toSet())
+            assertEquals(setOf(DAUGHTER), state.getSiblings(SON_ID).toSet())
         }
 
         @Test
