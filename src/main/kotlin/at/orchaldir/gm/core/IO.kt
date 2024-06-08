7074f6f5ba3b5d4abc98ab56c41fb1cecb670f23
Orchaldir <orchaldir@gmail.com>
2024-06-08 07:09:45 +0200

    Save to file

--- src/main/kotlin/at/orchaldir/gm/core/IO.kt
+++ src/main/kotlin/at/orchaldir/gm/core/IO.kt
@@ -7,16 +7,17 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import mu.KotlinLogging
+import java.io.File
 
 val logger = KotlinLogging.logger {}
-
 @OptIn(ExperimentalSerializationApi::class)
+val prettyJson = Json {
+    prettyPrint = true
+    prettyPrintIndent = " "
+}
+
 inline fun <reified ID : Id<ID>, reified ELEMENT : Element<ID>> save(storage: Storage<ID, ELEMENT>) {
-    logger.info { "save(): " }
-    val prettyJson = Json { // this returns the JsonBuilder
-        prettyPrint = true
-        // optional: specify indent
-        prettyPrintIndent = " "
-    }
-    logger.info { prettyJson.encodeToString(storage) }
+    logger.info { "save(): ${storage.name}s" }
+
+    File("data/${storage.name}s.json").writeText(prettyJson.encodeToString(storage))
 }
