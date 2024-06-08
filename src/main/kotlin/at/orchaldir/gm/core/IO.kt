89c376d292220c8aa4c84707b9503036a9c8d93c
Orchaldir <orchaldir@gmail.com>
2024-06-08 07:15:52 +0200

    Save to file

--- src/main/kotlin/at/orchaldir/gm/core/IO.kt
+++ src/main/kotlin/at/orchaldir/gm/core/IO.kt
@@ -4,6 +4,7 @@ import at.orchaldir.gm.utils.Element
 import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.Storage
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import mu.KotlinLogging
@@ -15,9 +16,15 @@ val prettyJson = Json {
     prettyPrint = true
     prettyPrintIndent = " "
 }
+const val VERSION = 1
+
+@Serializable
+data class Data<I, E>(val version: Int, val elements: Map<I, E>)
 
 inline fun <reified ID : Id<ID>, reified ELEMENT : Element<ID>> save(storage: Storage<ID, ELEMENT>) {
     logger.info { "save(): ${storage.name}s" }
 
-    File("data/${storage.name}s.json").writeText(prettyJson.encodeToString(storage))
+    val data = Data(VERSION, storage.getAll().associateBy { it.id() })
+
+    File("data/${storage.name}s.json").writeText(prettyJson.encodeToString(data))
 }
