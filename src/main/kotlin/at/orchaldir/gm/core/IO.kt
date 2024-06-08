14c31f99508ea8b55d593a75249eeb0be66449b7
Orchaldir <orchaldir@gmail.com>
2024-06-08 06:54:30 +0200

    pretty

--- src/main/kotlin/at/orchaldir/gm/core/IO.kt
+++ src/main/kotlin/at/orchaldir/gm/core/IO.kt
@@ -3,12 +3,14 @@ package at.orchaldir.gm.core
 import at.orchaldir.gm.utils.Element
 import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.Storage
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import mu.KotlinLogging
 
 val logger = KotlinLogging.logger {}
 
+@OptIn(ExperimentalSerializationApi::class)
 inline fun <reified ID : Id<ID>, reified ELEMENT : Element<ID>> save(storage: Storage<ID, ELEMENT>) {
     logger.info { "save(): " }
     val prettyJson = Json { // this returns the JsonBuilder
