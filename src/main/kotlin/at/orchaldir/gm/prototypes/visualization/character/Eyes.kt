38c05a0be8f905c2eac8779ae06717d3fc753108
Orchaldir <orchaldir@gmail.com>
2024-06-08 21:21:48 +0200

    Refactor

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Eyes.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Eyes.kt
@@ -3,15 +3,7 @@ package at.orchaldir.gm.prototypes.visualization.character
 import at.orchaldir.gm.core.model.appearance.Size
 import at.orchaldir.gm.core.model.character.appearance.*
 import at.orchaldir.gm.prototypes.visualization.RENDER_CONFIG
-import at.orchaldir.gm.utils.math.AABB
 import at.orchaldir.gm.utils.math.Distance
-import at.orchaldir.gm.utils.math.Point2d
-import at.orchaldir.gm.utils.math.Size2d
-import at.orchaldir.gm.utils.renderer.svg.SvgBuilder
-import at.orchaldir.gm.visualization.RenderConfig
-import at.orchaldir.gm.visualization.character.calculateSize
-import at.orchaldir.gm.visualization.character.visualizeAppearance
-import java.io.File
 
 fun main() {
     val config = RENDER_CONFIG
@@ -21,37 +13,8 @@ fun main() {
             appearances.add(createRow(Eye(eyeShape, pupilShape)))
         }
     }
+
     renderTable("eyes.svg", config, appearances)
-}
-
-private fun renderTable(
-    filename: String,
-    config: RenderConfig,
-    appearances: List<List<Appearance>>,
-) {
-    val size = calculateSize(config, appearances[0][0])
-    val maxColumns = appearances.maxOf { it.size }
-    val totalSize = Size2d(size.width * maxColumns, size.height * appearances.size)
-    val builder = SvgBuilder.create(totalSize)
-    val columnStep = Point2d(size.width, 0.0f)
-    val rowStep = Point2d(0.0f, size.height)
-    var startOfRow = Point2d()
-
-    appearances.forEach { row ->
-        var start = startOfRow.copy()
-
-        row.forEach { appearance ->
-            val aabb = AABB(start, size)
-
-            visualizeAppearance(builder, config, aabb, appearance)
-
-            start += columnStep
-        }
-
-        startOfRow += rowStep
-    }
-
-    File(filename).writeText(builder.finish().export())
 }
 
 private fun createAppearance(eyes: Eyes) = HeadOnly(Head(eyes = eyes, skin = ExoticSkin()), Distance(0.2f))
