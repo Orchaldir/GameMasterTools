024f0ae1c20716264c1c32bef569b7792e15d8c3
Orchaldir <orchaldir@gmail.com>
2024-06-10 20:18:49 +0200

    Ad example

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Beard.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Beard.kt
@@ -4,23 +4,17 @@ import at.orchaldir.gm.core.model.appearance.Color
 import at.orchaldir.gm.core.model.appearance.Side
 import at.orchaldir.gm.core.model.appearance.Size
 import at.orchaldir.gm.core.model.character.appearance.*
-import at.orchaldir.gm.core.model.character.appearance.beard.NoBeard
+import at.orchaldir.gm.core.model.character.appearance.beard.*
 import at.orchaldir.gm.core.model.character.appearance.hair.*
 import at.orchaldir.gm.prototypes.visualization.RENDER_CONFIG
 import at.orchaldir.gm.utils.math.Distance
 
 fun main() {
     val appearances = mutableListOf<List<Appearance>>()
-    val styles: List<HairStyle> = listOf(
-        Afro,
-        BuzzCut,
-        FlatTop,
-        MiddlePart,
-        Shaved,
-        SidePart(Side.Left),
-        SidePart(Side.Right),
-        Spiked,
-    )
+    val styles = mutableListOf<BeardStyle>()
+
+    MoustacheStyle.entries.forEach { styles.add(Moustache(it)) }
+    GoateeStyle.entries.forEach { styles.add(Goatee(it)) }
 
     styles.forEach { style ->
         val row = mutableListOf<Appearance>()
@@ -33,8 +27,16 @@ fun main() {
         appearances.add(row)
     }
 
-    renderTable("hair.svg", RENDER_CONFIG, appearances)
+    renderTable("beard.svg", RENDER_CONFIG, appearances)
 }
 
-private fun createAppearance(style: HairStyle, eyes: Eyes) =
-    HeadOnly(Head(NoBeard, NormalEars(), eyes, NormalHair(style, Color.SaddleBrown), SimpleMouth()), Distance(0.2f))
+private fun createAppearance(style: BeardStyle, eyes: Eyes) =
+    HeadOnly(
+        Head(
+            NormalBeard(style, Color.SaddleBrown),
+            NormalEars(),
+            eyes,
+            NormalHair(SidePart(Side.Left), Color.SaddleBrown),
+            SimpleMouth()
+        ), Distance(0.2f)
+    )
