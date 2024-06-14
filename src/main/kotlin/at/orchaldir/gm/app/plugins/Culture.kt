1bd0446d0ddb0b68ba8aae136c866bb9e5fd204a
Orchaldir <orchaldir@gmail.com>
2024-06-14 15:37:00 +0200

    Add style options

--- src/main/kotlin/at/orchaldir/gm/app/plugins/Culture.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/Culture.kt
@@ -6,10 +6,13 @@ import at.orchaldir.gm.core.action.CreateCulture
 import at.orchaldir.gm.core.action.DeleteCulture
 import at.orchaldir.gm.core.action.UpdateCulture
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.character.appearance.beard.GoateeStyle
+import at.orchaldir.gm.core.model.character.appearance.beard.MoustacheStyle
 import at.orchaldir.gm.core.model.culture.Culture
 import at.orchaldir.gm.core.model.culture.CultureId
 import at.orchaldir.gm.core.model.culture.style.HairStyleType
 import at.orchaldir.gm.core.model.culture.style.StyleOptions
+import at.orchaldir.gm.core.model.race.appearance.BeardStyleType
 import at.orchaldir.gm.core.selector.canDelete
 import at.orchaldir.gm.core.selector.getCharacters
 import io.ktor.http.*
@@ -28,7 +31,10 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 private const val NAME = "name"
-private const val HAIR_STYLE = "hair_style"
+private const val HAIR_STYLE = "hair"
+private const val BEARD_STYLE = "beard"
+private const val GOATEE_STYLE = "goatee"
+private const val MOUSTACHE_STYLE = "moustache"
 
 @Resource("/cultures")
 class Cultures {
@@ -137,6 +143,9 @@ private fun HTML.showCultureDetails(
         field("Id", culture.id.value.toString())
         field("Name", culture.name)
         h2 { +"Style Options" }
+        showRarityMap("Beard Styles", culture.styleOptions.beardStyles)
+        showRarityMap("Goatee Styles", culture.styleOptions.goateeStyles)
+        showRarityMap("Moustache Styles", culture.styleOptions.moustacheStyle)
         showRarityMap("Hair Styles", culture.styleOptions.hairStyles)
         h2 { +"Characters" }
         showList(state.getCharacters(culture.id)) { character ->
@@ -168,6 +177,9 @@ private fun HTML.showCultureEditor(
                 }
             }
             h2 { +"Style Options" }
+            selectRarityMap("Beard Styles", BEARD_STYLE, culture.styleOptions.beardStyles)
+            selectRarityMap("Goatee Styles", GOATEE_STYLE, culture.styleOptions.goateeStyles)
+            selectRarityMap("Moustache Styles", MOUSTACHE_STYLE, culture.styleOptions.moustacheStyle)
             selectRarityMap("Hair Styles", HAIR_STYLE, culture.styleOptions.hairStyles)
             p {
                 submitInput {
@@ -186,11 +198,15 @@ fun parseCulture(
     id: CultureId,
 ): Culture {
     val name = parameters.getOrFail(NAME)
-    val hairStyle = parseRarityMap(parameters, HAIR_STYLE, HairStyleType::valueOf)
 
     return Culture(
         id,
         name,
-        StyleOptions(hairStyle),
+        StyleOptions(
+            parseRarityMap(parameters, BEARD_STYLE, BeardStyleType::valueOf),
+            parseRarityMap(parameters, GOATEE_STYLE, GoateeStyle::valueOf),
+            parseRarityMap(parameters, MOUSTACHE_STYLE, MoustacheStyle::valueOf),
+            parseRarityMap(parameters, HAIR_STYLE, HairStyleType::valueOf),
+        ),
     )
 }
--- src/main/kotlin/at/orchaldir/gm/core/model/culture/style/StyleOptions.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/culture/style/StyleOptions.kt
@@ -1,9 +1,15 @@
 package at.orchaldir.gm.core.model.culture.style
 
 import at.orchaldir.gm.core.model.appearance.RarityMap
+import at.orchaldir.gm.core.model.character.appearance.beard.GoateeStyle
+import at.orchaldir.gm.core.model.character.appearance.beard.MoustacheStyle
+import at.orchaldir.gm.core.model.race.appearance.BeardStyleType
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class StyleOptions(
+    val beardStyles: RarityMap<BeardStyleType> = RarityMap(BeardStyleType.entries),
+    val goateeStyles: RarityMap<GoateeStyle> = RarityMap(GoateeStyle.entries),
+    val moustacheStyle: RarityMap<MoustacheStyle> = RarityMap(MoustacheStyle.entries),
     val hairStyles: RarityMap<HairStyleType> = RarityMap(HairStyleType.entries),
 )
