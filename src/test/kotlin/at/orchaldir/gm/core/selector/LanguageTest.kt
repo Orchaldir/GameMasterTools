6ec02dc8c25e0873357aead40da57f4ebcd05de5
Orchaldir <orchaldir@gmail.com>
2024-06-21 21:12:43 +0200

    test

--- src/test/kotlin/at/orchaldir/gm/core/selector/LanguageTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/LanguageTest.kt
@@ -1,6 +1,8 @@
 package at.orchaldir.gm.core.selector
 
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.language.CombinedLanguage
+import at.orchaldir.gm.core.model.language.EvolvedLanguage
 import at.orchaldir.gm.core.model.language.Language
 import at.orchaldir.gm.core.model.language.LanguageId
 import at.orchaldir.gm.utils.Storage
@@ -9,7 +11,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private val ID0 = LanguageId(0)
-private val ID1 = LanguageId(0)
+private val ID1 = LanguageId(1)
 
 class LanguageTest {
 
@@ -21,6 +23,23 @@ class LanguageTest {
             val state = State(languages = Storage(listOf(Language(ID0), Language(ID1))))
 
             assertTrue(state.canDelete(ID0))
+            assertTrue(state.canDelete(ID1))
+        }
+
+        @Test
+        fun `Cannot delete parent of evolved language`() {
+            val state = State(languages = Storage(listOf(Language(ID0), Language(ID1, origin = EvolvedLanguage(ID0)))))
+
+            assertFalse(state.canDelete(ID0))
+            assertTrue(state.canDelete(ID1))
+        }
+
+        @Test
+        fun `Cannot delete parent of combined language`() {
+            val state =
+                State(languages = Storage(listOf(Language(ID0), Language(ID1, origin = CombinedLanguage(setOf(ID0))))))
+
+            assertFalse(state.canDelete(ID0))
             assertTrue(state.canDelete(ID1))
         }
     }
