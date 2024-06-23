096084e2b095c48b01647f895324dcb36fdb7687
Orchaldir <orchaldir@gmail.com>
2024-06-23 12:10:32 +0200

    test

---
+++ src/test/kotlin/at/orchaldir/gm/utils/math/FactorTest.kt
@@ -0,0 +1,20 @@
+package at.orchaldir.gm.utils.math
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FactorTest {
+    @Test
+    fun `Interpolate between 2 factors`() {
+        val start = Factor(10.0f)
+        val end = Factor(14.0f)
+
+        testInterpolate(start, end, 0.0f, 10.0f)
+        testInterpolate(start, end, 0.5f, 12.0f)
+        testInterpolate(start, end, 1.0f, 14.0f)
+    }
+
+    private fun testInterpolate(start: Factor, end: Factor, input: Float, result: Float) {
+        assertEquals(Factor(result), start.interpolate(end, input))
+    }
+}
