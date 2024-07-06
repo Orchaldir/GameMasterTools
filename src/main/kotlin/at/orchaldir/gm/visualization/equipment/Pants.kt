16cba4a6df4a70d0af604efdb782dbdfa9e0fa4d
Orchaldir <orchaldir@gmail.com>
2024-07-06 16:23:40 +0200

    Add visualizePants()

--- src/main/kotlin/at/orchaldir/gm/visualization/equipment/Pants.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/equipment/Pants.kt
@@ -1,25 +1,17 @@
 package at.orchaldir.gm.visualization.equipment
 
 import at.orchaldir.gm.core.model.character.appearance.Body
-import at.orchaldir.gm.core.model.item.Equipment
-import at.orchaldir.gm.core.model.item.NoEquipment
 import at.orchaldir.gm.core.model.item.Pants
-import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.AABB
 import at.orchaldir.gm.utils.renderer.Renderer
 import at.orchaldir.gm.visualization.RenderConfig
 
-fun visualizeBodyEquipment(
+fun visualizePants(
     renderer: Renderer,
     config: RenderConfig,
     aabb: AABB,
     body: Body,
-    equipment: List<Equipment>,
+    pants: Pants,
 ) {
-    equipment.forEach {
-        when (it) {
-            NoEquipment -> doNothing()
-            is Pants -> visualizePants(renderer, config, aabb, body, it)
-        }
-    }
+
 }
