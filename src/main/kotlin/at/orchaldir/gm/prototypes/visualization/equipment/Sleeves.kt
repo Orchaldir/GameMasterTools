8ad5528289b8a137f68eff631cc82c48c0d13359
Orchaldir <orchaldir@gmail.com>
2024-07-07 19:32:35 +0200

    Add sleeves example

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/equipment/Sleeves.kt
@@ -0,0 +1,32 @@
+package at.orchaldir.gm.prototypes.visualization.equipment
+
+import at.orchaldir.gm.core.model.appearance.Color.Blue
+import at.orchaldir.gm.core.model.appearance.Size
+import at.orchaldir.gm.core.model.character.appearance.Body
+import at.orchaldir.gm.core.model.character.appearance.BodyShape
+import at.orchaldir.gm.core.model.character.appearance.Head
+import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
+import at.orchaldir.gm.core.model.item.*
+import at.orchaldir.gm.core.model.item.NecklineStyle.None
+import at.orchaldir.gm.prototypes.visualization.RENDER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.addNames
+import at.orchaldir.gm.prototypes.visualization.character.renderTable
+import at.orchaldir.gm.utils.math.Distance
+
+fun main() {
+    renderTable(
+        "sleeves.svg",
+        RENDER_CONFIG,
+        addNames(SleeveStyle.entries),
+        addNames(BodyShape.entries)
+    ) { distance, shape, style ->
+        Pair(createAppearance(distance, shape), listOf(Shirt(None, style, Blue)))
+    }
+}
+
+private fun createAppearance(distance: Distance, shape: BodyShape) =
+    HumanoidBody(
+        Body(shape, Size.Medium),
+        Head(),
+        distance,
+    )
