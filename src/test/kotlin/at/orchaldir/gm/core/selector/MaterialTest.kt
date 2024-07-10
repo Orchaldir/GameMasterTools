78c999c5fd3f6360600950ccd0327df134a87618
Orchaldir <orchaldir@gmail.com>
2024-07-10 20:00:03 +0200

    tests

---
+++ src/test/kotlin/at/orchaldir/gm/core/selector/MaterialTest.kt
@@ -0,0 +1,42 @@
+package at.orchaldir.gm.core.selector
+
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.item.ItemTemplate
+import at.orchaldir.gm.core.model.item.ItemTemplateId
+import at.orchaldir.gm.core.model.item.Shirt
+import at.orchaldir.gm.core.model.material.Material
+import at.orchaldir.gm.core.model.material.MaterialId
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+private val ID0 = MaterialId(0)
+private val ID1 = MaterialId(1)
+private val TEMPLATE0 = ItemTemplateId(0)
+
+class MaterialTest {
+    @Nested
+    inner class CanDeleteTest {
+
+        @Test
+        fun `Can delete unused material`() {
+            val state = State(materials = Storage(listOf(Material(ID0), Material(ID1))))
+
+            assertTrue(state.canDelete(ID0))
+            assertTrue(state.canDelete(ID1))
+        }
+
+        @Test
+        fun `Cannot delete material used by item template`() {
+            val state = State(
+                itemTemplates = Storage(listOf(ItemTemplate(TEMPLATE0, equipment = Shirt(material = ID0)))),
+                materials = Storage(listOf(Material(ID0), Material(ID1)))
+            )
+
+            assertFalse(state.canDelete(ID0))
+            assertTrue(state.canDelete(ID1))
+        }
+    }
+}
