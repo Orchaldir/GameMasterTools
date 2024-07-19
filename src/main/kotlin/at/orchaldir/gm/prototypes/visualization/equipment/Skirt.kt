0c86b38c6d9f501b0ff31f7ab039fb8fd81a7be5
Orchaldir <orchaldir@gmail.com>
2024-07-19 18:56:10 +0200

    Add example

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/equipment/Skirt.kt
@@ -0,0 +1,31 @@
+package at.orchaldir.gm.prototypes.visualization.equipment
+
+import at.orchaldir.gm.core.model.appearance.Size
+import at.orchaldir.gm.core.model.character.appearance.Body
+import at.orchaldir.gm.core.model.character.appearance.BodyShape
+import at.orchaldir.gm.core.model.character.appearance.Head
+import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
+import at.orchaldir.gm.core.model.item.Skirt
+import at.orchaldir.gm.core.model.item.style.SkirtStyle
+import at.orchaldir.gm.prototypes.visualization.RENDER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.addNames
+import at.orchaldir.gm.prototypes.visualization.character.renderTable
+import at.orchaldir.gm.utils.math.Distance
+
+fun main() {
+    renderTable(
+        "skirts.svg",
+        RENDER_CONFIG,
+        addNames(SkirtStyle.entries),
+        addNames(BodyShape.entries)
+    ) { distance, shape, style ->
+        Pair(createAppearance(distance, shape), listOf(Skirt(style)))
+    }
+}
+
+private fun createAppearance(distance: Distance, shape: BodyShape) =
+    HumanoidBody(
+        Body(shape, Size.Medium),
+        Head(),
+        distance,
+    )
