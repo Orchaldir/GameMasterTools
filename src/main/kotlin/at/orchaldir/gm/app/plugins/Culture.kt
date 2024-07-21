c922e1d931a24bf9758340962ac367472f794d33
Orchaldir <orchaldir@gmail.com>
2024-07-21 15:11:43 +0200

    Add style.css

--- src/main/kotlin/at/orchaldir/gm/app/plugins/Culture.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/Culture.kt
@@ -276,17 +276,18 @@ private fun BODY.showAppearanceOptions(culture: Culture) {
 private fun BODY.showClothingOptions(culture: Culture) {
     h2 { +"Clothing Options" }
     culture.clothingStyles.getMap().forEach { (gender, style) ->
-        field("Gender", gender.toString())
-        h3 { +"$gender Clothing" }
-        showRarityMap("Clothing Sets", style.clothingSets)
-        h4 { +"Dress" }
-        showRarityMap("Colors", style.dress.colors)
-        showRarityMap("Skirt Styles", style.dress.skirtStyles)
-        showRarityMap("Neckline Styles", style.dress.necklineStyles)
-        showRarityMap("Sleeve Styles", style.dress.sleeveStyles)
-        h4 { +"Footwear" }
-        showRarityMap("Styles", style.footwear.styles)
-        //showRarityMap("Colors", style.footwear.colors)
+        details {
+            summary { +gender.toString() }
+            showRarityMap("Clothing Sets", style.clothingSets)
+            h3 { +"Dress" }
+            showRarityMap("Colors", style.dress.colors)
+            showRarityMap("Skirt Styles", style.dress.skirtStyles)
+            showRarityMap("Neckline Styles", style.dress.necklineStyles)
+            showRarityMap("Sleeve Styles", style.dress.sleeveStyles)
+            h3 { +"Footwear" }
+            showRarityMap("Styles", style.footwear.styles)
+            //showRarityMap("Colors", style.footwear.colors)
+        }
     }
 }
 
