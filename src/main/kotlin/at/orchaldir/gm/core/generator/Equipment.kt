99752143262521a36ec4deaf958fdc90ae243ce3
Orchaldir <orchaldir@gmail.com>
2024-07-26 21:08:06 +0200

    Finish EquipmentGenerator.generate()

--- src/main/kotlin/at/orchaldir/gm/core/generator/Equipment.kt
+++ src/main/kotlin/at/orchaldir/gm/core/generator/Equipment.kt
@@ -4,8 +4,10 @@ import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.appearance.RarityMap
 import at.orchaldir.gm.core.model.character.Character
 import at.orchaldir.gm.core.model.character.EquipmentMap
+import at.orchaldir.gm.core.model.fashion.ClothingSet
 import at.orchaldir.gm.core.model.fashion.Fashion
 import at.orchaldir.gm.core.model.item.EquipmentType
+import at.orchaldir.gm.core.model.item.EquipmentType.*
 import at.orchaldir.gm.core.model.item.ItemTemplateId
 import at.orchaldir.gm.utils.NumberGenerator
 import at.orchaldir.gm.utils.RandomNumberGenerator
@@ -34,11 +36,35 @@ data class EquipmentGenerator(
     fun generate(): EquipmentMap {
         val map = mutableMapOf<EquipmentType, ItemTemplateId>()
 
+        when (generate(fashion.clothingSets)) {
+            ClothingSet.Dress -> generate(map, Dress)
+            ClothingSet.PantsAndShirt -> generatePantsAndShirt(map)
+            ClothingSet.ShirtAndSkirt -> generateShirtAndSkirt(map)
+        }
+
+        generate(map, Footwear)
+        generate(map, Hat)
+
         return EquipmentMap(map)
     }
 
-    fun <T> generate(map: RarityMap<T>) = rarityGenerator.generate(map, numberGenerator)
+    private fun generatePantsAndShirt(map: MutableMap<EquipmentType, ItemTemplateId>) {
+        generate(map, Pants)
+        generate(map, Shirt)
+    }
 
-    fun <T> select(list: List<T>) = numberGenerator.select(list)
+    private fun generateShirtAndSkirt(map: MutableMap<EquipmentType, ItemTemplateId>) {
+        generate(map, Shirt)
+        generate(map, Skirt)
+    }
+
+    private fun generate(map: MutableMap<EquipmentType, ItemTemplateId>, type: EquipmentType) {
+        val options = fashion.getOptions(type)
+        map[type] = generate(options)
+    }
+
+    private fun <T> generate(map: RarityMap<T>) = rarityGenerator.generate(map, numberGenerator)
+
+    private fun <T> select(list: List<T>) = numberGenerator.select(list)
 
 }
