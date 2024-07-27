8c68abb89c99813528c133c0c2065f4d6527b230
Orchaldir <orchaldir@gmail.com>
2024-07-27 22:52:19 +0200

    Fix serialize problem

--- src/main/kotlin/at/orchaldir/gm/core/model/appearance/Fill.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/appearance/Fill.kt
@@ -1,14 +1,19 @@
 package at.orchaldir.gm.core.model.appearance
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Fill<C>
 
+@Serializable
+@SerialName("Solid")
 data class Solid<C>(
     val color: C,
 ) : Fill<C>()
 
+@Serializable
+@SerialName("VerticalStripes")
 data class VerticalStripes<C>(
     val color0: C,
     val color1: C,
