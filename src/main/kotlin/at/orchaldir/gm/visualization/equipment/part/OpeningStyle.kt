9d7eeffa7d951958a7001d6cf27f9b07eb65d75d
Orchaldir <orchaldir@gmail.com>
2024-07-30 23:04:12 +0200

    Add visualizeButtons()

--- src/main/kotlin/at/orchaldir/gm/visualization/equipment/part/OpeningStyle.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/equipment/part/OpeningStyle.kt
@@ -3,8 +3,11 @@ package at.orchaldir.gm.visualization.equipment.part
 import at.orchaldir.gm.core.model.item.style.*
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.AABB
+import at.orchaldir.gm.utils.math.Distance
 import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.HALF
 import at.orchaldir.gm.utils.renderer.LineOptions
+import at.orchaldir.gm.utils.renderer.NoBorder
 import at.orchaldir.gm.visualization.RenderState
 import at.orchaldir.gm.visualization.character.ABOVE_EQUIPMENT_LAYER
 
@@ -19,8 +22,28 @@ fun visualizeOpening(
     when (openingStyle) {
         NoOpening -> doNothing()
         is DoubleBreasted -> doNothing()
-        is SingleBreasted -> doNothing()
+        is SingleBreasted -> visualizeButtons(state, aabb, x, topY, bottomY, openingStyle.buttons)
         is Zipper -> visualizeZipper(state, aabb, x, topY, bottomY, openingStyle)
+    }
+}
+
+fun visualizeButtons(
+    state: RenderState,
+    aabb: AABB,
+    x: Factor,
+    topY: Factor,
+    bottomY: Factor,
+    buttons: ButtonColumn,
+) {
+    val options = NoBorder(buttons.button.color.toRender())
+    val distance = bottomY - topY
+    val step = distance / buttons.count.toFloat()
+    var y = topY + step * HALF
+
+    for (i in 0..<buttons.count.toInt()) {
+        val center = aabb.getPoint(x, y)
+        state.renderer.renderCircle(center, Distance(0.01f), options, ABOVE_EQUIPMENT_LAYER)
+        y += step
     }
 }
 
