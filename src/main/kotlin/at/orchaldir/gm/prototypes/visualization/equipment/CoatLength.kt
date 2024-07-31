aa8e875f9ddb0b712945198c13e0c9131f61c4f7
Orchaldir <orchaldir@gmail.com>
2024-07-31 19:35:25 +0200

    Improve example

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/equipment/CoatLength.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/equipment/CoatLength.kt
@@ -1,5 +1,6 @@
 package at.orchaldir.gm.prototypes.visualization.equipment
 
+import at.orchaldir.gm.core.model.appearance.Color
 import at.orchaldir.gm.core.model.appearance.Color.Blue
 import at.orchaldir.gm.core.model.appearance.Size
 import at.orchaldir.gm.core.model.appearance.Solid
@@ -8,6 +9,9 @@ import at.orchaldir.gm.core.model.character.appearance.BodyShape
 import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
 import at.orchaldir.gm.core.model.item.Coat
+import at.orchaldir.gm.core.model.item.style.Button
+import at.orchaldir.gm.core.model.item.style.ButtonColumn
+import at.orchaldir.gm.core.model.item.style.DoubleBreasted
 import at.orchaldir.gm.core.model.item.style.OuterwearLength
 import at.orchaldir.gm.prototypes.visualization.RENDER_CONFIG
 import at.orchaldir.gm.prototypes.visualization.character.addNames
@@ -21,7 +25,16 @@ fun main() {
         addNames(OuterwearLength.entries),
         addNames(BodyShape.entries)
     ) { distance, shape, length ->
-        Pair(createAppearance(distance, shape), listOf(Coat(length, fill = Solid(Blue))))
+        Pair(
+            createAppearance(distance, shape),
+            listOf(
+                Coat(
+                    length,
+                    openingStyle = DoubleBreasted(ButtonColumn(Button(Size.Medium, Color.Gold), 5u)),
+                    fill = Solid(Blue)
+                )
+            )
+        )
     }
 }
 
