7edd8cea8037a30e20e7d25519634acf4d0b4df6
Orchaldir <orchaldir@gmail.com>
2024-08-05 21:36:10 +0200

    Split

--- src/main/kotlin/at/orchaldir/gm/core/model/calendar/CalendarEra.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/calendar/CalendarEra.kt
@@ -1,10 +1,6 @@
 package at.orchaldir.gm.core.model.calendar
 
-import at.orchaldir.gm.core.model.calendar.date.DisplayDate
-import at.orchaldir.gm.core.model.calendar.date.DisplayDay
-import at.orchaldir.gm.core.model.calendar.date.DisplayYear
 import kotlinx.serialization.Serializable
-import kotlin.math.absoluteValue
 
 @Serializable
 data class CalendarEra(
@@ -18,37 +14,5 @@ data class CalendarEra(
         "$text $date"
     } else {
         "$date $text"
-    }
-}
-
-@Serializable
-data class BeforeAndCurrent(
-    val before: CalendarEra,
-    val current: CalendarEra,
-) {
-    constructor(beforeText: String, beforeIsPrefix: Boolean, afterText: String, afterIsPrefix: Boolean) :
-            this(CalendarEra(false, beforeText, beforeIsPrefix), CalendarEra(true, afterText, afterIsPrefix))
-
-    fun getAll() = listOf(before, current)
-
-    fun resolve(date: DisplayDate) = when (date) {
-        is DisplayDay -> resolve(date)
-        is DisplayYear -> resolve(date)
-    }
-
-    fun resolve(day: DisplayDay) = if (day.yearIndex >= 0) {
-        val year = day.yearIndex + 1
-        current.resolve(resolve(year, day.monthIndex, day.dayIndex))
-    } else {
-        val year = day.yearIndex.absoluteValue
-        before.resolve(resolve(year, day.monthIndex, day.dayIndex))
-    }
-
-    private fun resolve(year: Int, month: Int, day: Int) = "$day.$month.$year"
-
-    fun resolve(year: DisplayYear) = if (year.year >= 0) {
-        current.resolve(year.year + 1)
-    } else {
-        before.resolve(year.year.absoluteValue)
     }
 }
