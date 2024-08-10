1f03827ffc8fd8b22df23c2b0b36b0cf0da735d1
Orchaldir <orchaldir@gmail.com>
2024-08-10 15:52:24 +0200

    Show time

--- src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/Routing.kt
@@ -1,7 +1,9 @@
 package at.orchaldir.gm.app.plugins
 
 import at.orchaldir.gm.app.STORE
+import at.orchaldir.gm.app.html.field
 import at.orchaldir.gm.app.html.fieldStorageLink
+import at.orchaldir.gm.app.html.link
 import at.orchaldir.gm.app.html.simpleHtml
 import at.orchaldir.gm.app.plugins.character.Characters
 import io.ktor.http.*
@@ -11,6 +13,7 @@ import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.h2
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
@@ -26,6 +29,7 @@ fun Application.configureRouting() {
 
             call.respondHtml(HttpStatusCode.OK) {
                 simpleHtml(TITLE) {
+                    h2 { +"Elements" }
                     fieldStorageLink(call, state.characters, Characters())
                     fieldStorageLink(call, state.calendars, Calendars())
                     fieldStorageLink(call, state.cultures, Cultures())
@@ -36,6 +40,11 @@ fun Application.configureRouting() {
                     fieldStorageLink(call, state.nameLists, NameLists())
                     fieldStorageLink(call, state.personalityTraits, Personality())
                     fieldStorageLink(call, state.races, Races())
+                    h2 { +"Time" }
+                    field("Default Calendar") {
+                        link(call, state, state.time.defaultCalendar)
+                    }
+                    field(state, "Current Date", state.time.currentDate)
                 }
             }
         }
