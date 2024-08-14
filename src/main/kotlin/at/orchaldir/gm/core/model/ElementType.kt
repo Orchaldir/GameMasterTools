132b1e1785c6a3b84deeb6dff0ba59a8b6bd39c4
Orchaldir <orchaldir@gmail.com>
2024-08-14 16:50:26 +0200

    Add ElementType

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/ElementType.kt
@@ -0,0 +1,14 @@
+package at.orchaldir.gm.core.model
+
+enum class ElementType {
+    Calendar,
+    Character,
+    Culture,
+    Fashion,
+    ItemTemplate,
+    Language,
+    Material,
+    NameList,
+    PersonalityTrait,
+    Race,
+}
