b8f50b8c6b96d49d41dbd4c62335c38f634f2bdb
Orchaldir <orchaldir@gmail.com>
2024-09-08 10:30:14 +0200

    remove old

--- src/main/kotlin/at/orchaldir/gm/app/plugins/race/Race.kt
+++ src/main/kotlin/at/orchaldir/gm/app/plugins/race/Race.kt
@@ -2,19 +2,14 @@ package at.orchaldir.gm.app.plugins.race
 
 import at.orchaldir.gm.app.STORE
 import at.orchaldir.gm.app.html.*
-import at.orchaldir.gm.app.parse.*
+import at.orchaldir.gm.app.parse.GENDER
+import at.orchaldir.gm.app.parse.NAME
+import at.orchaldir.gm.app.parse.parseRace
 import at.orchaldir.gm.core.action.CreateRace
 import at.orchaldir.gm.core.action.DeleteRace
 import at.orchaldir.gm.core.action.UpdateRace
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.character.appearance.EarsLayout
-import at.orchaldir.gm.core.model.character.appearance.EyesLayout
-import at.orchaldir.gm.core.model.character.appearance.SkinType
-import at.orchaldir.gm.core.model.character.appearance.beard.BeardType
-import at.orchaldir.gm.core.model.character.appearance.hair.HairType
 import at.orchaldir.gm.core.model.race.Race
-import at.orchaldir.gm.core.model.race.appearance.EyeOptions
-import at.orchaldir.gm.core.model.race.appearance.RaceAppearance
 import at.orchaldir.gm.core.selector.canDelete
 import at.orchaldir.gm.core.selector.getCharacters
 import io.ktor.http.*
@@ -119,8 +114,6 @@ private fun HTML.showRaceDetails(
     state: State,
     race: Race,
 ) {
-    val appearance = race.appearance
-    val eyeOptions = appearance.eyeOptions
     val backLink = call.application.href(RaceRoutes())
     val deleteLink = call.application.href(RaceRoutes.Delete(race.id))
     val editLink = call.application.href(RaceRoutes.Edit(race.id))
@@ -129,7 +122,7 @@ private fun HTML.showRaceDetails(
         field("Id", race.id.value.toString())
         field("Name", race.name)
         showRarityMap("Gender", race.genders)
-        showAppearanceOptions(appearance, eyeOptions)
+        showLifeStages(race)
         h2 { +"Characters" }
         showList(state.getCharacters(race.id)) { character ->
             link(call, state, character)
@@ -144,52 +137,16 @@ private fun HTML.showRaceDetails(
     }
 }
 
-private fun BODY.showAppearanceOptions(
-    appearance: RaceAppearance,
-    eyeOptions: EyeOptions,
+private fun BODY.showLifeStages(
+    race: Race,
 ) {
-    h2 { +"Appearance Options" }
-    showRarityMap("Type", appearance.appearanceType)
-    h3 { +"Skin" }
-    showRarityMap("Type", appearance.skinTypes)
-    if (appearance.skinTypes.isAvailable(SkinType.Scales)) {
-        showRarityMap("Scale Colors", appearance.scalesColors)
-    }
-    if (appearance.skinTypes.isAvailable(SkinType.Normal)) {
-        showRarityMap("Normal Skin Colors", appearance.normalSkinColors)
-    }
-    if (appearance.skinTypes.isAvailable(SkinType.Exotic)) {
-        showRarityMap("Exotic Skin Colors", appearance.exoticSkinColors)
-    }
-    h3 { +"Ears" }
-    showRarityMap("Layout", appearance.earsLayout)
-    if (appearance.earsLayout.isAvailable(EarsLayout.NormalEars)) {
-        showRarityMap("Ear Shapes", appearance.earShapes)
-    }
-    h3 { +"Eyes" }
-    showRarityMap("Layout", appearance.eyesLayout)
-    if (!appearance.eyesLayout.isAvailable(EyesLayout.NoEyes)) {
-        showRarityMap("Eye Shapes", eyeOptions.eyeShapes)
-        showRarityMap("Pupil Shape", eyeOptions.pupilShapes)
-        showRarityMap("Pupil Colors", eyeOptions.pupilColors)
-        showRarityMap("Sclera Colors", eyeOptions.scleraColors)
-    }
-    h3 { +"Hair" }
-    showRarityMap("Beard", appearance.hairOptions.beardTypes)
-    showRarityMap("Hair", appearance.hairOptions.hairTypes)
-    if (requiresHairColor(appearance)) {
-        showRarityMap("Colors", appearance.hairOptions.colors)
-    }
-    h3 { +"Mouth" }
-    showRarityMap("Types", appearance.mouthTypes)
+    h2 { +"Life Stages" }
 }
 
 private fun HTML.showRaceEditor(
     call: ApplicationCall,
     race: Race,
 ) {
-    val appearance = race.appearance
-    val eyeOptions = appearance.eyeOptions
     val backLink = call.application.href(RaceRoutes.Details(race.id))
     val previewLink = call.application.href(RaceRoutes.Preview(race.id))
     val updateLink = call.application.href(RaceRoutes.Update(race.id))
@@ -206,48 +163,7 @@ private fun HTML.showRaceEditor(
                 }
             }
             selectRarityMap("Gender", GENDER, race.genders)
-            h2 { +"Appearance Options" }
-            selectRarityMap("Type", APPEARANCE_TYPE, appearance.appearanceType)
-            h3 { +"Skin" }
-            selectRarityMap("Type", SKIN_TYPE, appearance.skinTypes, true)
-            if (appearance.skinTypes.isAvailable(SkinType.Scales)) {
-                selectRarityMap("Scale Colors", SCALE_COLOR, appearance.scalesColors)
-            }
-            if (appearance.skinTypes.isAvailable(SkinType.Normal)) {
-                selectRarityMap(
-                    "Normal Skin Colors",
-                    NORMAL_SKIN_COLOR,
-                    appearance.normalSkinColors
-                )
-            }
-            if (appearance.skinTypes.isAvailable(SkinType.Exotic)) {
-                selectRarityMap(
-                    "Exotic Skin Colors",
-                    EXOTIC_SKIN_COLOR,
-                    appearance.exoticSkinColors
-                )
-            }
-            h3 { +"Ears" }
-            selectRarityMap("Layout", EARS_LAYOUT, appearance.earsLayout, true)
-            if (appearance.earsLayout.isAvailable(EarsLayout.NormalEars)) {
-                selectRarityMap("Ear Shapes", EAR_SHAPE, appearance.earShapes)
-            }
-            h3 { +"Eyes" }
-            selectRarityMap("Layout", EYES_LAYOUT, appearance.eyesLayout, true)
-            if (!appearance.eyesLayout.isAvailable(EyesLayout.NoEyes)) {
-                selectRarityMap("Eye Shapes", EYE_SHAPE, eyeOptions.eyeShapes)
-                selectRarityMap("Pupil Shape", PUPIL_SHAPE, eyeOptions.pupilShapes)
-                selectRarityMap("Pupil Colors", PUPIL_COLOR, eyeOptions.pupilColors)
-                selectRarityMap("Sclera Colors", SCLERA_COLOR, eyeOptions.scleraColors)
-            }
-            h3 { +"Hair" }
-            selectRarityMap("Beard", BEARD_TYPE, appearance.hairOptions.beardTypes, true)
-            selectRarityMap("Hair", HAIR_TYPE, appearance.hairOptions.hairTypes, true)
-            if (requiresHairColor(appearance)) {
-                selectRarityMap("Colors", HAIR_COLOR, appearance.hairOptions.colors)
-            }
-            h3 { +"Mouth" }
-            selectRarityMap("Types", MOUTH_TYPE, appearance.mouthTypes)
+
             p {
                 submitInput {
                     value = "Update"
@@ -259,7 +175,3 @@ private fun HTML.showRaceEditor(
         back(backLink)
     }
 }
-
-private fun requiresHairColor(appearance: RaceAppearance) =
-    appearance.hairOptions.beardTypes.isAvailable(BeardType.Normal) ||
-            appearance.hairOptions.hairTypes.isAvailable(HairType.Normal)
