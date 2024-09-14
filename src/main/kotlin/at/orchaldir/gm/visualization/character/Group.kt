1e9a6da166463388a9aa789341fd8f0fe7569402
Orchaldir <orchaldir@gmail.com>
2024-09-14 16:57:15 +0200

    MR

--- src/main/kotlin/at/orchaldir/gm/visualization/character/Group.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/Group.kt
@@ -17,10 +17,10 @@ fun visualizeGroup(
     equipped: List<Equipment> = emptyList(),
     renderFront: Boolean = true,
 ): Svg {
-    val N = appearances.size
+    val number = appearances.size
     val sizes = appearances.map { it.getSize() }
     val maxSize = sizes.maxBy { it.value }
-    val groupSize = Size2d(maxSize * N + config.padding * 2, maxSize + config.padding * 2)
+    val groupSize = Size2d(maxSize * number + config.padding * 2, maxSize + config.padding * 2)
     val builder = SvgBuilder(groupSize)
     var start = Point2d(config.padding, Distance(groupSize.height) - config.padding)
 
