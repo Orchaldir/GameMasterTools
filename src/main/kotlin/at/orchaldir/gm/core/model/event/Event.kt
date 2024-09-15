92b6fd12f5e7a94f5cd7da6873254328414554bc
Orchaldir <orchaldir@gmail.com>
2024-09-15 09:52:23 +0200

    Add events

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/event/Event.kt
@@ -0,0 +1,39 @@
+package at.orchaldir.gm.core.model.event
+
+import at.orchaldir.gm.core.model.character.CauseOfDeath
+import at.orchaldir.gm.core.model.character.CharacterId
+import at.orchaldir.gm.core.model.character.CharacterOrigin
+import at.orchaldir.gm.core.model.time.Day
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Event {
+
+    abstract fun getDay(): Day
+
+}
+
+@Serializable
+@SerialName("CharacterOrigin")
+data class CharacterOriginEvent(
+    val day: Day,
+    val characterId: CharacterId,
+    val origin: CharacterOrigin,
+) : Event() {
+
+    override fun getDay() = day
+
+}
+
+@Serializable
+@SerialName("CharacterDeath")
+data class CharacterDeathEvent(
+    val day: Day,
+    val characterId: CharacterId,
+    val causeOfDeath: CauseOfDeath,
+) : Event() {
+
+    override fun getDay() = day
+
+}
