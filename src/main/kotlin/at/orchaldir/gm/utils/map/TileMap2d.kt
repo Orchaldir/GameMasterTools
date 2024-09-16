ffb710737691ffd41528ed9ecae7df3e10b108e3
Orchaldir <orchaldir@gmail.com>
2024-09-16 20:51:59 +0200

    Add TileMap2dRenderer

--- src/main/kotlin/at/orchaldir/gm/utils/map/TileMap2d.kt
+++ src/main/kotlin/at/orchaldir/gm/utils/map/TileMap2d.kt
@@ -4,12 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TileMap2d<TILE>(
-    val size2d: MapSize2d,
+    val size: MapSize2d,
     val tiles: List<TILE>,
 ) {
     constructor(size2d: MapSize2d, tile: TILE) : this(size2d, (1..size2d.tiles()).map { tile })
 
     init {
-        require(size2d.tiles() == tiles.size) { "The number of tiles must match the map size" }
+        require(size.tiles() == tiles.size) { "The number of tiles must match the map size" }
     }
+
+    fun getTile(index: Int) = tiles.getOrNull(index)
 }
---
+++ src/main/kotlin/at/orchaldir/gm/visualization/tile/TileMap2dRenderer.kt
@@ -0,0 +1,60 @@
+package at.orchaldir.gm.visualization.tile
+
+import at.orchaldir.gm.core.model.util.Color
+import at.orchaldir.gm.core.model.util.Color.Black
+import at.orchaldir.gm.utils.map.TileMap2d
+import at.orchaldir.gm.utils.math.AABB
+import at.orchaldir.gm.utils.math.Distance
+import at.orchaldir.gm.utils.math.Point2d
+import at.orchaldir.gm.utils.math.Size2d
+import at.orchaldir.gm.utils.renderer.FillAndBorder
+import at.orchaldir.gm.utils.renderer.LineOptions
+import at.orchaldir.gm.utils.renderer.Renderer
+
+data class TileMap2dRenderer(
+    val tileSize: Distance,
+    val borderSize: Distance,
+) {
+
+    fun <TILE> calculateMapSize(map: TileMap2d<TILE>) = Size2d(
+        tileSize * map.size.width,
+        tileSize * map.size.height,
+    )
+
+    fun <TILE> render(
+        map: TileMap2d<TILE>,
+        renderTile: (Int, Int, Int, AABB, TILE) -> Unit,
+    ) {
+        val size = map.size
+        val tileSize = Size2d.square(tileSize)
+        var index = 0
+
+        repeat(size.height) { y ->
+            repeat(size.width) { x ->
+                map.getTile(index)?.let { tile ->
+                    val position = calculateTilePosition(x, y)
+                    renderTile(index, x, y, AABB(position, tileSize), tile)
+                }
+
+                index++
+            }
+        }
+    }
+
+    fun <TILE> render(
+        renderer: Renderer,
+        map: TileMap2d<TILE>,
+        lookup: (TILE) -> Color,
+    ) {
+        val lineStyle = LineOptions(Black.toRender(), borderSize)
+
+        render(map) { _, _, _, aabb, tile ->
+            val color = lookup(tile)
+            val style = FillAndBorder(color.toRender(), lineStyle)
+
+            renderer.renderRectangle(aabb, style)
+        }
+    }
+
+    private fun calculateTilePosition(x: Int, y: Int) = Point2d(tileSize * x, tileSize * y)
+}
