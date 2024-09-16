e5cf12e18f86e93a7f7ed9eef8be8f7fc79bae8b
Orchaldir <orchaldir@gmail.com>
2024-09-16 19:40:52 +0200

    Add MapSize2d

---
+++ src/main/kotlin/at/orchaldir/gm/utils/map/MapSize2d.kt
@@ -0,0 +1,29 @@
+package at.orchaldir.gm.utils.map
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MapSize2d(val width: Int, val height: Int) {
+
+    init {
+        require(width >= 0) { "Width muster be greater or equal 0!" }
+        require(height >= 0) { "Height muster be greater or equal 0!" }
+    }
+
+    companion object {
+        fun square(size: Int) = MapSize2d(size, size)
+    }
+
+    fun isInside(x: Int, y: Int) = x in 0..<width && y in 0..<height
+
+    fun toIndex(x: Int, y: Int): Int? {
+        if (isInside(x, y)) {
+            return toIndexRisky(x, y)
+        }
+
+        return null
+    }
+
+    fun toIndexRisky(x: Int, y: Int) = y * width + x
+
+}
