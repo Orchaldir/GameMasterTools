21a3e4c348f1484f82e019d05b317cb764278f6a
Orchaldir <orchaldir@gmail.com>
2024-09-16 20:05:13 +0200

    test

---
+++ src/test/kotlin/at/orchaldir/gm/utils/map/MapSize2dTest.kt
@@ -0,0 +1,60 @@
+package at.orchaldir.gm.utils.map
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class MapSize2dTest {
+
+    private val size = MapSize2d(3, 2)
+
+    @ParameterizedTest(name = "x={0} y={1}")
+    @MethodSource("inside")
+    fun `Test if inside for points inside`(x: Int, y: Int, index: Int) {
+        assertTrue(size.isInside(x, y))
+    }
+
+    @ParameterizedTest(name = "x={0} y={1}")
+    @MethodSource("outside")
+    fun `Test if inside for points outside`(x: Int, y: Int) {
+        assertFalse(size.isInside(x, y))
+    }
+
+    @ParameterizedTest(name = "x={0} y={1} -> {2}")
+    @MethodSource("inside")
+    fun `Get index for points inside`(x: Int, y: Int, index: Int) {
+        assertEquals(index, size.toIndex(x, y))
+    }
+
+    @ParameterizedTest(name = "x={0} y={1}")
+    @MethodSource("outside")
+    fun `Get no index for points outside`(x: Int, y: Int) {
+        assertNull(size.toIndex(x, y))
+    }
+
+    companion object {
+        @JvmStatic
+        fun inside() = listOf(
+            Arguments.of(0, 0, 0),
+            Arguments.of(1, 0, 1),
+            Arguments.of(2, 0, 2),
+            Arguments.of(0, 1, 3),
+            Arguments.of(1, 1, 4),
+            Arguments.of(2, 1, 5),
+        )
+
+        @JvmStatic
+        fun outside() = listOf(
+            Arguments.of(-1, 0),
+            Arguments.of(0, -1),
+            Arguments.of(-1, -1),
+            Arguments.of(3, 0),
+            Arguments.of(0, 2),
+            Arguments.of(3, 2),
+        )
+    }
+}
