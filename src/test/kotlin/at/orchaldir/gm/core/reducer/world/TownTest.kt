b128867f9ce14f5d3f70f554b19758d82bce1a35
Orchaldir <orchaldir@gmail.com>
2024-09-18 07:21:03 +0200

    Test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/world/TownTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/world/TownTest.kt
@@ -1,18 +1,27 @@
 package at.orchaldir.gm.core.reducer.world
 
 import at.orchaldir.gm.core.action.DeleteTown
+import at.orchaldir.gm.core.action.UpdateTerrain
 import at.orchaldir.gm.core.action.UpdateTown
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.world.terrain.River
+import at.orchaldir.gm.core.model.world.terrain.RiverId
+import at.orchaldir.gm.core.model.world.terrain.RiverTerrain
+import at.orchaldir.gm.core.model.world.terrain.TerrainType
 import at.orchaldir.gm.core.model.world.town.Town
 import at.orchaldir.gm.core.model.world.town.TownId
+import at.orchaldir.gm.core.model.world.town.TownTile
 import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.map.MapSize2d
+import at.orchaldir.gm.utils.map.TileMap2d
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 private val ID0 = TownId(0)
+private val RIVER0 = RiverId(0)
 
 class TownTest {
 
@@ -52,6 +61,28 @@ class TownTest {
             val action = UpdateTown(town)
 
             assertEquals(town, REDUCER.invoke(state, action).first.getTownStorage().get(ID0))
+        }
+    }
+
+    @Nested
+    inner class UpdateTerrainTest {
+
+        @Test
+        fun `Cannot update unknown id`() {
+            val action = UpdateTerrain(ID0, TerrainType.Plain, 0, 0)
+            assertFailsWith<IllegalArgumentException> { REDUCER.invoke(State(), action) }
+        }
+
+        @Test
+        fun `Set to river`() {
+            val oldMap = TileMap2d(MapSize2d(2, 1), listOf(TownTile(), TownTile()))
+            val newMap = TileMap2d(MapSize2d(2, 1), listOf(TownTile(), TownTile(RiverTerrain(RIVER0))))
+            val oldTown = Town(ID0, map = oldMap)
+            val newTown = Town(ID0, map = newMap)
+            val state = State(listOf(Storage(River(RIVER0)), Storage(oldTown)))
+            val action = UpdateTerrain(ID0, TerrainType.River, 0, 1)
+
+            assertEquals(newTown, REDUCER.invoke(state, action).first.getTownStorage().get(ID0))
         }
     }
 
