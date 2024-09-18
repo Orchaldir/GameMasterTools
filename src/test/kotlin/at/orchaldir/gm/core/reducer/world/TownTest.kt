03f78e7dcaf233e03d7589575ca32a1fb9fa4728
Orchaldir <orchaldir@gmail.com>
2024-09-18 07:24:34 +0200

    Test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/world/TownTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/world/TownTest.kt
@@ -4,14 +4,13 @@ import at.orchaldir.gm.core.action.DeleteTown
 import at.orchaldir.gm.core.action.UpdateTerrain
 import at.orchaldir.gm.core.action.UpdateTown
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.world.terrain.River
-import at.orchaldir.gm.core.model.world.terrain.RiverId
-import at.orchaldir.gm.core.model.world.terrain.RiverTerrain
-import at.orchaldir.gm.core.model.world.terrain.TerrainType
+import at.orchaldir.gm.core.model.world.terrain.*
 import at.orchaldir.gm.core.model.world.town.Town
 import at.orchaldir.gm.core.model.world.town.TownId
 import at.orchaldir.gm.core.model.world.town.TownTile
 import at.orchaldir.gm.core.reducer.REDUCER
+import at.orchaldir.gm.utils.Element
+import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.Storage
 import at.orchaldir.gm.utils.map.MapSize2d
 import at.orchaldir.gm.utils.map.TileMap2d
@@ -21,6 +20,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 private val ID0 = TownId(0)
+private val MOUNTAIN0 = MountainId(0)
 private val RIVER0 = RiverId(0)
 
 class TownTest {
@@ -74,13 +74,31 @@ class TownTest {
         }
 
         @Test
+        fun `Set to Hill`() {
+            testSuccess(Mountain(MOUNTAIN0), TerrainType.Hill, HillTerrain(MOUNTAIN0))
+        }
+
+        @Test
+        fun `Set to mountain`() {
+            testSuccess(Mountain(MOUNTAIN0), TerrainType.Mountain, MountainTerrain(MOUNTAIN0))
+        }
+
+        @Test
         fun `Set to river`() {
+            testSuccess(River(RIVER0), TerrainType.River, RiverTerrain(RIVER0))
+        }
+
+        private fun <ID : Id<ID>, ELEMENT : Element<ID>> testSuccess(
+            river: ELEMENT,
+            type: TerrainType,
+            result: Terrain,
+        ) {
             val oldMap = TileMap2d(MapSize2d(2, 1), listOf(TownTile(), TownTile()))
-            val newMap = TileMap2d(MapSize2d(2, 1), listOf(TownTile(), TownTile(RiverTerrain(RIVER0))))
+            val newMap = TileMap2d(MapSize2d(2, 1), listOf(TownTile(), TownTile(result)))
             val oldTown = Town(ID0, map = oldMap)
             val newTown = Town(ID0, map = newMap)
-            val state = State(listOf(Storage(River(RIVER0)), Storage(oldTown)))
-            val action = UpdateTerrain(ID0, TerrainType.River, 0, 1)
+            val state = State(listOf(Storage(river), Storage(oldTown)))
+            val action = UpdateTerrain(ID0, type, 0, 1)
 
             assertEquals(newTown, REDUCER.invoke(state, action).first.getTownStorage().get(ID0))
         }
