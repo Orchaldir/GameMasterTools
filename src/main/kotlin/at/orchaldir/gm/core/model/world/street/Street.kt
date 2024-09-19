c293d9d8a58767d88bb827378a0ca9a8e0b1d315
Orchaldir <orchaldir@gmail.com>
2024-09-19 20:57:50 +0200

    Add Street

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/world/street/Street.kt
@@ -0,0 +1,31 @@
+package at.orchaldir.gm.core.model.world.street
+
+import at.orchaldir.gm.core.model.time.Date
+import at.orchaldir.gm.core.model.time.Year
+import at.orchaldir.gm.utils.Element
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val STREET = "Street"
+
+@JvmInline
+@Serializable
+value class StreetId(val value: Int) : Id<StreetId> {
+
+    override fun next() = StreetId(value + 1)
+    override fun type() = STREET
+    override fun value() = value
+
+}
+
+@Serializable
+data class Street(
+    val id: StreetId,
+    val name: String = "Street ${id.value}",
+    val startDate: Date = Year(0),
+) : Element<StreetId> {
+
+    override fun id() = id
+    override fun name() = name
+
+}
