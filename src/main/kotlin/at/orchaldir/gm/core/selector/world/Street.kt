416bfa66b1a1020fe860f855a1a579b0bb1dba3d
Orchaldir <orchaldir@gmail.com>
2024-09-21 08:00:26 +0200

    Add StreetTest

--- src/main/kotlin/at/orchaldir/gm/core/selector/world/Street.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/world/Street.kt
@@ -3,5 +3,5 @@ package at.orchaldir.gm.core.selector.world
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.world.street.StreetId
 
-fun State.canDelete(street: StreetId) = true
+fun State.canDelete(street: StreetId) = getTowns(street).isEmpty()
 
