666206ff8d97c5ecec97e5d1544cfc07116e9f3a
Orchaldir <orchaldir@gmail.com>
2024-09-23 20:19:02 +0200

    reformat

--- src/main/kotlin/at/orchaldir/gm/app/parse/world/Building.kt
+++ src/main/kotlin/at/orchaldir/gm/app/parse/world/Building.kt
@@ -1,7 +1,10 @@
 package at.orchaldir.gm.app.parse.world
 
 import at.orchaldir.gm.app.*
-import at.orchaldir.gm.app.parse.*
+import at.orchaldir.gm.app.parse.combine
+import at.orchaldir.gm.app.parse.parseCharacterId
+import at.orchaldir.gm.app.parse.parseDate
+import at.orchaldir.gm.app.parse.parseInt
 import at.orchaldir.gm.core.action.UpdateBuilding
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.time.Date
