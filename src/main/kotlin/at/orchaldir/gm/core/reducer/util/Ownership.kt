0ddb18713303d7cda277933e3829f81cfbba570f
Orchaldir <orchaldir@gmail.com>
2024-11-08 17:21:30 +0100

    Refactor checkOwnership()

--- src/main/kotlin/at/orchaldir/gm/core/reducer/util/Ownership.kt
+++ src/main/kotlin/at/orchaldir/gm/core/reducer/util/Ownership.kt
@@ -9,55 +9,44 @@ import at.orchaldir.gm.core.model.util.History
 import at.orchaldir.gm.core.selector.getDefaultCalendar
 import at.orchaldir.gm.core.selector.isAlive
 import at.orchaldir.gm.core.selector.world.exists
-import at.orchaldir.gm.utils.doNothing
 
 fun checkOwnership(
     state: State,
     ownership: History<Owner>,
     creationDate: Date,
 ) {
-    checkOwner(state, ownership.owner, "owner")
-
     val calendar = state.getDefaultCalendar()
     var min = creationDate
 
-
     ownership.previousOwners.withIndex().forEach { (index, previous) ->
-        checkOwner(state, previous.entry, "previous owner")
-        checkOwnerStart(state, previous.entry, "${index + 1}.previous owner", min)
+        checkOwner(state, previous.entry, "${index + 1}.previous owner", min)
         require(calendar.compareTo(previous.until, min) > 0) { "${index + 1}.previous owner's until is too early!" }
 
         min = previous.until
     }
 
-    checkOwnerStart(state, ownership.owner, "Owner", min)
+    checkOwner(state, ownership.owner, "Owner", min)
 }
 
 private fun checkOwner(
     state: State,
     owner: Owner,
     noun: String,
-) {
-    when (owner) {
-        is OwnedByCharacter -> state.getCharacterStorage()
-            .require(owner.character) { "Cannot use an unknown character ${owner.character.value} as $noun!" }
-
-        is OwnedByTown -> state.getTownStorage()
-            .require(owner.town) { "Cannot use an unknown town ${owner.town.value} as $noun!" }
-
-        else -> doNothing()
-    }
-}
-
-private fun checkOwnerStart(
-    state: State,
-    owner: Owner,
-    noun: String,
     startInterval: Date,
 ) {
     val exists = when (owner) {
-        is OwnedByCharacter -> state.isAlive(owner.character, startInterval)
-        is OwnedByTown -> state.exists(owner.town, startInterval)
+        is OwnedByCharacter -> {
+            val character = state.getCharacterStorage()
+                .getOrThrow(owner.character) { "Cannot use an unknown character ${owner.character.value} as $noun!" }
+            state.isAlive(character, startInterval)
+        }
+
+        is OwnedByTown -> {
+            val town = state.getTownStorage()
+                .getOrThrow(owner.town) { "Cannot use an unknown town ${owner.town.value} as $noun!" }
+            state.exists(town, startInterval)
+        }
+
         else -> return
     }
 
