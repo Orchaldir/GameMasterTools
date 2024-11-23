63817a33624261e1f29670cda87567785d934095
Orchaldir <orchaldir@gmail.com>
2024-11-23 21:08:25 +0100

    Add ReferenceForName

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/name/ReferenceForName.kt
@@ -0,0 +1,26 @@
+package at.orchaldir.gm.core.model.name
+
+import at.orchaldir.gm.core.model.world.moon.MoonId
+import at.orchaldir.gm.core.model.world.terrain.Mountain
+import at.orchaldir.gm.core.model.world.terrain.RiverId
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class ReferenceForName
+
+@Serializable
+@SerialName("Moon")
+data class ReferencedMoon(val id: MoonId) : ReferenceForName()
+
+@Serializable
+@SerialName("Mountain")
+data class ReferencedMountain(val id: Mountain) : ReferenceForName()
+
+@Serializable
+@SerialName("River")
+data class ReferencedRiver(val id: RiverId) : ReferenceForName()
+
+@Serializable
+@SerialName("River")
+data class ReferencedTown(val id: RiverId) : ReferenceForName()
