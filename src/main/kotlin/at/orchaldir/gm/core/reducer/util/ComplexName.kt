ff39470f0792595c1abd6b45d37e233d483faa57
Orchaldir <orchaldir@gmail.com>
2024-11-24 22:16:23 +0100

    Add checkComplexName()

--- src/main/kotlin/at/orchaldir/gm/core/reducer/util/ComplexName.kt
+++ src/main/kotlin/at/orchaldir/gm/core/reducer/util/ComplexName.kt
@@ -1,9 +1,9 @@
 package at.orchaldir.gm.core.reducer.util
 
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.name.ComplexName
-import at.orchaldir.gm.core.model.name.NameWithReference
-import at.orchaldir.gm.core.model.name.SimpleName
+import at.orchaldir.gm.core.model.name.*
+
+private const val REQUIRED_REFERENCE = "Reference for complex name is unknown!"
 
 fun checkComplexName(
     state: State,
@@ -12,6 +12,15 @@ fun checkComplexName(
     when (name) {
         is NameWithReference -> {
             require(!(name.prefix.isNullOrEmpty() && name.postfix.isNullOrEmpty())) { "The prefix & the postfix of the complex must not be empty at the same time!" }
+
+            when (name.reference) {
+                is ReferencedFamilyName -> state.getCharacterStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+                is ReferencedFullName -> state.getCharacterStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+                is ReferencedMoon -> state.getMoonStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+                is ReferencedMountain -> state.getMountainStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+                is ReferencedRiver -> state.getRiverStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+                is ReferencedTown -> state.getTownStorage().require(name.reference.id) { REQUIRED_REFERENCE }
+            }
         }
 
         is SimpleName -> require(name.name.isNotEmpty()) { "Simple must not be empty!" }
