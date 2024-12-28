bd2da7ca8ccd1e18af45cb6125b98d4d86890aa1
Orchaldir <orchaldir@gmail.com>
2024-12-28 18:25:21 +0100

    Add tests

---
+++ src/test/kotlin/at/orchaldir/gm/core/model/material/MaterialCostTest.kt
@@ -0,0 +1,26 @@
+package at.orchaldir.gm.core.model.material
+
+import at.orchaldir.gm.MATERIAL_ID_0
+import at.orchaldir.gm.MATERIAL_ID_1
+import at.orchaldir.gm.core.model.material.MaterialCost.Companion.init
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class MaterialCostTest {
+
+    @Test
+    fun `Test simple constructor`() {
+        assertEquals(init(mapOf(MATERIAL_ID_0 to 1)), MaterialCost(MATERIAL_ID_0))
+    }
+
+    @Test
+    fun `Filter a cost of 0`() {
+        assertEquals(init(mapOf(MATERIAL_ID_0 to 2)), init(mapOf(MATERIAL_ID_0 to 2, MATERIAL_ID_1 to 0)))
+    }
+
+    @Test
+    fun `Filter a negative cost`() {
+        assertEquals(init(mapOf(MATERIAL_ID_1 to 3)), init(mapOf(MATERIAL_ID_0 to -1, MATERIAL_ID_1 to 3)))
+    }
+
+}
