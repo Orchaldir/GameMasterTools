8aec60d9d3aa01280e5fd4985139ee43ee317477
Orchaldir <orchaldir@gmail.com>
2024-12-31 16:45:16 +0100

    Add Book

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/book/Book.kt
@@ -0,0 +1,28 @@
+package at.orchaldir.gm.core.model.book
+
+import at.orchaldir.gm.core.model.util.ElementWithSimpleName
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val BOOK_TYPE = "Book"
+
+@JvmInline
+@Serializable
+value class BookId(val value: Int) : Id<BookId> {
+
+    override fun next() = BookId(value + 1)
+    override fun type() = BOOK_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class Book(
+    val id: BookId,
+    val name: String = "Book ${id.value}",
+) : ElementWithSimpleName<BookId> {
+
+    override fun id() = id
+    override fun name() = name
+
+}
