0ea09317c67cc665a7f8a2ac6444f83b4e839a8c
Orchaldir <orchaldir@gmail.com>
2025-01-03 16:36:34 +0100

    Add StitchType

--- src/main/kotlin/at/orchaldir/gm/core/model/item/book/BookBinding.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/book/BookBinding.kt
@@ -1,9 +1,12 @@
 package at.orchaldir.gm.core.model.item.book
 
+import at.orchaldir.gm.core.model.item.book.StitchType.Kettle
 import at.orchaldir.gm.core.model.material.MaterialId
 import at.orchaldir.gm.core.model.util.Color
+import at.orchaldir.gm.core.model.util.Size
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.intellij.lang.annotations.Pattern
 
 enum class BookBindingType {
     Coptic,
@@ -26,6 +29,7 @@ sealed class BookBinding {
 data class CopticBinding(
     val material: MaterialId,
     val sewingColor: Color,
+    val stitchPattern: List<StitchType>,
     val coverColor: Color,
 ) : BookBinding()
 
---
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/book/StitchType.kt
@@ -0,0 +1,8 @@
+package at.orchaldir.gm.core.model.item.book
+
+enum class StitchType {
+    Kettle,
+    FrenchLink,
+    WideFrenchLink,
+    Empty,
+}
