7a5d7424887cc7a2e83f0fd6a337a773880fc946
Orchaldir <orchaldir@gmail.com>
2025-01-04 18:26:15 +0100

    Fix padding

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/book/BookBinding.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/book/BookBinding.kt
@@ -4,17 +4,18 @@ import at.orchaldir.gm.core.model.item.book.*
 import at.orchaldir.gm.core.model.material.MaterialId
 import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.prototypes.visualization.addNames
+import at.orchaldir.gm.utils.math.Distance
 import at.orchaldir.gm.utils.math.Size2i
 
 private val ID = MaterialId(0)
 
 fun main() {
-    val size = Size2i(100, 200)
+    val size = Size2i(125, 190)
 
     renderBookTable(
         "book-binding.svg",
         BOOK_CONFIG,
-        size.toSize2d(),
+        size.toSize2d() + Distance(50),
         addNames(listOf(Color.SaddleBrown, Color.Red, Color.Black, Color.Green)),
         addNames(BookBindingType.entries),
     ) { color, type ->
