fa7334b3c89b757fcd10a803c524dafb9123e2b7
Orchaldir <orchaldir@gmail.com>
2025-01-25 15:40:52 +0100

    Reformat

--- src/test/kotlin/at/orchaldir/gm/core/reducer/organization/OrganizationTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/organization/OrganizationTest.kt
@@ -8,7 +8,7 @@ import at.orchaldir.gm.core.model.organization.Organization
 import at.orchaldir.gm.core.model.util.CreatedByCharacter
 import at.orchaldir.gm.core.model.util.CreatedByOrganization
 import at.orchaldir.gm.core.model.world.building.Building
-import at.orchaldir.gm.core.reducer.*
+import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
