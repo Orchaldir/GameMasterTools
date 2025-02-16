e618222fff9c0d973facd274bf05d296b3af8154
Orchaldir <orchaldir@gmail.com>
2025-02-16 09:02:43 +0100

    Refactor test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/character/AppearanceTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/character/AppearanceTest.kt
@@ -1,40 +1,44 @@
 package at.orchaldir.gm.core.reducer.character
 
+import at.orchaldir.gm.*
 import at.orchaldir.gm.core.action.UpdateAppearance
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.character.Character
-import at.orchaldir.gm.core.model.character.CharacterId
 import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.HeadOnly
 import at.orchaldir.gm.core.model.character.appearance.NormalSkin
+import at.orchaldir.gm.core.model.race.Race
 import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
 import at.orchaldir.gm.utils.math.Distance
+import at.orchaldir.gm.utils.math.Distribution
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-
-private val ID0 = CharacterId(0)
 
 class AppearanceTest {
 
-    private val appearance = HeadOnly(Head(skin = NormalSkin()), Distance(500))
-    private val action = UpdateAppearance(ID0, appearance)
+    private val state = State(
+        listOf(
+            Storage(CALENDAR0),
+            Storage(Character(CHARACTER_ID_0)),
+            Storage(Race(RACE_ID_0, height = Distribution.fromMeters(1.0f, 0.1f))),
+        )
+    )
+    private val appearance = HeadOnly(Head(skin = NormalSkin()), Distance(1000))
+    private val action = UpdateAppearance(CHARACTER_ID_0, appearance)
 
     @Test
     fun `Update appearance`() {
-        val state = State(Storage(listOf(Character(ID0))))
-
         val result = REDUCER.invoke(state, action).first
 
-        assertEquals(appearance, result.getCharacterStorage().getOrThrow(ID0).appearance)
+        assertEquals(appearance, result.getCharacterStorage().getOrThrow(CHARACTER_ID_0).appearance)
     }
 
     @Test
     fun `Cannot update unknown character`() {
         val state = State()
 
-        assertFailsWith<IllegalArgumentException> { REDUCER.invoke(state, action) }
+        assertIllegalArgument("Requires unknown Character 0!") { REDUCER.invoke(state, action) }
     }
 
 }
