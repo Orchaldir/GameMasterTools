98c5e1cc49c7ad7c2d417df34dd468a2dd3394ee
Orchaldir <orchaldir@gmail.com>
2025-02-19 17:39:33 +0100

    Cannot delete the god of a heart plane

--- src/test/kotlin/at/orchaldir/gm/core/reducer/religion/GodTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/religion/GodTest.kt
@@ -1,27 +1,27 @@
 package at.orchaldir.gm.core.reducer.religion
 
-import at.orchaldir.gm.CALENDAR0
-import at.orchaldir.gm.GOD_ID_0
-import at.orchaldir.gm.assertIllegalArgument
+import at.orchaldir.gm.*
 import at.orchaldir.gm.core.action.DeleteGod
 import at.orchaldir.gm.core.action.UpdateGod
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.religion.God
+import at.orchaldir.gm.core.model.world.plane.HeartPlane
+import at.orchaldir.gm.core.model.world.plane.Plane
 import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-private val god0 = God(GOD_ID_0)
-private val STATE = State(
-    listOf(
-        Storage(CALENDAR0),
-        Storage(god0),
-    )
-)
-
 class GodTest {
+
+    private val god0 = God(GOD_ID_0)
+    private val state = State(
+        listOf(
+            Storage(CALENDAR0),
+            Storage(god0),
+        )
+    )
 
     @Nested
     inner class DeleteTest {
@@ -29,7 +29,15 @@ class GodTest {
 
         @Test
         fun `Can delete an existing god`() {
-            assertEquals(0, REDUCER.invoke(STATE, action).first.getGodStorage().getSize())
+            assertEquals(0, REDUCER.invoke(state, action).first.getGodStorage().getSize())
+        }
+
+        @Test
+        fun `Cannot delete the god of a heart plane`() {
+            val plane = Plane(PLANE_ID_0, purpose = HeartPlane(GOD_ID_0))
+            val newState = state.updateStorage(Storage(plane))
+
+            assertIllegalArgument("The god 0 is used!") { REDUCER.invoke(newState, action) }
         }
 
         @Test
@@ -43,10 +51,9 @@ class GodTest {
 
         @Test
         fun `Cannot update unknown id`() {
-            val action = UpdateGod(God(GOD_ID_0))
-            val state = STATE.removeStorage(GOD_ID_0)
+            val action = UpdateGod(God(UNKNOWN_GOD_ID))
 
-            assertIllegalArgument("Requires unknown God 0!") { REDUCER.invoke(state, action) }
+            assertIllegalArgument("Requires unknown God 99!") { REDUCER.invoke(state, action) }
         }
 
         @Test
@@ -54,7 +61,7 @@ class GodTest {
             val god = God(GOD_ID_0, "Test")
             val action = UpdateGod(god)
 
-            assertEquals(god, REDUCER.invoke(STATE, action).first.getGodStorage().get(GOD_ID_0))
+            assertEquals(god, REDUCER.invoke(state, action).first.getGodStorage().get(GOD_ID_0))
         }
     }
 
