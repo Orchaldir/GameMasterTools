31da82afbf055bad7d9a614282cb96f4fd1b1782
Orchaldir <orchaldir@gmail.com>
2025-02-22 19:19:18 +0100

    Add visualizeClaws()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Body.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Body.kt
@@ -3,7 +3,10 @@ package at.orchaldir.gm.visualization.character.appearance
 import at.orchaldir.gm.core.model.character.appearance.Body
 import at.orchaldir.gm.core.model.character.appearance.BodyShape
 import at.orchaldir.gm.core.model.character.appearance.BodyShape.*
+import at.orchaldir.gm.core.model.character.appearance.ClawedFoot
+import at.orchaldir.gm.core.model.util.Size
 import at.orchaldir.gm.utils.math.*
+import at.orchaldir.gm.utils.renderer.model.NoBorder
 import at.orchaldir.gm.utils.renderer.model.RenderOptions
 import at.orchaldir.gm.visualization.SizeConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderConfig
@@ -257,5 +260,45 @@ fun visualizeFeet(
 
     layer.renderCircleArc(left, radius, offset, angle, options)
     layer.renderCircleArc(right, radius, offset, angle, options)
+
+    if (body.foot is ClawedFoot) {
+        visualizeClaws(state, body.foot, layerIndex, left, radius)
+        visualizeClaws(state, body.foot, layerIndex, right, radius)
+    }
+}
+
+fun visualizeClaws(
+    state: CharacterRenderState,
+    foot: ClawedFoot,
+    layerIndex: Int,
+    center: Point2d,
+    radius: Distance,
+) {
+    val options = NoBorder(foot.color.toRender())
+    val length = radius * 2
+    val stepLength = length / foot.count
+    val step = Point2d(stepLength, Distance(0))
+    var position = center - step * ((foot.count - 1) / 2.0f)
+    val clawLength = radius.toMeters() * 0.5f * when (foot.size) {
+        Size.Small -> 0.75f
+        Size.Medium -> 1.0f
+        Size.Large -> 1.25f
+    }
+    val clawLengthHalf = clawLength / 2.0f
+    val clawWidth = clawLength / 2.0f
+    val clawWidthHalf = clawWidth / 2.0f
+
+    (0..<foot.count).forEach {
+        val points = listOf(
+            position + Point2d(-clawWidthHalf, -clawLengthHalf),
+            position + Point2d(0.0f, clawLengthHalf),
+            position + Point2d(clawWidthHalf, -clawLengthHalf),
+        )
+        val polygon = Polygon2d(points)
+
+        state.renderer.getLayer(layerIndex).renderPolygon(polygon, options)
+
+        position += step
+    }
 }
 
