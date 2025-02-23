72091620ed933e943aef3d701209df8fb52a1df2
Orchaldir <orchaldir@gmail.com>
2025-02-23 09:43:50 +0100

    Add example

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/appearance/Wings.kt
@@ -0,0 +1,42 @@
+package at.orchaldir.gm.prototypes.visualization.character.appearance
+
+import at.orchaldir.gm.core.model.character.appearance.Appearance
+import at.orchaldir.gm.core.model.character.appearance.Body
+import at.orchaldir.gm.core.model.character.appearance.Head
+import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
+import at.orchaldir.gm.core.model.character.appearance.wing.*
+import at.orchaldir.gm.core.model.util.Side
+import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
+import at.orchaldir.gm.utils.math.Distance
+
+fun main() {
+    val appearances: MutableList<List<Appearance>> = mutableListOf<List<Appearance>>()
+
+    appearances.add(WingType.entries.map { createAppearance(OneWing(createWing(it), Side.Left)) })
+    appearances.add(WingType.entries.map { createAppearance(OneWing(createWing(it), Side.Right)) })
+    appearances.add(WingType.entries.map { createAppearance(TwoWings(createWing(it))) })
+    appearances.add(
+        listOf(
+            createAppearance(DifferentWings(BatWing(), BirdWing())),
+            createAppearance(DifferentWings(BirdWing(), ButterflyWing())),
+            createAppearance(DifferentWings(ButterflyWing(), BatWing())),
+        )
+    )
+
+    renderCharacterTable("wings.svg", CHARACTER_CONFIG, appearances)
+}
+
+private fun createWing(type: WingType) = when (type) {
+    WingType.Bat -> BatWing()
+    WingType.Bird -> BirdWing()
+    WingType.Butterfly -> ButterflyWing()
+}
+
+private fun createAppearance(wings: Wings) =
+    HumanoidBody(
+        Body(),
+        Head(),
+        Distance(200),
+        wings,
+    )
