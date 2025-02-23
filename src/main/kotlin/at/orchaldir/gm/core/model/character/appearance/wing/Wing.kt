8fef40a80a775c853a263409eba32fd8e8f219dc
Orchaldir <orchaldir@gmail.com>
2025-02-23 09:33:52 +0100

    Add Wings

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/character/appearance/wing/Wing.kt
@@ -0,0 +1,40 @@
+package at.orchaldir.gm.core.model.character.appearance.wing
+
+import at.orchaldir.gm.core.model.util.Color
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class WingType {
+    Bat,
+    Bird,
+    Butterfly,
+}
+
+@Serializable
+sealed class Wing {
+
+    fun getType() = when (this) {
+        is BatWing -> WingType.Bat
+        is BirdWing -> WingType.Bird
+        is ButterflyWing -> WingType.Butterfly
+    }
+
+}
+
+@Serializable
+@SerialName("Bat")
+data class BatWing(
+    val color: Color = Color.Black,
+) : Wing()
+
+@Serializable
+@SerialName("Bird")
+data class BirdWing(
+    val color: Color = Color.White,
+) : Wing()
+
+@Serializable
+@SerialName("Butterfly")
+data class ButterflyWing(
+    val color: Color = Color.White,
+) : Wing()
---
+++ src/main/kotlin/at/orchaldir/gm/core/model/character/appearance/wing/Wings.kt
@@ -0,0 +1,47 @@
+package at.orchaldir.gm.core.model.character.appearance.wing
+
+import at.orchaldir.gm.core.model.util.Side
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class WingsLayout {
+    NoWings,
+    OneWing,
+    TwoWings,
+    DifferentWings,
+}
+
+@Serializable
+sealed class Wings {
+
+    fun getType() = when (this) {
+        NoWings -> WingsLayout.NoWings
+        is OneWing -> WingsLayout.OneWing
+        is TwoWings -> WingsLayout.TwoWings
+        is DifferentWings -> WingsLayout.DifferentWings
+    }
+
+}
+
+@Serializable
+@SerialName("None")
+data object NoWings : Wings()
+
+@Serializable
+@SerialName("One")
+data class OneWing(
+    val eye: Wing = BirdWing(),
+    val size: Side = Side.Left,
+) : Wings()
+
+@Serializable
+@SerialName("Two")
+data class TwoWings(val wing: Wing = BirdWing()) : Wings()
+
+@Serializable
+@SerialName("Different")
+data class DifferentWings(
+    val left: Wing = BirdWing(),
+    val right: Wing = BirdWing(),
+) : Wings()
+
