ac476f5518cea019fe297ccfbffdc5ac7bf48993
Orchaldir <orchaldir@gmail.com>
2025-03-05 10:09:54 +0100

    Refactor EmploymentStatusTest

--- src/test/kotlin/at/orchaldir/gm/core/reducer/util/EmploymentStatusTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/util/EmploymentStatusTest.kt
@@ -9,46 +9,46 @@ import at.orchaldir.gm.core.model.util.History
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Test
 
-private val EMPLOYED = Employed(BUSINESS_ID_0, JOB_ID_0)
-private val STATE = State(
-    listOf(
-        Storage(CALENDAR0),
-        Storage(Business(BUSINESS_ID_0)),
-        Storage(Job(JOB_ID_0)),
-    )
-)
-
 class EmploymentStatusTest {
+
+    private val employed = Employed(BUSINESS_ID_0, JOB_ID_0)
+    private val state = State(
+        listOf(
+            Storage(CALENDAR0),
+            Storage(Business(BUSINESS_ID_0)),
+            Storage(Job(JOB_ID_0)),
+        )
+    )
 
     @Test
     fun `Cannot use unknown business`() {
-        val state = STATE.removeStorage(BUSINESS_ID_0)
+        val newState = state.removeStorage(BUSINESS_ID_0)
 
         assertIllegalArgument("The employment's business doesn't exist!") {
-            checkEmploymentStatusHistory(state, History(EMPLOYED), DAY0)
+            checkEmploymentStatusHistory(newState, History(employed), DAY0)
         }
     }
 
     @Test
     fun `Cannot use unknown job`() {
-        val state = STATE.removeStorage(JOB_ID_0)
+        val newState = state.removeStorage(JOB_ID_0)
 
         assertIllegalArgument("The employment's job doesn't exist!") {
-            checkEmploymentStatusHistory(state, History(EMPLOYED), DAY0)
+            checkEmploymentStatusHistory(newState, History(employed), DAY0)
         }
     }
 
     @Test
     fun `Character has a valid job`() {
-        checkEmploymentStatusHistory(STATE, History(EMPLOYED), DAY0)
+        checkEmploymentStatusHistory(state, History(employed), DAY0)
     }
 
     @Test
     fun `Character employed by a business before its founding`() {
-        val state = STATE.updateStorage(Storage(Business(BUSINESS_ID_0, startDate = DAY1)))
+        val newState = state.updateStorage(Storage(Business(BUSINESS_ID_0, startDate = DAY1)))
 
         assertIllegalArgument("The employment's business is not in operation!") {
-            checkEmploymentStatusHistory(state, History(EMPLOYED), DAY0)
+            checkEmploymentStatusHistory(newState, History(employed), DAY0)
         }
     }
 
