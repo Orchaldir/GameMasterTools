e251db672c3af75eb5bf67c8de42ae58b268e342
Orchaldir <orchaldir@gmail.com>
2025-03-08 23:44:45 +0100

    Add sumUntil()

--- src/main/kotlin/at/orchaldir/gm/utils/math/SegmentWeightCalculator.kt
+++ src/main/kotlin/at/orchaldir/gm/utils/math/SegmentWeightCalculator.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.utils.math
 sealed class SegmentWeightCalculator {
 
     abstract fun calculate(index: Int): Float
+    abstract fun sumUntil(index: Int): Float
 
 }
 
@@ -14,10 +15,12 @@ class ConstantWeight private constructor(
     constructor(segments: Int) : this(segments, 1.0f / segments)
 
     override fun calculate(index: Int): Float {
-        require(index in 0..<segments) { "Index $index is invalid!" }
+        validateIndex(index, segments)
 
         return weight
     }
+
+    override fun sumUntil(index: Int) = calculate(index) * index
 }
 
 class LinearDecreasingWeight private constructor(
@@ -28,9 +31,22 @@ class LinearDecreasingWeight private constructor(
     constructor(segments: Int) : this(segments, (0..<segments).sumOf { it + 2 }.toFloat())
 
     override fun calculate(index: Int): Float {
-        require(index in 0..<segments) { "Index $index is invalid!" }
+        validateIndex(index, segments)
 
         return (segments - index + 1) / totalWeight
     }
+
+    override fun sumUntil(index: Int): Float {
+        validateIndex(index, segments)
+
+        return (0..<index)
+            .map { calculate(it) }
+            .sum()
+    }
+
+}
+
+private fun validateIndex(index: Int, segments: Int) {
+    require(index in 0..<segments) { "Index $index is invalid!" }
 }
 
--- src/test/kotlin/at/orchaldir/gm/utils/math/SegmentWeightCalculatorTest.kt
+++ src/test/kotlin/at/orchaldir/gm/utils/math/SegmentWeightCalculatorTest.kt
@@ -12,22 +12,50 @@ class SegmentWeightCalculatorTest {
 
         private val calculator = ConstantWeight(4)
 
-        @Test
-        fun `Test valid indices`() {
-            assertEquals(0.25f, calculator.calculate(0))
-            assertEquals(0.25f, calculator.calculate(1))
-            assertEquals(0.25f, calculator.calculate(2))
-            assertEquals(0.25f, calculator.calculate(3))
+        @Nested
+        inner class CalculateTest {
+
+            @Test
+            fun `Test valid indices`() {
+                assertEquals(0.25f, calculator.calculate(0))
+                assertEquals(0.25f, calculator.calculate(1))
+                assertEquals(0.25f, calculator.calculate(2))
+                assertEquals(0.25f, calculator.calculate(3))
+            }
+
+            @Test
+            fun `Negative index`() {
+                assertIllegalArgument("Index -1 is invalid!") { calculator.calculate(-1) }
+            }
+
+            @Test
+            fun `Index too high`() {
+                assertIllegalArgument("Index 4 is invalid!") { calculator.calculate(4) }
+            }
+
         }
 
-        @Test
-        fun `Negative index`() {
-            assertIllegalArgument("Index -1 is invalid!") { calculator.calculate(-1) }
-        }
+        @Nested
+        inner class SumUntilTest {
 
-        @Test
-        fun `Index too high`() {
-            assertIllegalArgument("Index 4 is invalid!") { calculator.calculate(4) }
+            @Test
+            fun `Test valid indices`() {
+                assertEquals(0.0f, calculator.sumUntil(0))
+                assertEquals(0.25f, calculator.sumUntil(1))
+                assertEquals(0.5f, calculator.sumUntil(2))
+                assertEquals(0.75f, calculator.sumUntil(3))
+            }
+
+            @Test
+            fun `Negative index`() {
+                assertIllegalArgument("Index -1 is invalid!") { calculator.sumUntil(-1) }
+            }
+
+            @Test
+            fun `Index too high`() {
+                assertIllegalArgument("Index 4 is invalid!") { calculator.sumUntil(4) }
+            }
+
         }
 
     }
@@ -37,21 +65,48 @@ class SegmentWeightCalculatorTest {
 
         private val calculator = LinearDecreasingWeight(3)
 
-        @Test
-        fun `Test valid indices`() {
-            assertEquals(4.0f / 9.0f, calculator.calculate(0))
-            assertEquals(3.0f / 9.0f, calculator.calculate(1))
-            assertEquals(2.0f / 9.0f, calculator.calculate(2))
+        @Nested
+        inner class CalculateTest {
+
+            @Test
+            fun `Test valid indices`() {
+                assertEquals(4.0f / 9.0f, calculator.calculate(0))
+                assertEquals(3.0f / 9.0f, calculator.calculate(1))
+                assertEquals(2.0f / 9.0f, calculator.calculate(2))
+            }
+
+            @Test
+            fun `Negative index`() {
+                assertIllegalArgument("Index -1 is invalid!") { calculator.calculate(-1) }
+            }
+
+            @Test
+            fun `Index too high`() {
+                assertIllegalArgument("Index 3 is invalid!") { calculator.calculate(3) }
+            }
+
         }
 
-        @Test
-        fun `Negative index`() {
-            assertIllegalArgument("Index -1 is invalid!") { calculator.calculate(-1) }
-        }
+        @Nested
+        inner class SumUntilTest {
 
-        @Test
-        fun `Index too high`() {
-            assertIllegalArgument("Index 3 is invalid!") { calculator.calculate(3) }
+            @Test
+            fun `Test valid indices`() {
+                assertEquals(0.0f / 9.0f, calculator.sumUntil(0))
+                assertEquals(4.0f / 9.0f, calculator.sumUntil(1))
+                assertEquals(7.0f / 9.0f, calculator.sumUntil(2))
+            }
+
+            @Test
+            fun `Negative index`() {
+                assertIllegalArgument("Index -1 is invalid!") { calculator.sumUntil(-1) }
+            }
+
+            @Test
+            fun `Index too high`() {
+                assertIllegalArgument("Index 4 is invalid!") { calculator.sumUntil(4) }
+            }
+
         }
 
     }
