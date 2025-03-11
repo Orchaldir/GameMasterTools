1b9dacd78219326961346a9e1716098abe8be1a7
Orchaldir <orchaldir@gmail.com>
2025-03-11 11:25:53 +0100

    Add tests

--- src/test/kotlin/at/orchaldir/gm/core/selector/time/DisplayTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/time/DisplayTest.kt
@@ -6,6 +6,7 @@ import at.orchaldir.gm.core.model.time.DisplayDecade
 import at.orchaldir.gm.core.model.time.DisplayYear
 import at.orchaldir.gm.core.model.time.calendar.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class DisplayTest {
@@ -17,93 +18,101 @@ class DisplayTest {
     private val format0 = DateFormat()
     private val format1 = DateFormat(DateOrder.YearMonthDay, '/', true)
 
-    @Test
-    fun `Test a day in AD`() {
-        val date = DisplayDay(1, 2023, 1, 2)
+    @Nested
+    inner class AdTest {
 
-        assertDisplay(format0, date, "3.2.2024 AD")
-        assertDisplay(format1, date, "2024/Second/3 AD")
+        @Test
+        fun `Test a day in AD`() {
+            val date = DisplayDay(1, 2023, 1, 2)
+
+            assertDisplay(format0, date, "3.2.2024 AD")
+            assertDisplay(format1, date, "2024/Second/3 AD")
+        }
+
+        @Test
+        fun `Test a year in AD`() {
+            val date = DisplayYear(1, 2023)
+
+            assertDisplay(format0, date, "2024 AD")
+            assertDisplay(format1, date, "2024 AD")
+        }
+
+        @Test
+        fun `Test a decade in AD`() {
+            val date = DisplayDecade(1, 202)
+
+            assertDisplay(format0, date, "2020s AD")
+            assertDisplay(format1, date, "2020s AD")
+        }
+
+        @Test
+        fun `Test the first day in AD`() {
+            val date = DisplayDay(1, 0, 0, 0)
+
+            assertDisplay(format0, date, "1.1.1 AD")
+            assertDisplay(format1, date, "1/First/1 AD")
+        }
+
+        @Test
+        fun `Test the first year in AD`() {
+            val date = DisplayYear(1, 0)
+
+            assertDisplay(format0, date, "1 AD")
+            assertDisplay(format1, date, "1 AD")
+        }
+
+        @Test
+        fun `Test the first decade in AD`() {
+            // not sure about this
+            val date = DisplayDecade(1, 0)
+
+            assertDisplay(format0, date, "0s AD")
+            assertDisplay(format1, date, "0s AD")
+        }
+
+        @Test
+        fun `Test a single digit decade in AD`() {
+            val date = DisplayDecade(1, 5)
+
+            assertDisplay(format0, date, "50s AD")
+            assertDisplay(format1, date, "50s AD")
+        }
     }
 
-    @Test
-    fun `Test a year in AD`() {
-        val date = DisplayYear(1, 2023)
+    @Nested
+    inner class BcTest {
 
-        assertDisplay(format0, date, "2024 AD")
-        assertDisplay(format1, date, "2024 AD")
-    }
+        @Test
+        fun `Test a day in BC`() {
+            val date = DisplayDay(0, 101, 2, 3)
 
-    @Test
-    fun `Test a decade in AD`() {
-        val date = DisplayDecade(1, 202)
+            assertDisplay(format0, date, "BC 4.3.102")
+            assertDisplay(format1, date, "BC 102/Third/4")
+        }
 
-        assertDisplay(format0, date, "2020s AD")
-        assertDisplay(format1, date, "2020s AD")
-    }
+        @Test
+        fun `Test a year in BC`() {
+            val date = DisplayYear(0, 1234)
 
-    @Test
-    fun `Test the first day in AD`() {
-        val date = DisplayDay(1, 0, 0, 0)
+            assertDisplay(format0, date, "BC 1235")
+            assertDisplay(format1, date, "BC 1235")
+        }
 
-        assertDisplay(format0, date, "1.1.1 AD")
-        assertDisplay(format1, date, "1/First/1 AD")
-    }
+        @Test
+        fun `Test a decade in BC`() {
+            val date = DisplayDecade(0, 11)
 
-    @Test
-    fun `Test the first year in AD`() {
-        val date = DisplayYear(1, 0)
+            assertDisplay(format0, date, "BC 110s")
+            assertDisplay(format1, date, "BC 110s")
+        }
 
-        assertDisplay(format0, date, "1 AD")
-        assertDisplay(format1, date, "1 AD")
-    }
+        @Test
+        fun `Test the first decade in BC`() {
+            val date = DisplayDecade(0, 0)
 
-    @Test
-    fun `Test the first decade in AD`() {
-        // not sure about this
-        val date = DisplayDecade(1, 0)
-
-        assertDisplay(format0, date, "0s AD")
-        assertDisplay(format1, date, "0s AD")
-    }
-
-    @Test
-    fun `Test a single digit decade in AD`() {
-        val date = DisplayDecade(1, 5)
-
-        assertDisplay(format0, date, "50s AD")
-        assertDisplay(format1, date, "50s AD")
-    }
-
-    @Test
-    fun `Test a day in BC`() {
-        val date = DisplayDay(0, 101, 2, 3)
-
-        assertDisplay(format0, date, "BC 4.3.102")
-        assertDisplay(format1, date, "BC 102/Third/4")
-    }
-
-    @Test
-    fun `Test a year in BC`() {
-        val date = DisplayYear(0, 1234)
-
-        assertDisplay(format0, date, "BC 1235")
-        assertDisplay(format1, date, "BC 1235")
-    }
-
-    @Test
-    fun `Test a decade in BC`() {
-        val date = DisplayDecade(0, 11)
-
-        assertDisplay(format0, date, "BC 110s")
-        assertDisplay(format1, date, "BC 110s")
-    }
-
-    @Test
-    fun `Test the first decade in BC`() {
-        val date = DisplayDecade(0, 0)
-
-        assertDisplay(format0, date, "BC 0s")
-        assertDisplay(format1, date, "BC 0s")
+            assertDisplay(format0, date, "BC 0s")
+            assertDisplay(format1, date, "BC 0s")
+        }
     }
 
     private fun assertDisplay(format: DateFormat, date: DisplayDate, result: String) {
