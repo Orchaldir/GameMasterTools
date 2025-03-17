cc43cf6ef58844c6afb8a5a3b0c19cce2595f2a4
Orchaldir <orchaldir@gmail.com>
2025-03-17 21:02:30 +0100

    belt holes UI

--- src/main/kotlin/at/orchaldir/gm/app/html/model/item/equipment/Belt.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/item/equipment/Belt.kt
@@ -2,19 +2,17 @@ package at.orchaldir.gm.app.html.model.item.equipment
 
 import at.orchaldir.gm.app.*
 import at.orchaldir.gm.app.html.*
-import at.orchaldir.gm.app.html.model.selectWeight
 import at.orchaldir.gm.app.parse.combine
 import at.orchaldir.gm.app.parse.parse
 import at.orchaldir.gm.app.parse.parseMaterialId
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.item.equipment.*
 import at.orchaldir.gm.core.model.item.equipment.style.*
+import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.core.model.util.Size
 import at.orchaldir.gm.utils.doNothing
-import at.orchaldir.gm.utils.math.unit.Weight
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.util.*
 import kotlinx.html.*
 
 // show
@@ -27,9 +25,10 @@ fun BODY.showBelt(
     showBuckle(call, state, belt.buckle)
     showFill(belt.fill)
     fieldLink("Material", call, state, belt.material)
+    showBeltHoles(belt.holes)
 }
 
-fun BODY.showBuckle(
+private fun BODY.showBuckle(
     call: ApplicationCall,
     state: State,
     buckle: Buckle,
@@ -47,6 +46,25 @@ fun BODY.showBuckle(
     }
 }
 
+private fun BODY.showBeltHoles(
+    holes: BeltHoles,
+) {
+    showDetails("Belt Holes", true) {
+        field("Type", holes.getType())
+
+        when (holes) {
+            NoBeltHoles -> doNothing()
+            is OneRowOfBeltHoles -> {
+                field("Size", holes.size)
+                optionalField("Border Color", holes.border)
+            }
+
+            is ThreeRowsOfBeltHoles -> optionalField("Border Color", holes.border)
+            is TwoRowsOfBeltHoles -> optionalField("Border Color", holes.border)
+        }
+    }
+}
+
 // edit
 
 fun FORM.editBelt(
@@ -56,14 +74,15 @@ fun FORM.editBelt(
     editBuckle(state, belt.buckle)
     selectFill(belt.fill)
     selectMaterial(state, belt.material)
+    editBeltHoles(belt.holes)
 }
 
-fun FORM.editBuckle(
+private fun FORM.editBuckle(
     state: State,
     buckle: Buckle,
 ) {
     showDetails("Buckle", true) {
-        selectValue("Buckle Type", combine(BUCKLE, TYPE), BuckleType.entries, buckle.getType(), true)
+        selectValue("Type", combine(BUCKLE, TYPE), BuckleType.entries, buckle.getType(), true)
 
         when (buckle) {
             NoBuckle -> doNothing()
@@ -77,12 +96,36 @@ fun FORM.editBuckle(
     }
 }
 
+private fun FORM.editBeltHoles(
+    holes: BeltHoles,
+) {
+    showDetails("Belt Holes", true) {
+        selectValue("Type", combine(HOLE, TYPE), BeltHolesType.entries, holes.getType(), true)
+
+        when (holes) {
+            NoBeltHoles -> doNothing()
+            is OneRowOfBeltHoles -> {
+                selectValue("Size", combine(HOLE, SIZE), Size.entries, holes.size, true)
+                selectBorderColor(holes.border)
+            }
+
+            is ThreeRowsOfBeltHoles -> selectBorderColor(holes.border)
+            is TwoRowsOfBeltHoles -> selectBorderColor(holes.border)
+        }
+    }
+}
+
+private fun DETAILS.selectBorderColor(color: Color?) {
+    selectOptionalColor("Border Color", combine(HOLE, COLOR), color, Color.entries, true)
+}
+
 // parse
 
 fun parseBelt(parameters: Parameters) = Belt(
     parseBuckle(parameters),
     parseFill(parameters),
     parseMaterialId(parameters, MATERIAL),
+    parseBeltHoles(parameters),
 )
 
 private fun parseBuckle(parameters: Parameters): Buckle {
@@ -98,3 +141,21 @@ private fun parseBuckle(parameters: Parameters): Buckle {
         )
     }
 }
+
+private fun parseBeltHoles(parameters: Parameters): BeltHoles {
+    val type = parse(parameters, combine(HOLE, TYPE), BeltHolesType.NoBeltHoles)
+
+    return when (type) {
+        BeltHolesType.NoBeltHoles -> NoBeltHoles
+        BeltHolesType.OneRow -> OneRowOfBeltHoles(
+            parse(parameters, combine(HOLE, SIZE), Size.Small),
+            parseBorderColor(parameters),
+        )
+
+        BeltHolesType.TwoRows -> TwoRowsOfBeltHoles(parseBorderColor(parameters))
+        BeltHolesType.ThreeRows -> ThreeRowsOfBeltHoles(parseBorderColor(parameters))
+    }
+}
+
+private fun parseBorderColor(parameters: Parameters) =
+    parse<Color>(parameters, combine(HOLE, COLOR))
