048b21cab88e21bd763e6b7bf89520bc280fe9a0
Orchaldir <orchaldir@gmail.com>
2025-03-18 20:56:43 +0100

    Add visualizeHawk()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Beak.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Beak.kt
@@ -2,26 +2,19 @@ package at.orchaldir.gm.visualization.character.appearance.mouth
 
 import at.orchaldir.gm.core.model.character.appearance.*
 import at.orchaldir.gm.core.model.character.appearance.mouth.*
-import at.orchaldir.gm.core.model.util.Color
-import at.orchaldir.gm.core.model.util.Size
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.*
-import at.orchaldir.gm.utils.renderer.model.FillAndBorder
-import at.orchaldir.gm.utils.renderer.model.NoBorder
-import at.orchaldir.gm.visualization.SizeConfig
-import at.orchaldir.gm.visualization.character.CharacterRenderConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderState
-import at.orchaldir.gm.visualization.character.appearance.beard.visualizeBeard
 
 fun visualizeBeak(state: CharacterRenderState, beak: Beak) = when (beak.shape) {
-    BeakShape.Crow -> visualizeCrowBeak(state, beak)
+    BeakShape.Crow -> visualizeCrow(state, beak)
     BeakShape.Duck -> doNothing()
-    BeakShape.Hawk -> doNothing()
+    BeakShape.Hawk -> visualizeHawk(state, beak)
     BeakShape.Owl -> doNothing()
     BeakShape.Parrot -> doNothing()
 }
 
-private fun visualizeCrowBeak(state: CharacterRenderState, beak: Beak) {
+private fun visualizeCrow(state: CharacterRenderState, beak: Beak) {
     val y = state.config.head.mouth.y
     val half = Factor(0.3f)
     val width = Factor(0.45f)
@@ -35,6 +28,29 @@ private fun visualizeCrowBeak(state: CharacterRenderState, beak: Beak) {
         .addMirroredPoints(state.aabb, width, y, true)
         .addMirroredPoints(state.aabb, width / 3.0f, y)
         .addLeftPoint(state.aabb, CENTER, y + half * 0.5f)
+        .build()
+
+    state.renderer.getLayer()
+        .renderRoundedPolygon(lowerPolygon, options)
+    state.renderer.getLayer()
+        .renderRoundedPolygon(upperPolygon, options)
+}
+
+private fun visualizeHawk(state: CharacterRenderState, beak: Beak) {
+    val y = state.config.head.mouth.y
+    val upperHeight = Factor(0.4f)
+    val lowerHeight = Factor(0.3f)
+    val width = Factor(0.35f)
+    val options = state.config.getLineOptions(beak.color)
+    val lowerPolygon = Polygon2dBuilder()
+        .addMirroredPoints(state.aabb, width, y, true)
+        .addMirroredPoints(state.aabb, width / 3.0f, y + lowerHeight * 0.5f)
+        .build()
+    val upperPolygon = Polygon2dBuilder()
+        .addLeftPoint(state.aabb, CENTER, y - upperHeight)
+        .addMirroredPoints(state.aabb, width, y, true)
+        .addMirroredPoints(state.aabb, width / 3.0f, y)
+        .addLeftPoint(state.aabb, CENTER, y + lowerHeight, true)
         .build()
 
     state.renderer.getLayer()
