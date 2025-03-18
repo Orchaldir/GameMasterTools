b4cccfeb0480fa6ec1df994e02c287eb3903972e
Orchaldir <orchaldir@gmail.com>
2025-03-18 20:40:52 +0100

    Improve visualizeCrowBeak()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Beak.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Beak.kt
@@ -6,6 +6,7 @@ import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.core.model.util.Size
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.*
+import at.orchaldir.gm.utils.renderer.model.FillAndBorder
 import at.orchaldir.gm.utils.renderer.model.NoBorder
 import at.orchaldir.gm.visualization.SizeConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderConfig
@@ -21,5 +22,22 @@ fun visualizeBeak(state: CharacterRenderState, beak: Beak) = when (beak.shape) {
 }
 
 private fun visualizeCrowBeak(state: CharacterRenderState, beak: Beak) {
+    val y = state.config.head.mouth.y
+    val half = Factor(0.2f)
+    val options = state.config.getLineOptions(beak.color)
+    val lowerPolygon = Polygon2dBuilder()
+        .addMirroredPoints(state.aabb, Factor(0.3f), y, true)
+        .addMirroredPoints(state.aabb, Factor(0.1f), y + half * 0.6f)
+        .build()
+    val upperPolygon = Polygon2dBuilder()
+        .addLeftPoint(state.aabb, CENTER, y - half)
+        .addMirroredPoints(state.aabb, Factor(0.3f), y, true)
+        .addMirroredPoints(state.aabb, Factor(0.1f), y)
+        .addLeftPoint(state.aabb, CENTER, y + half * 0.5f)
+        .build()
 
+    state.renderer.getLayer()
+        .renderRoundedPolygon(lowerPolygon, options)
+    state.renderer.getLayer()
+        .renderRoundedPolygon(upperPolygon, options)
 }
