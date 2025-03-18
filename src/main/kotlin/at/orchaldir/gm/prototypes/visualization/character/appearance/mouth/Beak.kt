912401821a3771b69903c859eac38149104d04a1
Orchaldir <orchaldir@gmail.com>
2025-03-18 16:50:47 +0100

    Add example

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/appearance/mouth/Beak.kt
@@ -0,0 +1,34 @@
+package at.orchaldir.gm.prototypes.visualization.character.appearance.mouth
+
+import at.orchaldir.gm.core.model.character.appearance.Appearance
+import at.orchaldir.gm.core.model.character.appearance.Head
+import at.orchaldir.gm.core.model.character.appearance.HeadOnly
+import at.orchaldir.gm.core.model.character.appearance.eye.*
+import at.orchaldir.gm.core.model.character.appearance.mouth.Beak
+import at.orchaldir.gm.core.model.character.appearance.mouth.BeakShape
+import at.orchaldir.gm.core.model.util.Size
+import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
+import at.orchaldir.gm.utils.math.unit.Distance
+
+fun main() {
+    val appearances = mutableListOf<List<Appearance>>()
+
+    BeakShape.entries.forEach { shape ->
+        appearances.add(createRow(shape))
+    }
+
+    renderCharacterTable("beaks.svg", CHARACTER_CONFIG, appearances)
+}
+
+
+private fun createRow(beakShape: BeakShape) =
+    listOf(OneEye(size = Size.Small), OneEye(size = Size.Medium), OneEye(size = Size.Large), TwoEyes())
+        .map { createAppearance(beakShape, it) }
+
+
+private fun createAppearance(beakShape: BeakShape, eyes: Eyes) =
+    HeadOnly(
+        Head(eyes = eyes, mouth = Beak(beakShape)),
+        Distance(1000),
+    )
