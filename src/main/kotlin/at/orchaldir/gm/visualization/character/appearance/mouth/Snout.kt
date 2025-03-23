efa0af22a17a8a841e9522a36f2e1fc8cb7b7f2e
Orchaldir <orchaldir@gmail.com>
2025-03-23 15:03:59 +0100

    Add visualizeCow()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Snout.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/mouth/Snout.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.visualization.character.appearance.mouth
 import at.orchaldir.gm.core.model.character.appearance.mouth.Snout
 import at.orchaldir.gm.core.model.character.appearance.mouth.SnoutShape
 import at.orchaldir.gm.utils.doNothing
+import at.orchaldir.gm.utils.math.CENTER
 import at.orchaldir.gm.utils.math.Factor
 import at.orchaldir.gm.utils.math.Polygon2dBuilder
 import at.orchaldir.gm.visualization.character.CharacterRenderState
@@ -22,9 +23,15 @@ fun visualizeSnout(state: CharacterRenderState, snout: Snout) {
 
 private fun visualizeCow(state: CharacterRenderState, snout: Snout) {
     val options = state.config.getLineOptions(snout.color)
+    val upperY = Factor(0.6f)
+    val lowerY = Factor(1.1f)
+    val width = Factor(1.1f)
     val polygon = Polygon2dBuilder()
-        .addMirroredPoints(state.aabb, Factor(1.1f), Factor(0.5f))
-        .addMirroredPoints(state.aabb, Factor(1.1f), Factor(1.0f))
+        .addLeftPoint(state.aabb, CENTER, upperY)
+        .addMirroredPoints(state.aabb, width, upperY)
+        .addMirroredPoints(state.aabb, width, (upperY + lowerY) / 2.0f)
+        .addMirroredPoints(state.aabb, width, lowerY)
+        .addLeftPoint(state.aabb, CENTER, lowerY)
         .build()
     state.renderer.getLayer()
         .renderRoundedPolygon(polygon, options)
