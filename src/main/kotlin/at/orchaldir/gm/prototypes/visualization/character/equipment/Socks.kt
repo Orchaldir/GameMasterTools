e37e039da68a83924613f318cd247e2109a0db30
Orchaldir <orchaldir@gmail.com>
2025-03-25 19:06:15 +0100

    Add example

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/Socks.kt
@@ -0,0 +1,29 @@
+package at.orchaldir.gm.prototypes.visualization.character.equipment
+
+import at.orchaldir.gm.core.model.character.appearance.*
+import at.orchaldir.gm.core.model.item.equipment.Socks
+import at.orchaldir.gm.core.model.item.equipment.style.SocksStyle
+import at.orchaldir.gm.core.model.util.Size
+import at.orchaldir.gm.prototypes.visualization.addNames
+import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
+import at.orchaldir.gm.utils.math.unit.Distance
+
+fun main() {
+    renderCharacterTable(
+        "socks.svg",
+        CHARACTER_CONFIG,
+        addNames(listOf(BodyShape.Rectangle)),
+        addNames(SocksStyle.entries),
+        true,
+    ) { distance, style, shape ->
+        Pair(createAppearance(distance, shape), listOf(Socks(style)))
+    }
+}
+
+private fun createAppearance(distance: Distance, shape: BodyShape) =
+    HumanoidBody(
+        Body(shape, NormalFoot, Size.Medium),
+        Head(),
+        distance,
+    )
