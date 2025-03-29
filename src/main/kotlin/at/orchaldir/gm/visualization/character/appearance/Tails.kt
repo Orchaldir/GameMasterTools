c53b1ce77fc6abb07a243326ecdc8a42bff99edd
Orchaldir <orchaldir@gmail.com>
2025-03-29 16:39:02 +0100

    Add visualizeSquirrel()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Tails.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Tails.kt
@@ -7,6 +7,7 @@ import at.orchaldir.gm.core.model.character.appearance.tail.Tails
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.CENTER
 import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.Factor.Companion.fromPercentage
 import at.orchaldir.gm.utils.math.Polygon2dBuilder
 import at.orchaldir.gm.visualization.SizeConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderState
@@ -27,7 +28,7 @@ private fun visualizeSimpleTail(state: CharacterRenderState, tail: SimpleTail) =
     SimpleTailShape.Cat -> doNothing()
     SimpleTailShape.Horse -> visualizeHorse(state, tail)
     SimpleTailShape.Rat -> doNothing()
-    SimpleTailShape.Squirrel -> doNothing()
+    SimpleTailShape.Squirrel -> visualizeSquirrel(state, tail)
 }
 
 private fun visualizeBunny(state: CharacterRenderState, tail: SimpleTail) {
@@ -50,4 +51,26 @@ private fun visualizeHorse(state: CharacterRenderState, tail: SimpleTail) {
         .build()
 
     state.getTailLayer().renderRoundedPolygon(polygon, options)
+}
+
+private fun visualizeSquirrel(state: CharacterRenderState, tail: SimpleTail) {
+    val config = state.config.body.tail
+    val options = state.config.getLineOptions(tail.color)
+    val polygon = Polygon2dBuilder()
+        .addMirroredPoints(state.aabb, fromPercentage(50), fromPercentage(80))
+        .addMirroredPoints(state.aabb, fromPercentage(50), fromPercentage(10))
+        .addLeftPoint(state.aabb, CENTER, fromPercentage(10))
+        .build()
+
+    state.getTailLayer().renderRoundedPolygon(polygon, options)
+
+    if (!state.renderFront) {
+        val backPolygon = Polygon2dBuilder()
+            .addLeftPoint(state.aabb, CENTER, fromPercentage(10))
+            .addMirroredPoints(state.aabb, fromPercentage(40), fromPercentage(10))
+            .addMirroredPoints(state.aabb, fromPercentage(20), fromPercentage(40))
+            .build()
+
+        state.getTailLayer().renderRoundedPolygon(backPolygon, options)
+    }
 }
