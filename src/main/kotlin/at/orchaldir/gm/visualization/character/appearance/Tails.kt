8c130cdd261920efa4439c00faf5af6cf497ed43
Orchaldir <orchaldir@gmail.com>
2025-04-01 19:42:54 +0200

    Add visualizeRat()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Tails.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/Tails.kt
@@ -1,14 +1,19 @@
 package at.orchaldir.gm.visualization.character.appearance
 
+import at.orchaldir.gm.app.COLOR
 import at.orchaldir.gm.core.model.character.appearance.tail.NoTails
 import at.orchaldir.gm.core.model.character.appearance.tail.SimpleTail
 import at.orchaldir.gm.core.model.character.appearance.tail.SimpleTailShape
 import at.orchaldir.gm.core.model.character.appearance.tail.Tails
+import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.CENTER
 import at.orchaldir.gm.utils.math.Factor
 import at.orchaldir.gm.utils.math.Factor.Companion.fromPercentage
+import at.orchaldir.gm.utils.math.Line2dBuilder
 import at.orchaldir.gm.utils.math.Polygon2dBuilder
+import at.orchaldir.gm.utils.math.unit.Distance
+import at.orchaldir.gm.utils.renderer.model.LineOptions
 import at.orchaldir.gm.visualization.SizeConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderState
 
@@ -27,7 +32,7 @@ private fun visualizeSimpleTail(state: CharacterRenderState, tail: SimpleTail) =
     SimpleTailShape.Bunny -> visualizeBunny(state, tail)
     SimpleTailShape.Cat -> doNothing()
     SimpleTailShape.Horse -> visualizeHorse(state, tail)
-    SimpleTailShape.Rat -> doNothing()
+    SimpleTailShape.Rat -> visualizeRat(state, tail)
     SimpleTailShape.Squirrel -> visualizeSquirrel(state, tail)
 }
 
@@ -42,15 +47,30 @@ private fun visualizeBunny(state: CharacterRenderState, tail: SimpleTail) {
 
 private fun visualizeHorse(state: CharacterRenderState, tail: SimpleTail) {
     val config = state.config.body.tail
-    val width = config.bunnySize.convert(tail.size) * 2.0f
+    val radius = config.bunnySize.convert(tail.size)
+    val width = radius * 2.0f
     val options = state.config.getLineOptions(tail.color)
     val polygon = Polygon2dBuilder()
-        .addMirroredPoints(state.aabb, width, config.startY)
+        .addMirroredPoints(state.aabb, width, config.startY - radius)
         .addMirroredPoints(state.aabb, width * 0.9f, config.startY + config.horseLength / 2.0f)
         .addMirroredPoints(state.aabb, width, config.startY + config.horseLength, true)
         .build()
 
     state.getTailLayer().renderRoundedPolygon(polygon, options)
+}
+
+private fun visualizeRat(state: CharacterRenderState, tail: SimpleTail) {
+    val config = state.config.body.tail
+    val line = Line2dBuilder()
+        .addPoint(state.aabb, CENTER, config.startY)
+        .addPoint(state.aabb, fromPercentage(30), config.startY)
+        .addPoint(state.aabb, fromPercentage(30), config.startY + fromPercentage(30))
+        .addPoint(state.aabb, fromPercentage(70), config.startY + fromPercentage(30))
+        .addPoint(state.aabb, fromPercentage(70), config.startY - fromPercentage(20))
+        .addPoint(state.aabb, fromPercentage(90), config.startY - fromPercentage(20))
+        .build()
+
+    state.getTailLayer().renderLine(line.points, LineOptions(Color.Red.toRender(), Distance.fromMillimeters(10)))
 }
 
 private fun visualizeSquirrel(state: CharacterRenderState, tail: SimpleTail) {
