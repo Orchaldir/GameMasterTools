976896fee452ca38f4039f795c9bed40f16ee6f1
Orchaldir <orchaldir@gmail.com>
2025-04-02 08:35:20 +0200

    Test subdivideLine()

---
+++ src/test/kotlin/at/orchaldir/gm/utils/math/SubdivisionTest.kt
@@ -0,0 +1,43 @@
+package at.orchaldir.gm.utils.math
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SubdivisionTest {
+
+    @Test
+    fun `Subdividing an empty list returns the input`() {
+        val noPoints = emptyList<Point2d>()
+
+        assertEquals(noPoints, subdivideLine(noPoints, 1))
+    }
+
+    @Test
+    fun `Subdividing a single point returns the input`() {
+        val point = listOf(Point2d())
+
+        assertEquals(point, subdivideLine(point, 1))
+    }
+
+    @Test
+    fun `Subdividing two points returns the input`() {
+        val twoPoints = listOf(Point2d(), Point2d(1.0f, 2.0f))
+
+        assertEquals(twoPoints, subdivideLine(twoPoints, 1))
+    }
+
+    @Test
+    fun `Subdivide a list of points`() {
+        val input = listOf(Point2d(), Point2d(3.0f, 0.0f), Point2d(3.0f, 3.0f))
+        val output = listOf(
+            Point2d(),
+            Point2d(1.0f, 0.0f),
+            Point2d(2.0f, 0.0f),
+            Point2d(3.0f, 1.0f),
+            Point2d(3.0f, 2.0f),
+            Point2d(3.0f, 3.0f),
+        )
+
+        assertEquals(output, subdivideLine(input, 1))
+    }
+}
