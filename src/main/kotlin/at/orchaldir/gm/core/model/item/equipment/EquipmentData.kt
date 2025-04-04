f9ba10c966bf0e4e4b1c17607cb847362b4497b0
Orchaldir <orchaldir@gmail.com>
2025-04-04 12:18:10 +0200

    Add Earring

--- src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentData.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentData.kt
@@ -16,6 +16,7 @@ enum class EquipmentDataType {
     Belt,
     Coat,
     Dress,
+    Earring,
     Footwear,
     Glasses,
     Gloves,
@@ -31,6 +32,7 @@ enum class EquipmentDataType {
         Belt -> setOf(BeltSlot)
         Coat -> setOf(OuterSlot)
         Dress -> setOf(BottomSlot, TopSlot)
+        Earring -> setOf(EarSlot)
         Footwear -> setOf(FootSlot)
         Glasses -> setOf(EyeSlot)
         Gloves -> setOf(HandSlot)
@@ -53,6 +55,7 @@ sealed class EquipmentData {
         is Belt -> EquipmentDataType.Belt
         is Coat -> EquipmentDataType.Coat
         is Dress -> EquipmentDataType.Dress
+        is Earring -> EquipmentDataType.Earring
         is Footwear -> EquipmentDataType.Footwear
         is Glasses -> EquipmentDataType.Glasses
         is Gloves -> EquipmentDataType.Gloves
@@ -110,6 +113,17 @@ data class Dress(
     val skirtStyle: SkirtStyle = SkirtStyle.Sheath,
     val sleeveStyle: SleeveStyle = SleeveStyle.Long,
     val fill: Fill = Solid(Color.SaddleBrown),
+    val material: MaterialId = MaterialId(0),
+) : EquipmentData() {
+
+    override fun contains(id: MaterialId) = material == id
+    override fun getMaterials() = setOf(material)
+}
+
+@Serializable
+@SerialName("Earring")
+data class Earring(
+    val color: Color = Color.Gold,
     val material: MaterialId = MaterialId(0),
 ) : EquipmentData() {
 
