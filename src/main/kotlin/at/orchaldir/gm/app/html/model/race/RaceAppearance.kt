d0018daa1c09261fec22d754b373e375d4429910
Orchaldir <orchaldir@gmail.com>
2025-04-05 19:58:47 +0200

    Fix

--- src/main/kotlin/at/orchaldir/gm/app/html/model/race/RaceAppearance.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/race/RaceAppearance.kt
@@ -529,7 +529,7 @@ private fun parseMouthOptions(parameters: Parameters) = MouthOptions(
 )
 
 private fun parseSkinOptions(parameters: Parameters, param: String) = SkinOptions(
-    parseOneOf(parameters, combine(param, TYPE), SkinType::valueOf),
+    parseOneOf(parameters, combine(param, TYPE), SkinType::valueOf, SkinType.entries),
     parseOneOf(parameters, combine(param, FUR, COLOR), Color::valueOf, Color.entries),
     parseOneOf(parameters, combine(param, SCALE, COLOR), Color::valueOf, Color.entries),
     parseOneOf(parameters, combine(param, NORMAL, COLOR), SkinColor::valueOf, SkinColor.entries),
