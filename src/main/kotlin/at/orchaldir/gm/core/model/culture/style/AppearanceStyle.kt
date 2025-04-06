c4d4447c94beafcf6af9b116fabcd642bd6f9fce
Orchaldir <orchaldir@gmail.com>
2025-04-06 18:31:42 +0200

    Update data

--- src/main/kotlin/at/orchaldir/gm/core/model/culture/style/AppearanceStyle.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/culture/style/AppearanceStyle.kt
@@ -16,9 +16,9 @@ data class AppearanceStyle(
     val beardStyles: OneOf<BeardStyleType> = OneOf(BeardStyleType.GoateeAndMoustache),
     val goateeStyles: OneOf<GoateeStyle> = OneOf(GoateeStyle.Goatee),
     val moustacheStyles: OneOf<MoustacheStyle> = OneOf(MoustacheStyle.Handlebar),
-    val hairStyles: OneOf<HairStyle> = OneOf(HairStyle.Short),
-    val shortHairStyles: OneOf<ShortHairStyle> = OneOf(ShortHairStyle.MiddlePart),
-    val longHairStyles: OneOf<LongHairStyle> = OneOf(LongHairStyle.Straight),
+    val hairStyles: OneOf<HairStyle> = OneOf(HairStyle.entries),
+    val shortHairStyles: OneOf<ShortHairStyle> = OneOf(ShortHairStyle.entries),
+    val longHairStyles: OneOf<LongHairStyle> = OneOf(LongHairStyle.entries),
     val hairLengths: OneOf<HairLength> = OneOf(HairLength.entries),
     val lipColors: OneOf<Color> = OneOf(Color.Black),
 )
