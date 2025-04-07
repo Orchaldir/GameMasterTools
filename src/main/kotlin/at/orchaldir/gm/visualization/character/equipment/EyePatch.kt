b867c2ab82adf77f9f0de31f38baa9ceb5753c7e
Orchaldir <orchaldir@gmail.com>
2025-04-07 11:26:23 +0200

    Handle OneBand

--- src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/EyePatch.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/EyePatch.kt
@@ -8,9 +8,7 @@ import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.core.model.util.Side
 import at.orchaldir.gm.core.model.util.Size
 import at.orchaldir.gm.utils.doNothing
-import at.orchaldir.gm.utils.math.AABB
-import at.orchaldir.gm.utils.math.FULL
-import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.*
 import at.orchaldir.gm.utils.renderer.model.LineOptions
 import at.orchaldir.gm.visualization.SizeConfig
 import at.orchaldir.gm.visualization.character.CharacterRenderState
@@ -47,17 +45,26 @@ fun visualizeEyePatchForTwoEyes(
         .flip()
         .get(eyesConfig.getTwoEyesCenter(state.aabb))
     val renderer = state.getLayer(EQUIPMENT_LAYER)
+    val offset = state.aabb.convertHeight(eyePatchConfig.fixationDeltaY) / 2.0f
 
     when (eyePatch.fixation) {
         NoFixation -> doNothing()
-        is OneBand -> doNothing()
+        is OneBand -> {
+            val xPair = Pair(START, END)
+            val closeEnd = state.aabb.getPoint(side.flip().get(xPair), eyesConfig.twoEyesY)
+            val distantEnd = state.aabb.getPoint(side.get(xPair), eyesConfig.twoEyesY - eyePatchConfig.fixationDeltaY)
+            val options = eyePatchConfig.getFixationOptions(state.aabb, eyePatch.fixation.color, eyePatch.fixation.size)
+
+            renderer.renderLine(listOf(closeEnd, center), options)
+            renderer.renderLine(listOf(distantEnd, center), options)
+        }
         is DiagonalBand -> doNothing()
         is TwoBands -> {
             val (topLeft, topRight) = state.aabb
                 .getMirroredPoints(FULL, eyesConfig.twoEyesY - eyePatchConfig.fixationDeltaY)
             val (bottomLeft, bottomRight) = state.aabb
                 .getMirroredPoints(FULL, eyesConfig.twoEyesY + eyePatchConfig.fixationDeltaY)
-            val offset = state.aabb.convertHeight(eyePatchConfig.fixationDeltaY) / 2.0f
+
             val options = eyePatchConfig.getFixationOptions(state.aabb, eyePatch.fixation.color, Size.Small)
 
             renderer.renderLine(listOf(topLeft, center.minusHeight(offset), topRight), options)
