305ba25e7e85267a38635dd348155de2b4279fd0
Orchaldir <orchaldir@gmail.com>
2025-04-07 12:47:55 +0200

    Add example eyepatch-eye.svg

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/eyepatch/EyePatchWithEye.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/eyepatch/EyePatchWithEye.kt
@@ -2,13 +2,17 @@ package at.orchaldir.gm.prototypes.visualization.character.equipment.eyepatch
 
 import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.HeadOnly
+import at.orchaldir.gm.core.model.character.appearance.eye.EyeShape
+import at.orchaldir.gm.core.model.character.appearance.eye.EyeShape.Circle
 import at.orchaldir.gm.core.model.character.appearance.eye.NormalEye
+import at.orchaldir.gm.core.model.character.appearance.eye.PupilShape
+import at.orchaldir.gm.core.model.character.appearance.eye.PupilShape.VerticalSlit
 import at.orchaldir.gm.core.model.character.appearance.eye.TwoEyes
 import at.orchaldir.gm.core.model.character.appearance.mouth.NormalMouth
 import at.orchaldir.gm.core.model.item.equipment.EyePatch
 import at.orchaldir.gm.core.model.item.equipment.style.*
 import at.orchaldir.gm.core.model.util.Color
-import at.orchaldir.gm.core.model.util.Color.SaddleBrown
+import at.orchaldir.gm.core.model.util.Color.*
 import at.orchaldir.gm.core.model.util.Size.*
 import at.orchaldir.gm.prototypes.visualization.addNames
 import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
@@ -19,11 +23,11 @@ fun main() {
     renderCharacterTable(
         "eyepatch-eye.svg",
         CHARACTER_CONFIG,
+        addNames(EyeShape.entries),
         addNames(VALID_LENSES),
-        FIXATIONS,
-        true,
-    ) { distance, fixation, shape ->
-        Pair(createAppearance(distance), listOf(EyePatch(EyePatchWithEye(NormalEye(), shape), fixation)))
+    ) { distance, shape, eyeShape ->
+        val eye = NormalEye(eyeShape, VerticalSlit, Red, Yellow)
+        Pair(createAppearance(distance), listOf(EyePatch(EyePatchWithEye(eye, shape), OneBand())))
     }
 }
 
