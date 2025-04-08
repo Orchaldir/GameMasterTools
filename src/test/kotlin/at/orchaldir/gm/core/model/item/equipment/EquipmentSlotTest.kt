68914854e63146748c5aa8d3275278e0ef731379
Orchaldir <orchaldir@gmail.com>
2025-04-08 10:37:13 +0200

    Add getAllBodySlotCombinations()

--- src/test/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentSlotTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentSlotTest.kt
@@ -11,7 +11,42 @@ class EquipmentSlotTest {
 
         @Test
         fun `An empty input results in an empty output`() {
-            assertEquals(emptySet<Set<BodySlot>>(), emptySet<EquipmentSlot>().getAllBodySlotCombinations())
+            assertEquals(
+                emptySet<Set<BodySlot>>(),
+                emptySet<EquipmentSlot>().getAllBodySlotCombinations()
+            )
+        }
+
+        @Test
+        fun `A single equipment slot that is a single body slot`() {
+            assertEquals(
+                setOf(setOf(BodySlot.BeltSlot)),
+                setOf(EquipmentSlot.BeltSlot).getAllBodySlotCombinations()
+            )
+        }
+
+        @Test
+        fun `A single equipment slot that is a set of two body slots`() {
+            assertEquals(
+                setOf(setOf(BodySlot.EyeSlotLeft, BodySlot.EyeSlotRight)),
+                setOf(EquipmentSlot.EyesSlot).getAllBodySlotCombinations()
+            )
+        }
+
+        @Test
+        fun `A single equipment slot that is two sets of 1 body slot`() {
+            assertEquals(
+                setOf(setOf(BodySlot.EarSlotLeft), setOf(BodySlot.EarSlotRight)),
+                setOf(EquipmentSlot.EarSlot).getAllBodySlotCombinations()
+            )
+        }
+
+        @Test
+        fun `Two equipment slots that are a set of two body slots`() {
+            assertEquals(
+                setOf(setOf(BodySlot.TopSlot, BodySlot.BottomSlot)),
+                setOf(EquipmentSlot.TopSlot, EquipmentSlot.BottomSlot).getAllBodySlotCombinations()
+            )
         }
     }
 }
