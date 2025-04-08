9ac91e23081cf7a218180007fa3ffef6cc98b3aa
Orchaldir <orchaldir@gmail.com>
2025-04-08 11:35:15 +0200

    Fix update

--- src/main/kotlin/at/orchaldir/gm/core/reducer/character/Equipment.kt
+++ src/main/kotlin/at/orchaldir/gm/core/reducer/character/Equipment.kt
@@ -2,25 +2,10 @@ package at.orchaldir.gm.core.reducer.character
 
 import at.orchaldir.gm.core.action.UpdateEquipmentOfCharacter
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.item.equipment.EquipmentSlot
 import at.orchaldir.gm.utils.redux.Reducer
 import at.orchaldir.gm.utils.redux.noFollowUps
 
 val UPDATE_EQUIPMENT: Reducer<UpdateEquipmentOfCharacter, State> = { state, action ->
-    val occupySlots = mutableSetOf<EquipmentSlot>()
-
-    action.map.map.forEach { (type, id) ->
-        val template = state.getEquipmentStorage().getOrThrow(id)
-        val slots = template.slots()
-
-        // TODO
-        //require(type == template.data.getType()) { "Item template ${template.id.value} has wrong type!" }
-
-        slots.forEach { slot -> require(!occupySlots.contains(slot)) { "An Equipment slot $slot is occupied multiple times" } }
-
-        occupySlots.addAll(slots)
-    }
-
     val character = state.getCharacterStorage().getOrThrow(action.id)
     val updated = character.copy(equipmentMap = action.map)
 
