19fcff9d4f403c3009fcc01fb2259ce270f5f7db
Orchaldir <orchaldir@gmail.com>
2025-04-08 10:03:34 +0200

    Add BodySlot

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/BodySlot.kt
@@ -0,0 +1,17 @@
+package at.orchaldir.gm.core.model.item.equipment
+
+enum class BodySlot {
+    BeltSlot,
+    BottomSlot,
+    EarSlotLeft,
+    EarSlotRight,
+    EyeSlotLeft,
+    EyeSlotRight,
+    FootSlot,
+    FootUnderwearSlot,
+    HandSlot,
+    HeadSlot,
+    OuterSlot,
+    TieSlot,
+    TopSlot,
+}
--- src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentSlot.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentSlot.kt
@@ -12,5 +12,20 @@ enum class EquipmentSlot {
     HeadSlot,
     OuterSlot,
     TieSlot,
-    TopSlot,
+    TopSlot;
+
+    fun toBodySlots() = when (this) {
+        BeltSlot -> setOf(setOf(BodySlot.BeltSlot))
+        BottomSlot -> setOf(setOf(BodySlot.BottomSlot))
+        EarSlot -> setOf(setOf(BodySlot.EarSlotLeft), setOf(BodySlot.EarSlotRight))
+        EyeSlot -> setOf(setOf(BodySlot.EyeSlotLeft), setOf(BodySlot.EyeSlotRight))
+        EyesSlot -> setOf(setOf(BodySlot.EyeSlotLeft, BodySlot.EyeSlotRight))
+        FootSlot -> setOf(setOf(BodySlot.FootSlot))
+        FootUnderwearSlot -> setOf(setOf(BodySlot.FootUnderwearSlot))
+        HandSlot -> setOf(setOf(BodySlot.HandSlot))
+        HeadSlot -> setOf(setOf(BodySlot.HeadSlot))
+        OuterSlot -> setOf(setOf(BodySlot.OuterSlot))
+        TieSlot -> setOf(setOf(BodySlot.TieSlot))
+        TopSlot -> setOf(setOf(BodySlot.TopSlot))
+    }
 }
