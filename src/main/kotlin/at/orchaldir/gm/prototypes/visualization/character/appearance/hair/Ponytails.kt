b69a2aa633671431af90fab1e1e1663e832b55a6
Orchaldir <orchaldir@gmail.com>
2025-04-09 11:22:19 +0200

    Add hair-ponytail.svg

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/appearance/hair/Ponytails.kt
@@ -0,0 +1,38 @@
+package at.orchaldir.gm.prototypes.visualization.character.appearance.hair
+
+import at.orchaldir.gm.core.model.character.EquipmentMap
+import at.orchaldir.gm.core.model.character.appearance.*
+import at.orchaldir.gm.core.model.character.appearance.eye.TwoEyes
+import at.orchaldir.gm.core.model.character.appearance.hair.*
+import at.orchaldir.gm.core.model.character.appearance.horn.NoHorns
+import at.orchaldir.gm.core.model.character.appearance.mouth.FemaleMouth
+import at.orchaldir.gm.core.model.util.Color
+import at.orchaldir.gm.prototypes.visualization.addNames
+import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
+import at.orchaldir.gm.utils.math.unit.Distance
+
+fun main() {
+    renderCharacterTable(
+        "hair-ponytail.svg",
+        CHARACTER_CONFIG,
+        addNames(PonytailStyle.entries),
+        addNames(PonytailPosition.entries),
+        true,
+    ) { distance, position, style ->
+        Pair(createAppearance(distance, position, style), EquipmentMap())
+    }
+}
+
+private fun createAppearance(height: Distance, position: PonytailPosition, style: PonytailStyle) =
+    HumanoidBody(
+        Body(BodyShape.Hourglass),
+        Head(
+            NormalEars(),
+            TwoEyes(),
+            NormalHair(Ponytail(style, HairLength.Classic, position), Color.Gold),
+            NoHorns,
+            FemaleMouth()
+        ),
+        height,
+    )
