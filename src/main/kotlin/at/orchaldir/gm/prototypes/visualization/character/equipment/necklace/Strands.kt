9018072444a3255668a58d32bbb0f6671659d6d8
Orchaldir <orchaldir@gmail.com>
2025-04-10 13:26:19 +0200

    Improve

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/necklace/Strands.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/necklace/Strands.kt
@@ -2,16 +2,13 @@ package at.orchaldir.gm.prototypes.visualization.character.equipment.necklace
 
 import at.orchaldir.gm.core.model.character.EquipmentMap.Companion.from
 import at.orchaldir.gm.core.model.character.appearance.Body
-import at.orchaldir.gm.core.model.character.appearance.BodyShape
 import at.orchaldir.gm.core.model.character.appearance.BodyShape.Hourglass
 import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
 import at.orchaldir.gm.core.model.item.equipment.Necklace
 import at.orchaldir.gm.core.model.item.equipment.style.*
-import at.orchaldir.gm.core.model.util.Color.SaddleBrown
 import at.orchaldir.gm.core.model.util.Color.White
 import at.orchaldir.gm.core.model.util.Size
-import at.orchaldir.gm.core.model.util.Size.*
 import at.orchaldir.gm.prototypes.visualization.addNames
 import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
 import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
@@ -28,7 +25,7 @@ fun main() {
     ) { distance, type, size ->
         val strand = when (type) {
             StrandType.Chain -> Chain(size)
-            StrandType.Ornament -> OrnamentChain(pearl)
+            StrandType.Ornament -> OrnamentChain(pearl, size)
             StrandType.Wire -> Wire(size)
         }
         val style = StrandNecklace(1, strand)
