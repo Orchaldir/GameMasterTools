32d35ece95585f21a05351cbdc52a5d44f65cbe5
Orchaldir <orchaldir@gmail.com>
2025-04-10 11:57:23 +0200

    Add necklaces-strands.svg

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/necklace/StrandNecklaces.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/necklace/StrandNecklaces.kt
@@ -3,32 +3,40 @@ package at.orchaldir.gm.prototypes.visualization.character.equipment.necklace
 import at.orchaldir.gm.core.model.character.EquipmentMap.Companion.from
 import at.orchaldir.gm.core.model.character.appearance.Body
 import at.orchaldir.gm.core.model.character.appearance.BodyShape
+import at.orchaldir.gm.core.model.character.appearance.BodyShape.Hourglass
 import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
 import at.orchaldir.gm.core.model.item.equipment.Necklace
-import at.orchaldir.gm.core.model.item.equipment.style.NecklaceLength
-import at.orchaldir.gm.core.model.item.equipment.style.StrandNecklace
-import at.orchaldir.gm.core.model.item.equipment.style.Wire
+import at.orchaldir.gm.core.model.item.equipment.style.*
+import at.orchaldir.gm.core.model.util.Color.SaddleBrown
+import at.orchaldir.gm.core.model.util.Size.*
 import at.orchaldir.gm.prototypes.visualization.addNames
 import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
 import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTable
 import at.orchaldir.gm.utils.math.unit.Distance
 
 fun main() {
+    val strand = Wire()
+    val strands: List<Pair<String, StrandNecklace>> = listOf(
+        Pair("1", StrandNecklace(1, strand)),
+        Pair("2", StrandNecklace(2, strand)),
+        Pair("3", StrandNecklace(3, strand)),
+    )
+
     renderCharacterTable(
-        "necklaces-body.svg",
+        "necklaces-strands.svg",
         CHARACTER_CONFIG,
-        addNames(BodyShape.entries),
+        strands,
         addNames(NecklaceLength.entries),
-    ) { distance, length, bodyShape ->
-        val necklace = Necklace(StrandNecklace(1, Wire()), length)
-        Pair(createAppearance(distance, bodyShape), from(necklace))
+    ) { distance, length, style ->
+        val necklace = Necklace(style, length)
+        Pair(createAppearance(distance), from(necklace))
     }
 }
 
-private fun createAppearance(height: Distance, shape: BodyShape) =
+private fun createAppearance(height: Distance) =
     HumanoidBody(
-        Body(shape),
+        Body(Hourglass),
         Head(),
         height,
     )
