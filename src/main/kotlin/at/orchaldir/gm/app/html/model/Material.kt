7ca06ab9001952bb494df3fc745e0544bce81a76
Orchaldir <orchaldir@gmail.com>
2025-04-11 08:20:55 +0200

    add color UI

--- src/main/kotlin/at/orchaldir/gm/app/html/model/Material.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/Material.kt
@@ -1,8 +1,11 @@
 package at.orchaldir.gm.app.html.model
 
 import at.orchaldir.gm.app.CATEGORY
+import at.orchaldir.gm.app.COLOR
+import at.orchaldir.gm.app.EQUIPMENT_COLOR_0
 import at.orchaldir.gm.app.NAME
 import at.orchaldir.gm.app.html.field
+import at.orchaldir.gm.app.html.selectColor
 import at.orchaldir.gm.app.html.selectName
 import at.orchaldir.gm.app.html.selectValue
 import at.orchaldir.gm.app.parse.parse
@@ -11,6 +14,7 @@ import at.orchaldir.gm.app.parse.parseOptionalInt
 import at.orchaldir.gm.core.model.material.Material
 import at.orchaldir.gm.core.model.material.MaterialCategory
 import at.orchaldir.gm.core.model.material.MaterialId
+import at.orchaldir.gm.core.model.util.Color
 import io.ktor.http.*
 import io.ktor.server.util.*
 import kotlinx.html.HtmlBlockTag
@@ -20,6 +24,7 @@ import kotlinx.html.HtmlBlockTag
 fun HtmlBlockTag.showMaterial(material: Material) {
     field("Name", material.name)
     field("Category", material.category)
+    field("Color", material.color)
 }
 
 // edit
@@ -27,6 +32,7 @@ fun HtmlBlockTag.showMaterial(material: Material) {
 fun HtmlBlockTag.editMaterial(material: Material) {
     selectName(material.name)
     selectValue("Category", CATEGORY, MaterialCategory.entries, material.category)
+    selectColor(material.color, COLOR)
 }
 
 // parse
@@ -42,5 +48,6 @@ fun parseMaterial(id: MaterialId, parameters: Parameters): Material {
         id,
         name,
         parse(parameters, CATEGORY, MaterialCategory.Metal),
+        parse(parameters, COLOR, Color.Pink),
     )
 }
