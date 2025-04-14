4124080b6db17ca068aa21aea2bed29bc39eae27
Orchaldir <orchaldir@gmail.com>
2025-04-14 15:39:18 +0200

    Add getRectangle()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/beard/FullBeard.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/appearance/beard/FullBeard.kt
@@ -1,6 +1,5 @@
 package at.orchaldir.gm.visualization.character.appearance.beard
 
-import at.orchaldir.gm.core.model.character.appearance.Head
 import at.orchaldir.gm.core.model.character.appearance.beard.FullBeardStyle
 import at.orchaldir.gm.core.model.character.appearance.hair.HairLength
 import at.orchaldir.gm.core.model.util.Color
@@ -19,15 +18,15 @@ fun visualizeFullBeard(
     val distance = state.config.getHairLength(state.aabb, length)
     val polygon = when (style) {
         FullBeardStyle.Forked -> return
-        FullBeardStyle.Rectangle -> getRectangleFullBeard(state, distance)
-        FullBeardStyle.Triangle -> return
+        FullBeardStyle.Rectangle -> getRectangle(state, distance)
+        FullBeardStyle.Triangle -> getTriangle(state, distance)
         FullBeardStyle.Wide -> return
     }
 
     layer.renderRoundedPolygon(polygon, options)
 }
 
-private fun getRectangleFullBeard(state: CharacterRenderState, distance: Distance): Polygon2d {
+private fun getRectangle(state: CharacterRenderState, distance: Distance): Polygon2d {
     val padding = state.config.head.hair.longPadding
     val width = FULL + padding * 2.0f
     val startY = HALF
@@ -38,5 +37,19 @@ private fun getRectangleFullBeard(state: CharacterRenderState, distance: Distanc
         .addMirroredPoints(state.aabb, width, startY)
         .addPoints(left, right)
         .addPoints(left.addHeight(distance), right.addHeight(distance))
+        .build()
+}
+
+private fun getTriangle(state: CharacterRenderState, distance: Distance): Polygon2d {
+    val padding = state.config.head.hair.longPadding
+    val width = FULL + padding * 2.0f
+    val startY = HALF
+    val center = state.aabb.getPoint(CENTER, FULL)
+
+    return Polygon2dBuilder()
+        .addLeftPoint(state.aabb, CENTER, startY)
+        .addMirroredPoints(state.aabb, width, startY)
+        .addMirroredPoints(state.aabb, width, END)
+        .addLeftPoint(center.addHeight(distance))
         .build()
 }
