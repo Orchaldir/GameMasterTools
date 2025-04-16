93a8fd668970c40b58b13f32c80eb72b3c822023
Orchaldir <orchaldir@gmail.com>
2025-04-16 20:00:22 +0200

    Add SexualOrientation

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/character/SexualOrientation.kt
@@ -0,0 +1,18 @@
+package at.orchaldir.gm.core.model.character
+
+enum class SexualOrientation {
+    Asexuality,
+
+    /**
+     * Regardless of sex or gender.
+     */
+    Pansexuality,
+    Bisexuality,
+
+    /**
+     * Attracted to someone after getting to know them for a certain period of time.
+     */
+    Demisexuality,
+    Heterosexuality,
+    Homosexuality,
+}
