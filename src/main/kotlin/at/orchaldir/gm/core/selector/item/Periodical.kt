a474562e6a16b4a61e94cf4a2830e548c59b3255
Orchaldir <orchaldir@gmail.com>
2025-04-17 20:51:39 +0200

    Update calendar UI

--- src/main/kotlin/at/orchaldir/gm/core/selector/item/Periodical.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/item/Periodical.kt
@@ -4,8 +4,13 @@ import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.item.periodical.Periodical
 import at.orchaldir.gm.core.model.item.periodical.PeriodicalId
 import at.orchaldir.gm.core.model.language.LanguageId
+import at.orchaldir.gm.core.model.time.calendar.CalendarId
 
 fun State.canDeletePeriodical(text: PeriodicalId) = true
+
+fun State.countPeriodicals(calendar: CalendarId) = getPeriodicalStorage()
+    .getAll()
+    .count { b -> b.calendar == calendar }
 
 fun State.countPeriodicals(language: LanguageId) = getPeriodicalStorage()
     .getAll()
@@ -14,6 +19,10 @@ fun State.countPeriodicals(language: LanguageId) = getPeriodicalStorage()
 fun countPublicationFrequencies(collection: Collection<Periodical>) = collection
     .groupingBy { it.frequency }
     .eachCount()
+
+fun State.getPeriodicals(calendar: CalendarId) = getPeriodicalStorage()
+    .getAll()
+    .filter { b -> b.calendar == calendar }
 
 fun State.getPeriodicals(language: LanguageId) = getPeriodicalStorage()
     .getAll()
