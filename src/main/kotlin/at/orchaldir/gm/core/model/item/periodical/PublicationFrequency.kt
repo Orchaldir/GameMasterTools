8ddfcadf8fb99f2f045d4f387eee8c5838059384
Orchaldir <orchaldir@gmail.com>
2025-04-19 22:44:37 +0200

    Update PublicationFrequency

--- src/main/kotlin/at/orchaldir/gm/core/model/item/periodical/PublicationFrequency.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/periodical/PublicationFrequency.kt
@@ -1,8 +1,54 @@
 package at.orchaldir.gm.core.model.item.periodical
 
-enum class PublicationFrequency {
+import at.orchaldir.gm.core.model.time.date.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class PublicationFrequencyType {
     Daily,
     Weekly,
     Monthly,
     Yearly,
 }
+
+@Serializable
+sealed class PublicationFrequency {
+
+    fun getType() = when (this) {
+        is DailyPublication -> PublicationFrequencyType.Daily
+        is WeeklyPublication -> PublicationFrequencyType.Weekly
+        is MonthlyPublication -> PublicationFrequencyType.Monthly
+        is YearlyPublication -> PublicationFrequencyType.Yearly
+    }
+
+    fun getStartDate(): Date = when (this) {
+        is DailyPublication -> start
+        is WeeklyPublication -> start
+        is MonthlyPublication -> start
+        is YearlyPublication -> start
+    }
+}
+
+@Serializable
+@SerialName("Daily")
+data class DailyPublication(
+    val start: Day,
+) : PublicationFrequency()
+
+@Serializable
+@SerialName("Weekly")
+data class WeeklyPublication(
+    val start: Week,
+) : PublicationFrequency()
+
+@Serializable
+@SerialName("Monthly")
+data class MonthlyPublication(
+    val start: Month,
+) : PublicationFrequency()
+
+@Serializable
+@SerialName("Yearly")
+data class YearlyPublication(
+    val start: Year,
+) : PublicationFrequency()
