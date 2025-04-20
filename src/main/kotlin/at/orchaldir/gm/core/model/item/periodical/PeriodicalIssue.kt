0dab2ddb8ea505fa5b812b288bbdcac2ece44163
Orchaldir <orchaldir@gmail.com>
2025-04-20 09:57:52 +0200

    Add PeriodicalIssue

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/periodical/PeriodicalIssue.kt
@@ -0,0 +1,30 @@
+package at.orchaldir.gm.core.model.item.periodical
+
+import at.orchaldir.gm.core.model.time.date.Date
+import at.orchaldir.gm.core.model.util.ElementWithSimpleName
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val PERIODICAL_ISSUE_TYPE = "Periodical Issue"
+
+@JvmInline
+@Serializable
+value class PeriodicalIssueId(val value: Int) : Id<PeriodicalIssueId> {
+
+    override fun next() = PeriodicalIssueId(value + 1)
+    override fun type() = PERIODICAL_ISSUE_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class PeriodicalIssue(
+    val id: PeriodicalIssueId,
+    val periodical: PeriodicalId = PeriodicalId(0),
+    val date: Date,
+) : ElementWithSimpleName<PeriodicalIssueId> {
+
+    override fun id() = id
+    override fun name() = ""
+
+}
