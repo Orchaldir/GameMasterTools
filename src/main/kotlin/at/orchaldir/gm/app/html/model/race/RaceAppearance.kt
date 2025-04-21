3a7d03d9afb1062b4dc2eaa044bf6036b7fc2d4e
Orchaldir <orchaldir@gmail.com>
2025-04-21 16:33:35 +0200

    Add hasWings()

--- src/main/kotlin/at/orchaldir/gm/app/html/model/race/RaceAppearance.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/race/RaceAppearance.kt
@@ -11,6 +11,7 @@ import at.orchaldir.gm.app.parse.combine
 import at.orchaldir.gm.app.parse.parse
 import at.orchaldir.gm.app.parse.parseInt
 import at.orchaldir.gm.app.parse.parseOneOf
+import at.orchaldir.gm.app.parse.parseOneOrNone
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.character.appearance.*
 import at.orchaldir.gm.core.model.character.appearance.beard.BeardType
@@ -264,6 +265,11 @@ private fun HtmlBlockTag.showWings(
     val options = appearance.wing
 
     showRarityMap("Layout", options.layouts)
+
+    if (!options.hasWings()) {
+        return
+    }
+
     showRarityMap("Type", options.types)
 
     if (options.types.isAvailable(WingType.Bat)) {
@@ -502,6 +508,11 @@ private fun FORM.editWings(state: State, appearance: RaceAppearance) {
     val options = appearance.wing
 
     selectRarityMap("Layout", combine(WING, LAYOUT), options.layouts, true)
+
+    if (!options.hasWings()) {
+        return
+    }
+
     selectRarityMap("Types", combine(WING, TYPE), options.types, true)
 
     if (options.types.isAvailable(WingType.Bat)) {
@@ -622,7 +633,7 @@ private fun parseFeatureColor(parameters: Parameters, param: String) = FeatureCo
 
 private fun parseWingOptions(parameters: Parameters) = WingOptions(
     parseOneOf(parameters, combine(WING, LAYOUT), WingsLayout::valueOf),
-    parseOneOf(parameters, combine(WING, TYPE), WingType::valueOf),
+    parseOneOrNone(parameters, combine(WING, TYPE), WingType::valueOf),
     parseFeatureColor(parameters, combine(WING, BAT)),
     parseOneOf(parameters, combine(WING, BIRD, COLOR), Color::valueOf, setOf(DEFAULT_BIRD_COLOR)),
     parseOneOf(parameters, combine(WING, BUTTERFLY, COLOR), Color::valueOf, setOf(DEFAULT_BUTTERFLY_COLOR)),
--- src/main/kotlin/at/orchaldir/gm/core/model/race/appearance/WingOptions.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/race/appearance/WingOptions.kt
@@ -6,13 +6,20 @@ import at.orchaldir.gm.core.model.character.appearance.wing.WingType
 import at.orchaldir.gm.core.model.character.appearance.wing.WingsLayout
 import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.core.model.util.OneOf
+import at.orchaldir.gm.core.model.util.OneOrNone
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class WingOptions(
     val layouts: OneOf<WingsLayout> = OneOf(WingsLayout.None),
-    val types: OneOf<WingType> = OneOf(WingType.Bird),
+    val types: OneOrNone<WingType> = OneOrNone(),
     val batColors: FeatureColorOptions = FeatureColorOptions(),
     val birdColors: OneOf<Color> = OneOf(DEFAULT_BIRD_COLOR),
     val butterflyColors: OneOf<Color> = OneOf(DEFAULT_BUTTERFLY_COLOR),
-)
+) {
+
+    fun hasWings() = layouts.contains(WingsLayout.One) ||
+            layouts.contains(WingsLayout.Two) ||
+            layouts.contains(WingsLayout.Different)
+
+}
