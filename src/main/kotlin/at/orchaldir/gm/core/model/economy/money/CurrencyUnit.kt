e1e95b45c7c69a0c97c9e58d7a0200547b25cb81
Orchaldir <orchaldir@gmail.com>
2025-04-25 17:53:46 +0200

    Add CurrencyUnit

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/economy/money/CurrencyUnit.kt
@@ -0,0 +1,34 @@
+package at.orchaldir.gm.core.model.economy.money
+
+import at.orchaldir.gm.core.model.util.ElementWithSimpleName
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val CURRENCY_UNIT_TYPE = "CurrencyUnit"
+
+@JvmInline
+@Serializable
+value class CurrencyUnitId(val value: Int) : Id<CurrencyUnitId> {
+
+    override fun next() = CurrencyUnitId(value + 1)
+    override fun type() = CURRENCY_UNIT_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class CurrencyUnit(
+    val id: CurrencyUnitId,
+    val name: String = "Currency Unit ${id.value}",
+    val currency: CurrencyId = CurrencyId(0),
+    val value: Int = 1,
+) : ElementWithSimpleName<CurrencyUnitId> {
+
+    init {
+        require(value > 0) { "The $name's value is too low!" }
+    }
+
+    override fun id() = id
+    override fun name() = name
+
+}
