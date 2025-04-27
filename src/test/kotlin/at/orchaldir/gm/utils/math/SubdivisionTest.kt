89ad7be4c4f968e5ac5cccea5756dd9664d269f2
Orchaldir <orchaldir@gmail.com>
2025-04-27 10:52:20 +0200

    Fix test

--- src/test/kotlin/at/orchaldir/gm/utils/math/SubdivisionTest.kt
+++ src/test/kotlin/at/orchaldir/gm/utils/math/SubdivisionTest.kt
@@ -45,4 +45,49 @@ class SubdivisionTest {
             assertEquals(output, subdivideLine(input, 1))
         }
     }
+
+    @Nested
+    inner class PolygonTest {
+
+        @Test
+        fun `Subdividing an empty list returns the input`() {
+            val noPoints = emptyList<Point2d>()
+
+            test(noPoints, noPoints)
+        }
+
+        @Test
+        fun `Subdividing a single point returns the input`() {
+            val point = listOf(Point2d())
+
+            test(point, point)
+        }
+
+        @Test
+        fun `Subdividing two points returns the input`() {
+            val twoPoints = listOf(Point2d(), Point2d(1.0f, 2.0f))
+
+            test(twoPoints, twoPoints)
+        }
+
+        @Test
+        fun `Subdivide a list of points`() {
+            val input = listOf(Point2d(), Point2d(3.0f, 0.0f), Point2d(3.0f, 3.0f))
+            val output = listOf(
+                Point2d(1.5f, 0.0f),
+                Point2d(3.0f, 0.0f),
+                Point2d(3.0f, 1.5f),
+                Point2d(3.0f, 3.0f),
+                Point2d(1.5f, 1.5f),
+                Point2d(),
+            )
+
+            test(input, output)
+        }
+
+        private fun test(input: List<Point2d>, output: List<Point2d>) {
+            assertEquals(output, subdividePolygon(input, 1, ::halfSegment))
+        }
+    }
+
 }
