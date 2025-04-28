73a12f9372bbbfbe46b7581d3ad6686be5951b35
Orchaldir <orchaldir@gmail.com>
2025-04-28 16:54:38 +0200

    Extract FontOption.kt

--- src/main/kotlin/at/orchaldir/gm/app/html/model/item/text/Typography.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/item/text/Typography.kt
@@ -1,21 +1,21 @@
 package at.orchaldir.gm.app.html.model.item.text
 
 import at.orchaldir.gm.app.*
+import at.orchaldir.gm.app.html.model.font.editFontOption
+import at.orchaldir.gm.app.html.model.font.parseFontOption
 import at.orchaldir.gm.app.html.model.parseDistance
-import at.orchaldir.gm.app.html.model.font.parseFontId
 import at.orchaldir.gm.app.html.model.selectDistance
-import at.orchaldir.gm.app.html.selectColor
-import at.orchaldir.gm.app.html.selectElement
 import at.orchaldir.gm.app.html.selectValue
 import at.orchaldir.gm.app.html.showDetails
 import at.orchaldir.gm.app.parse.combine
 import at.orchaldir.gm.app.parse.parse
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.font.*
+import at.orchaldir.gm.core.model.font.FontOption
 import at.orchaldir.gm.core.model.item.text.book.typography.*
-import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.utils.doNothing
-import at.orchaldir.gm.utils.math.unit.*
+import at.orchaldir.gm.utils.math.unit.Distance
+import at.orchaldir.gm.utils.math.unit.ONE_M
+import at.orchaldir.gm.utils.math.unit.ZERO
 import io.ktor.http.*
 import kotlinx.html.HtmlBlockTag
 
@@ -122,84 +122,6 @@ private fun HtmlBlockTag.editStringSharedOptions(
     editFontOption(state, fontOption, combine(param, FONT))
 }
 
-fun HtmlBlockTag.editFontOption(
-    state: State,
-    text: String,
-    option: FontOption,
-    param: String,
-) {
-    showDetails(text, true) {
-        editFontOption(state, option, param)
-    }
-}
-
-fun HtmlBlockTag.editFontOption(
-    state: State,
-    option: FontOption,
-    param: String,
-) {
-    selectValue("Font Option", combine(param, TYPE), FontOptionType.entries, option.getType(), true)
-
-    when (option) {
-        is SolidFont -> {
-            selectColor(option.color, combine(param, COLOR), "Font Color")
-            editSharedFontOptions(state, param, option.font, option.size)
-        }
-
-        is FontWithBorder -> {
-            selectColor(option.fill, combine(param, COLOR), "Fill Color")
-            selectColor(option.border, combine(param, BORDER, COLOR), "Border Color")
-            editSharedFontOptions(state, param, option.font, option.size)
-            selectBorderThickness(param, option.thickness)
-        }
-
-        is HollowFont -> {
-            selectColor(option.border, combine(param, BORDER, COLOR), "Border Color")
-            editSharedFontOptions(state, param, option.font, option.size)
-            selectBorderThickness(param, option.thickness)
-        }
-    }
-}
-
-private fun HtmlBlockTag.selectBorderThickness(
-    param: String,
-    thickness: Distance,
-) {
-    selectDistance(
-        "Border Thickness",
-        combine(param, BORDER, SIZE),
-        thickness,
-        HUNDRED_µM,
-        ONE_CM,
-        HUNDRED_µM,
-        update = true,
-    )
-}
-
-private fun HtmlBlockTag.editSharedFontOptions(
-    state: State,
-    param: String,
-    fontId: FontId,
-    size: Distance,
-) {
-    selectElement(
-        state,
-        "Font",
-        combine(param, FONT),
-        state.getFontStorage().getAll(),
-        fontId,
-        true,
-    )
-    selectDistance(
-        "Font Size",
-        combine(param, SIZE),
-        size,
-        ONE_MM,
-        ONE_M,
-        update = true
-    )
-}
-
 // parse
 
 fun parseTextTypography(parameters: Parameters) = when (parse(parameters, TYPOGRAPHY, TypographyType.None)) {
@@ -221,30 +143,6 @@ fun parseTextTypography(parameters: Parameters) = when (parse(parameters, TYPOGR
         parseStringRenderOption(parameters, CREATOR),
     )
 }
-
-private fun parseFontOption(parameters: Parameters, param: String) =
-    when (parse(parameters, combine(param, TYPE), FontOptionType.Solid)) {
-        FontOptionType.Solid -> SolidFont(
-            parseDistance(parameters, combine(param, SIZE), 10),
-            parse(parameters, combine(param, COLOR), Color.White),
-            parseFontId(parameters, combine(param, FONT)),
-        )
-
-        FontOptionType.Border -> FontWithBorder(
-            parseDistance(parameters, combine(param, SIZE), 10),
-            parseDistance(parameters, combine(param, BORDER, SIZE), 1),
-            parse(parameters, combine(param, COLOR), Color.White),
-            parse(parameters, combine(param, BORDER, COLOR), Color.White),
-            parseFontId(parameters, combine(param, FONT)),
-        )
-
-        FontOptionType.Hollow -> HollowFont(
-            parseDistance(parameters, combine(param, SIZE), 10),
-            parseDistance(parameters, combine(param, BORDER, SIZE), 1),
-            parse(parameters, combine(param, BORDER, COLOR), Color.White),
-            parseFontId(parameters, combine(param, FONT)),
-        )
-    }
 
 private fun parseStringRenderOption(parameters: Parameters, param: String) =
     when (parse(parameters, combine(param, TYPE), StringRenderOptionType.Simple)) {
