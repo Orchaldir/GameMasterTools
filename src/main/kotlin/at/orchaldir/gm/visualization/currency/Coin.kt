df5c776ab917df44d1507ee3ea7085d4d66ef56d
Orchaldir <orchaldir@gmail.com>
2025-04-28 14:58:46 +0200

    Add visualizeCoinRim()

--- src/main/kotlin/at/orchaldir/gm/visualization/currency/Coin.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/currency/Coin.kt
@@ -3,20 +3,30 @@ package at.orchaldir.gm.visualization.currency
 import at.orchaldir.gm.core.model.economy.money.BiMetallicCoin
 import at.orchaldir.gm.core.model.economy.money.Coin
 import at.orchaldir.gm.core.model.economy.money.HoledCoin
+import at.orchaldir.gm.core.model.economy.money.Shape
+import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.ONE
+import at.orchaldir.gm.utils.math.Point2d
+import at.orchaldir.gm.utils.math.unit.Distance
+import at.orchaldir.gm.utils.renderer.LayerRenderer
+import at.orchaldir.gm.utils.renderer.model.BorderOnly
 
 fun visualizeCoin(
     state: CurrencyRenderState,
     coin: Coin,
 ) {
     val options = state.getFillAndBorder(coin.material)
+    val renderer = state.renderer.getLayer()
+    val center = state.aabb.getCenter()
 
     visualizeShape(
-        state.renderer.getLayer(),
-        state.aabb.getCenter(),
+        renderer,
+        center,
         coin.shape,
         coin.radius,
         options
     )
+    visualizeCoinRim(state, renderer, center, coin.shape, coin.radius, coin.rimFactor)
 }
 
 fun visualizeHoledCoin(
@@ -24,16 +34,19 @@ fun visualizeHoledCoin(
     coin: HoledCoin,
 ) {
     val options = state.getFillAndBorder(coin.material)
+    val renderer = state.renderer.getLayer()
+    val center = state.aabb.getCenter()
 
     visualizeHoledShape(
-        state.renderer.getLayer(),
-        state.aabb.getCenter(),
+        renderer,
+        center,
         coin.shape,
         coin.radius,
         coin.holeShape,
         coin.calculateHoleRadius(),
         options
     )
+    visualizeCoinRim(state, renderer, center, coin.shape, coin.radius, coin.rimFactor)
 }
 
 fun visualizeBiMetallicCoin(
@@ -53,11 +66,33 @@ fun visualizeBiMetallicCoin(
         options
     )
 
+    visualizeCoinRim(state, renderer, center, coin.shape, coin.radius, coin.rimFactor)
+
     visualizeShape(
         renderer,
         center,
         coin.innerShape,
         coin.calculateInnerRadius(),
         innerOptions
+    )
+}
+
+private fun visualizeCoinRim(
+    state: CurrencyRenderState,
+    renderer: LayerRenderer,
+    center: Point2d,
+    shape: Shape,
+    radius: Distance,
+    rimFactor: Factor,
+) {
+    val rimRadius = radius * (ONE - rimFactor)
+    val rimOptions = BorderOnly(state.config.line)
+
+    visualizeShape(
+        renderer,
+        center,
+        shape,
+        rimRadius,
+        rimOptions,
     )
 }
