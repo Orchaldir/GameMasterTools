9205fcd1ceed723d85e7d8f03b620e24ea3a998c
Orchaldir <orchaldir@gmail.com>
2025-04-28 19:51:46 +0200

    Add visualizeDenomination()

--- src/main/kotlin/at/orchaldir/gm/visualization/currency/CoinSide.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/currency/CoinSide.kt
@@ -4,10 +4,15 @@ import at.orchaldir.gm.core.model.economy.money.BlankCoinSide
 import at.orchaldir.gm.core.model.economy.money.CoinSide
 import at.orchaldir.gm.core.model.economy.money.ShowDenomination
 import at.orchaldir.gm.core.model.economy.money.ShowName
+import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.Point2d
+import at.orchaldir.gm.utils.math.ZERO_ORIENTATION
 import at.orchaldir.gm.utils.math.unit.Distance
 import at.orchaldir.gm.utils.renderer.LayerRenderer
+import at.orchaldir.gm.utils.renderer.model.BorderOnly
+import at.orchaldir.gm.utils.renderer.model.LineOptions
+import at.orchaldir.gm.utils.renderer.model.RenderStringOptions
 
 fun visualizeCoinSide(
     state: CurrencyRenderState,
@@ -28,5 +33,12 @@ private fun visualizeDenomination(
     radius: Distance,
     side: ShowDenomination,
 ) {
+    val text = state.data.denomination.display(state.data.number)
+    val options = RenderStringOptions(
+        BorderOnly(LineOptions(Color.Black.toRender(), radius / 50.0f)),
+        radius.toMeters(),
+        state.state.getFontStorage().getOptional(side.font),
+    )
 
+    renderer.renderString(text, center, ZERO_ORIENTATION, options)
 }
