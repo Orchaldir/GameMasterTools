74b50597c9cadf3a17132195c508b947e6d3c4cc
Orchaldir <orchaldir@gmail.com>
2025-04-29 20:36:14 +0200

    Move select

--- src/main/kotlin/at/orchaldir/gm/app/html/Primitive.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/Primitive.kt
@@ -4,7 +4,11 @@ import io.ktor.http.Parameters
 import kotlinx.html.HtmlBlockTag
 import kotlinx.html.P
 import kotlinx.html.b
+import kotlinx.html.checkBoxInput
+import kotlinx.html.numberInput
+import kotlinx.html.onChange
 import kotlinx.html.p
+import kotlinx.html.textInput
 import kotlin.text.toFloat
 import kotlin.text.trim
 
@@ -44,6 +48,136 @@ fun HtmlBlockTag.field(label: String, content: P.() -> Unit) {
 }
 
 // edit
+
+fun HtmlBlockTag.selectBool(
+    label: String,
+    value: Boolean,
+    param: String,
+    isDisabled: Boolean = false,
+    update: Boolean = false,
+) {
+    field(label) {
+        selectBool(value, param, isDisabled, update)
+    }
+}
+
+fun HtmlBlockTag.selectBool(
+    isChecked: Boolean,
+    param: String,
+    isDisabled: Boolean = false,
+    update: Boolean = false,
+) {
+    checkBoxInput {
+        name = param
+        value = "true"
+        checked = isChecked
+        disabled = isDisabled
+        if (update) {
+            onChange = ON_CHANGE_SCRIPT
+        }
+    }
+}
+
+fun HtmlBlockTag.selectFloat(
+    label: String,
+    number: Float,
+    minNumber: Float,
+    maxNumber: Float,
+    step: Float,
+    param: String,
+    update: Boolean = false,
+) {
+    field(label) {
+        selectFloat(number, minNumber, maxNumber, step, param, update)
+    }
+}
+
+fun HtmlBlockTag.selectFloat(
+    number: Float,
+    minNumber: Float,
+    maxNumber: Float,
+    stepValue: Float,
+    param: String,
+    update: Boolean = false,
+) {
+    numberInput(name = param) {
+        min = "$minNumber"
+        max = "$maxNumber"
+        step = stepValue.toString()
+        value = number.toString()
+        if (update) {
+            onChange = ON_CHANGE_SCRIPT
+        }
+    }
+}
+
+fun HtmlBlockTag.selectInt(
+    label: String,
+    number: Int,
+    minNumber: Int,
+    maxNumber: Int,
+    stepNumber: Int,
+    param: String,
+    update: Boolean = false,
+) {
+    field(label) {
+        selectInt(number, minNumber, maxNumber, stepNumber, param, update)
+    }
+}
+
+fun HtmlBlockTag.selectInt(
+    number: Int,
+    minNumber: Int,
+    maxNumber: Int,
+    stepNumber: Int,
+    param: String,
+    update: Boolean = false,
+) {
+    numberInput(name = param) {
+        min = "$minNumber"
+        max = "$maxNumber"
+        step = stepNumber.toString()
+        value = number.toString()
+        if (update) {
+            onChange = ON_CHANGE_SCRIPT
+        }
+    }
+}
+
+fun HtmlBlockTag.selectOptionalText(
+    label: String,
+    text: String?,
+    param: String,
+) {
+    selectText(label, text ?: "", param, 0)
+}
+
+fun HtmlBlockTag.selectText(
+    label: String,
+    text: String,
+    param: String,
+    min: Int = 1,
+    max: Int? = null,
+) {
+    field(label) {
+        selectText(text, param, min, max)
+    }
+}
+
+fun HtmlBlockTag.selectText(
+    text: String,
+    param: String,
+    min: Int = 1,
+    max: Int? = null,
+) {
+    textInput(name = param) {
+        minLength = "$min"
+        if (max != null) {
+            maxLength = "$max"
+        }
+        value = text
+    }
+}
 
 // parse
 
