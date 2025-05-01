a25b6b50172eed7fa13ded0d360f09c16f838bd8
Orchaldir <orchaldir@gmail.com>
2025-05-01 20:11:55 +0200

    show details

---
+++ src/main/kotlin/at/orchaldir/gm/app/routes/economy/StandardOfLiving.kt
@@ -0,0 +1,58 @@
+package at.orchaldir.gm.app.routes.economy
+
+import at.orchaldir.gm.app.STORE
+import at.orchaldir.gm.app.html.back
+import at.orchaldir.gm.app.html.model.economy.showStandardOfLiving
+import at.orchaldir.gm.app.html.simpleHtmlDetails
+import at.orchaldir.gm.app.routes.DataRoutes
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.economy.standard.STANDARD_TYPE
+import at.orchaldir.gm.core.model.economy.standard.StandardOfLiving
+import at.orchaldir.gm.core.model.economy.standard.StandardOfLivingId
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.resources.*
+import io.ktor.server.routing.*
+import kotlinx.html.HTML
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+@Resource("/$STANDARD_TYPE")
+class StandardOfLivingRoutes {
+    @Resource("details")
+    class Details(val id: StandardOfLivingId, val parent: StandardOfLivingRoutes = StandardOfLivingRoutes())
+
+}
+
+fun Application.configureStandardOfLivingRouting() {
+    routing {
+        get<StandardOfLivingRoutes.Details> { details ->
+            logger.info { "Get details of standard ${details.id.value}" }
+
+            val state = STORE.getState()
+            val standard = state.data.economy.getStandardOfLiving(details.id)
+
+            call.respondHtml(HttpStatusCode.OK) {
+                showStandardOfLivingDetails(call, state, standard)
+            }
+        }
+    }
+}
+
+private fun HTML.showStandardOfLivingDetails(
+    call: ApplicationCall,
+    state: State,
+    standard: StandardOfLiving,
+) {
+    val backLink = call.application.href(DataRoutes())
+
+    simpleHtmlDetails(standard) {
+        showStandardOfLiving(call, state, standard)
+
+        back(backLink)
+    }
+}
+
