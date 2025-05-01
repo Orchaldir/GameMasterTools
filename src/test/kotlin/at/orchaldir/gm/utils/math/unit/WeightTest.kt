afb543d8004bb9b70d1f222bc215990bb5f8570a
Orchaldir <orchaldir@gmail.com>
2025-05-01 11:49:35 +0200

    fix test

--- src/test/kotlin/at/orchaldir/gm/utils/math/unit/WeightTest.kt
+++ src/test/kotlin/at/orchaldir/gm/utils/math/unit/WeightTest.kt
@@ -7,10 +7,11 @@ class WeightTest {
 
     @Test
     fun `To string`() {
-        assertEquals("0.001 kg", Weight.fromGrams(1).toString())
-        assertEquals("0.012 kg", Weight.fromGrams(12).toString())
-        assertEquals("0.123 kg", Weight.fromGrams(123).toString())
-        assertEquals("1.234 kg", Weight.fromGrams(1234).toString())
+        assertEquals("1.0 g", Weight.fromGrams(1).toString())
+        assertEquals("12.0 g", Weight.fromGrams(12).toString())
+        assertEquals("123.0 g", Weight.fromGrams(123).toString())
+        assertEquals("1.0 kg", Weight.fromKilograms(1).toString())
+        assertEquals("1.2 kg", Weight.fromGrams(1234).toString())
         assertEquals("12.3 kg", Weight.fromGrams(12345).toString())
     }
 
