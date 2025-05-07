39082f637cb15cc3aa4b23a3a6e24be2b53a67c3
Orchaldir <orchaldir@gmail.com>
2025-05-07 17:19:47 +0200

    Add visualizeAbstractText()

--- src/main/kotlin/at/orchaldir/gm/visualization/text/book/Page.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/text/book/Page.kt
@@ -1,7 +1,11 @@
 package at.orchaldir.gm.visualization.text.book
 
 import at.orchaldir.gm.core.model.item.text.Book
+import at.orchaldir.gm.core.model.item.text.content.AbstractChapters
+import at.orchaldir.gm.core.model.item.text.content.AbstractText
 import at.orchaldir.gm.core.model.item.text.content.TextContent
+import at.orchaldir.gm.core.model.item.text.content.UndefinedTextContent
+import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.renderer.model.FillAndBorder
 import at.orchaldir.gm.visualization.text.TextRenderState
 
@@ -12,6 +16,12 @@ fun visualizeBookPage(
     page: Int,
 ) {
     visualizePage(state, book)
+
+    when (content) {
+        is AbstractText -> visualizeAbstractText(state, book, content, page)
+        is AbstractChapters -> doNothing()
+        UndefinedTextContent -> doNothing()
+    }
 }
 
 private fun visualizePage(
@@ -22,5 +32,14 @@ private fun visualizePage(
     val options = FillAndBorder(color.toRender(), state.config.line)
 
     state.renderer.getLayer().renderRectangle(state.aabb, options)
+}
+
+private fun visualizeAbstractText(
+    state: TextRenderState,
+    book: Book,
+    content: AbstractText,
+    page: Int,
+) {
+
 }
 
