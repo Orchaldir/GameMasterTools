9d1754a78abab758a4604114bb51fbb0ab5b157c
Orchaldir <orchaldir@gmail.com>
2025-05-07 17:09:41 +0200

    Render Page

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/text/Page.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/text/Page.kt
@@ -1,11 +1,13 @@
 package at.orchaldir.gm.prototypes.visualization.text
 
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.item.ColorItemPart
 import at.orchaldir.gm.core.model.item.text.Book
 import at.orchaldir.gm.core.model.item.text.Text
 import at.orchaldir.gm.core.model.item.text.TextId
 import at.orchaldir.gm.core.model.item.text.book.Hardcover
 import at.orchaldir.gm.core.model.item.text.content.AbstractText
+import at.orchaldir.gm.core.model.util.Color
 import at.orchaldir.gm.utils.math.Size2i.Companion.fromMillimeters
 import at.orchaldir.gm.visualization.text.visualizeTextContent
 import java.io.File
@@ -15,6 +17,7 @@ private val ID = TextId(0)
 fun main() {
     val book = Book(
         Hardcover(),
+        page = ColorItemPart(Color.AntiqueWhite),
         size = fromMillimeters(125, 190)
     )
     val content = AbstractText()
--- src/main/kotlin/at/orchaldir/gm/visualization/text/book/Page.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/text/book/Page.kt
@@ -2,6 +2,7 @@ package at.orchaldir.gm.visualization.text.book
 
 import at.orchaldir.gm.core.model.item.text.Book
 import at.orchaldir.gm.core.model.item.text.content.TextContent
+import at.orchaldir.gm.utils.renderer.model.FillAndBorder
 import at.orchaldir.gm.visualization.text.TextRenderState
 
 fun visualizeBookPage(
@@ -10,6 +11,9 @@ fun visualizeBookPage(
     content: TextContent,
     page: Int,
 ) {
+    val color = book.page.getColor(state.state)
+    val options = FillAndBorder(color.toRender(), state.config.line)
 
+    state.renderer.getLayer().renderRectangle(state.aabb, options)
 }
 
