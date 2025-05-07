00bb006647130f526ae8c55475c6b23015543ce9
Orchaldir <orchaldir@gmail.com>
2025-05-07 22:34:20 +0200

    Add 2.example

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/text/content/PagesOfAbstractText.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/text/content/PagesOfAbstractText.kt
@@ -1,4 +1,4 @@
-package at.orchaldir.gm.prototypes.visualization.text
+package at.orchaldir.gm.prototypes.visualization.text.content
 
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.item.ColorItemPart
@@ -6,10 +6,10 @@ import at.orchaldir.gm.core.model.item.text.Book
 import at.orchaldir.gm.core.model.item.text.Text
 import at.orchaldir.gm.core.model.item.text.TextId
 import at.orchaldir.gm.core.model.item.text.book.Hardcover
-import at.orchaldir.gm.core.model.item.text.content.AbstractChapter
-import at.orchaldir.gm.core.model.item.text.content.AbstractChapters
 import at.orchaldir.gm.core.model.item.text.content.AbstractContent
+import at.orchaldir.gm.core.model.item.text.content.AbstractText
 import at.orchaldir.gm.core.model.util.Color
+import at.orchaldir.gm.prototypes.visualization.text.TEXT_CONFIG
 import at.orchaldir.gm.utils.math.Size2i.Companion.fromMillimeters
 import at.orchaldir.gm.visualization.text.content.visualizeTextContent
 import java.io.File
@@ -22,15 +22,14 @@ fun main() {
         page = ColorItemPart(Color.AntiqueWhite),
         size = fromMillimeters(125, 190)
     )
-    val chapter = AbstractChapter(0, AbstractContent(10))
-    val chapters = AbstractChapters(listOf(chapter))
+    val content = AbstractText(AbstractContent(10))
     val text = Text(
         ID,
         format = book,
-        content = chapters,
+        content = content,
     )
 
     val svg = visualizeTextContent(State(), TEXT_CONFIG, text, 0)
 
-    File("book-page.svg").writeText(svg.export())
+    File("book-abstract-text-pages.svg").writeText(svg.export())
 }
