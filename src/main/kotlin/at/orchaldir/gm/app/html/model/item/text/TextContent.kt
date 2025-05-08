ee651691564b7553ee83c583190874a7349d2720
Orchaldir <orchaldir@gmail.com>
2025-05-08 08:36:02 +0200

    Update UI

--- src/main/kotlin/at/orchaldir/gm/app/html/model/item/text/TextContent.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/model/item/text/TextContent.kt
@@ -3,12 +3,18 @@ package at.orchaldir.gm.app.html.model.item.text
 import at.orchaldir.gm.app.CONTENT
 import at.orchaldir.gm.app.MAIN
 import at.orchaldir.gm.app.PAGES
+import at.orchaldir.gm.app.SIDE
 import at.orchaldir.gm.app.SPELLS
 import at.orchaldir.gm.app.STYLE
 import at.orchaldir.gm.app.TITLE
 import at.orchaldir.gm.app.html.*
+import at.orchaldir.gm.app.html.model.fieldFactor
+import at.orchaldir.gm.app.html.model.font.editFontOption
 import at.orchaldir.gm.app.html.model.font.parseFontOption
+import at.orchaldir.gm.app.html.model.font.showFontOption
 import at.orchaldir.gm.app.html.model.magic.parseSpellId
+import at.orchaldir.gm.app.html.model.parseFactor
+import at.orchaldir.gm.app.html.model.selectFactor
 import at.orchaldir.gm.app.parse.combine
 import at.orchaldir.gm.app.parse.parse
 import at.orchaldir.gm.app.parse.parseElements
@@ -17,8 +23,11 @@ import at.orchaldir.gm.core.model.item.text.content.*
 import at.orchaldir.gm.core.model.magic.SpellId
 import at.orchaldir.gm.core.selector.util.sortSpells
 import at.orchaldir.gm.utils.doNothing
+import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.Factor.Companion.fromPermille
 import io.ktor.http.*
 import io.ktor.server.application.*
+import kotlinx.html.DETAILS
 import kotlinx.html.FORM
 import kotlinx.html.HtmlBlockTag
 
@@ -34,7 +43,10 @@ fun HtmlBlockTag.showTextContent(
         field("Type", content.getType())
 
         when (content) {
-            is AbstractText -> showAbstractContent(call, state, content.content)
+            is AbstractText -> {
+                showAbstractContent(call, state, content.content)
+                showStyle(call, state, content.style)
+            }
             is AbstractChapters -> showAbstractChapters(call, state, content)
             UndefinedTextContent -> doNothing()
         }
@@ -50,6 +62,7 @@ private fun HtmlBlockTag.showAbstractChapters(
         .withIndex()
         .forEach { showAbstractChapter(call, state, it.value, it.index) }
     field("Total Pages", chapters.chapters.sumOf { it.content.pages })
+    showStyle(call, state, chapters.style)
 }
 
 private fun HtmlBlockTag.showAbstractChapter(
@@ -73,6 +86,16 @@ private fun HtmlBlockTag.showAbstractContent(
     fieldIdList(call, state, content.spells)
 }
 
+private fun HtmlBlockTag.showStyle(
+    call: ApplicationCall,
+    state: State,
+    style: ContentStyle,
+) {
+    showFontOption(call, state, "Main Font", style.main)
+    showFontOption(call, state, "Title Font", style.title)
+    fieldFactor("Margin", style.margin)
+}
+
 // edit
 
 fun FORM.editTextContent(
@@ -84,19 +107,31 @@ fun FORM.editTextContent(
 
         when (content) {
             UndefinedTextContent -> doNothing()
-            is AbstractText -> editAbstractContent(state, content.content, CONTENT)
-            is AbstractChapters -> editList(
-                "Chapter",
-                CONTENT,
-                content.chapters,
-                0,
-                100,
-                1
-            ) { index, chapterParam, chapter ->
-                editAbstractChapter(state, chapter, index, chapterParam)
+            is AbstractText -> {
+                editAbstractContent(state, content.content, CONTENT)
+                editStyle(state, content.style, CONTENT)
             }
+
+            is AbstractChapters -> editAbstractChapters(state, content)
         }
     }
+}
+
+private fun DETAILS.editAbstractChapters(
+    state: State,
+    content: AbstractChapters,
+) {
+    editList(
+        "Chapter",
+        CONTENT,
+        content.chapters,
+        0,
+        100,
+        1
+    ) { index, chapterParam, chapter ->
+        editAbstractChapter(state, chapter, index, chapterParam)
+    }
+    editStyle(state, content.style, CONTENT)
 }
 
 private fun HtmlBlockTag.editAbstractChapter(
@@ -130,6 +165,24 @@ private fun HtmlBlockTag.editSpells(
     }
 }
 
+private fun HtmlBlockTag.editStyle(
+    state: State,
+    style: ContentStyle,
+    param: String,
+) {
+    editFontOption(state, "Main Font", style.main, combine(param, MAIN))
+    editFontOption(state, "Title Font", style.title, combine(param, TITLE))
+    selectFactor(
+        "Margin",
+        combine(param, SIDE),
+        style.margin,
+        MIN_MARGIN,
+        MAX_MARGIN,
+        fromPermille(1),
+        true
+    )
+}
+
 // parse
 
 fun parseTextContent(parameters: Parameters) = when (parse(parameters, CONTENT, TextContentType.Undefined)) {
@@ -161,4 +214,5 @@ private fun parseAbstractContent(parameters: Parameters, param: String) = Abstra
 private fun parseContentStyle(parameters: Parameters, param: String) = ContentStyle(
     parseFontOption(parameters, combine(param, MAIN)),
     parseFontOption(parameters, combine(param, TITLE)),
+    parseFactor(parameters, combine(param, SIDE), DEFAULT_MARGIN),
 )
--- src/main/kotlin/at/orchaldir/gm/core/model/item/text/content/ContentStyle.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/text/content/ContentStyle.kt
@@ -6,9 +6,20 @@ import at.orchaldir.gm.utils.math.Factor
 import at.orchaldir.gm.utils.math.unit.Distance
 import kotlinx.serialization.Serializable
 
+val MIN_MARGIN = Factor.fromPercentage(1)
+val DEFAULT_MARGIN = Factor.fromPercentage(10)
+val MAX_MARGIN = Factor.fromPercentage(20)
+
 @Serializable
 data class ContentStyle(
     val main: FontOption = SolidFont(Distance.fromMillimeters(5)),
     val title: FontOption = SolidFont(Distance.fromMillimeters(10)),
-    val margin: Factor = Factor.fromPercentage(10),
-)
+    val margin: Factor = DEFAULT_MARGIN,
+) {
+
+    init {
+        require(margin >= MIN_MARGIN) { "Margin is too small!" }
+        require(margin <= MAX_MARGIN) { "Margin is too large!" }
+    }
+
+}
