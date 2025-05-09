d8c0661ce63f5c666a0da2aba29918dc0b6b369d
Orchaldir <orchaldir@gmail.com>
2025-05-09 15:25:46 +0200

    Add visualizeTocLine()

--- src/main/kotlin/at/orchaldir/gm/visualization/text/content/TableOfContents.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/text/content/TableOfContents.kt
@@ -4,6 +4,8 @@ import at.orchaldir.gm.core.model.item.text.content.AbstractChapter
 import at.orchaldir.gm.core.model.item.text.content.AbstractChapters
 import at.orchaldir.gm.core.model.item.text.content.NoTableOfContents
 import at.orchaldir.gm.core.model.item.text.content.SimpleTableOfContents
+import at.orchaldir.gm.core.model.item.text.content.TocData
+import at.orchaldir.gm.core.model.item.text.content.TocLine
 import at.orchaldir.gm.core.model.name.NotEmptyString
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.renderer.model.RenderStringOptions
@@ -22,6 +24,8 @@ fun visualizeTableOfContents(
             chapters.chapters,
             titleOptions,
             mainOptions,
+            toc.data,
+            toc.line,
         )
     }
 }
@@ -32,9 +36,37 @@ private fun visualizeTableOfContents(
     chapters: List<AbstractChapter>,
     titleOptions: RenderStringOptions,
     mainOptions: RenderStringOptions,
+    data: TocData,
+    line: TocLine,
 ) {
     builder
         .addParagraph(title.text, titleOptions)
         .addBreak(mainOptions.size)
-        .addPageBreak()
+    var page = 2
+
+    chapters.forEach { chapter ->
+        visualizeTocLine(
+            builder,
+            chapter,
+            mainOptions,
+            data,
+            line,
+            page,
+        )
+
+        page += chapter.content.pages
+    }
+
+    builder.addPageBreak()
 }
+
+private fun visualizeTocLine(
+    builder: PagesBuilder,
+    chapter: AbstractChapter,
+    options: RenderStringOptions,
+    data: TocData,
+    line: TocLine,
+    page: Int,
+) = builder
+    .addParagraph("${chapter.title.text} $page", options)
+    .addBreak(options.size)
