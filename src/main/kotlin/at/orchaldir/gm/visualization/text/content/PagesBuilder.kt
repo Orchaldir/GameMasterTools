f4b09c44beac5c3752dda08b08ae714222073048
Orchaldir <orchaldir@gmail.com>
2025-05-09 15:17:57 +0200

    Add visualizeTableOfContents()

--- src/main/kotlin/at/orchaldir/gm/visualization/text/content/PagesBuilder.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/text/content/PagesBuilder.kt
@@ -1,6 +1,7 @@
 package at.orchaldir.gm.visualization.text.content
 
 import at.orchaldir.gm.core.model.item.text.content.*
+import at.orchaldir.gm.core.model.name.NotEmptyString
 import at.orchaldir.gm.core.model.util.HorizontalAlignment
 import at.orchaldir.gm.utils.math.AABB
 import at.orchaldir.gm.utils.math.Factor
--- src/main/kotlin/at/orchaldir/gm/visualization/text/content/TableOfContents.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/text/content/TableOfContents.kt
@@ -1,8 +1,10 @@
 package at.orchaldir.gm.visualization.text.content
 
+import at.orchaldir.gm.core.model.item.text.content.AbstractChapter
 import at.orchaldir.gm.core.model.item.text.content.AbstractChapters
 import at.orchaldir.gm.core.model.item.text.content.NoTableOfContents
 import at.orchaldir.gm.core.model.item.text.content.SimpleTableOfContents
+import at.orchaldir.gm.core.model.name.NotEmptyString
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.renderer.model.RenderStringOptions
 
@@ -12,8 +14,27 @@ fun visualizeTableOfContents(
     titleOptions: RenderStringOptions,
     mainOptions: RenderStringOptions,
 ) {
-    when (chapters.tableOfContents) {
+    when (val toc = chapters.tableOfContents) {
         NoTableOfContents -> doNothing()
-        is SimpleTableOfContents -> doNothing()
+        is SimpleTableOfContents -> visualizeTableOfContents(
+            builder,
+            toc.title,
+            chapters.chapters,
+            titleOptions,
+            mainOptions,
+        )
     }
+}
+
+private fun visualizeTableOfContents(
+    builder: PagesBuilder,
+    title: NotEmptyString,
+    chapters: List<AbstractChapter>,
+    titleOptions: RenderStringOptions,
+    mainOptions: RenderStringOptions,
+) {
+    builder
+        .addParagraph(title.text, titleOptions)
+        .addBreak(mainOptions.size)
+        .addPageBreak()
 }
