12679bd42ba0991bf947ad9028b70880d5dae6ff
Orchaldir <orchaldir@gmail.com>
2025-05-10 10:32:43 +0200

    Update UI

--- src/main/kotlin/at/orchaldir/gm/core/model/item/text/content/ContentStyle.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/text/content/ContentStyle.kt
@@ -11,6 +11,8 @@ import kotlinx.serialization.Serializable
 val MIN_MARGIN = Factor.fromPercentage(1)
 val DEFAULT_MARGIN = Factor.fromPercentage(10)
 val MAX_MARGIN = Factor.fromPercentage(20)
+const val MIN_PARAGRAPH_LENGTH = 4
+const val MAX_PARAGRAPH_LENGTH = 10
 
 @Serializable
 data class ContentStyle(
@@ -19,13 +21,16 @@ data class ContentStyle(
     val isJustified: Boolean = true,
     val margin: Factor = DEFAULT_MARGIN,
     val initials: Initials = NormalInitials,
-    val minParagraphLength: Int = 4,
-    val maxParagraphLength: Int = 10,
+    val minParagraphLength: Int = MIN_PARAGRAPH_LENGTH,
+    val maxParagraphLength: Int = MAX_PARAGRAPH_LENGTH,
 ) {
 
     init {
         require(margin >= MIN_MARGIN) { "Margin is too small!" }
         require(margin <= MAX_MARGIN) { "Margin is too large!" }
+        require(maxParagraphLength >= minParagraphLength) {
+            "The max paragraph length must be greater or equal than the min!"
+        }
     }
 
     fun contains(font: FontId) = main.font() == font ||
