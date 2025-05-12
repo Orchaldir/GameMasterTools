898c6d7907aee4edcbe69156a95e280071895e81
Orchaldir <orchaldir@gmail.com>
2025-05-12 16:55:30 +0200

    Add Quote

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/quote/Quote.kt
@@ -0,0 +1,38 @@
+package at.orchaldir.gm.core.model.quote
+
+import at.orchaldir.gm.core.model.name.NotEmptyString
+import at.orchaldir.gm.core.model.time.date.Date
+import at.orchaldir.gm.core.model.util.Created
+import at.orchaldir.gm.core.model.util.Creator
+import at.orchaldir.gm.core.model.util.ElementWithSimpleName
+import at.orchaldir.gm.core.model.util.HasStartDate
+import at.orchaldir.gm.core.model.util.UndefinedCreator
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val QUOTE_TYPE = "Quote"
+
+@JvmInline
+@Serializable
+value class QuoteId(val value: Int) : Id<QuoteId> {
+
+    override fun next() = QuoteId(value + 1)
+    override fun type() = QUOTE_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class Quote(
+    val id: QuoteId,
+    val text: NotEmptyString,
+    val source: Creator = UndefinedCreator,
+    val date: Date? = null,
+) : ElementWithSimpleName<QuoteId>, Created, HasStartDate {
+
+    override fun id() = id
+    override fun name() = text.text
+    override fun creator() = source
+    override fun startDate() = date
+
+}
