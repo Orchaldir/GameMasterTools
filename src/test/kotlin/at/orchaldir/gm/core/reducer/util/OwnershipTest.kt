ea0be39045dd5bb0904667c9c93cd75976bd257f
Orchaldir <orchaldir@gmail.com>
2025-05-14 21:16:06 +0200

    Add test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/util/OwnershipTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/util/OwnershipTest.kt
@@ -1,19 +1,23 @@
 package at.orchaldir.gm.core.reducer.util
 
 import at.orchaldir.gm.*
+import at.orchaldir.gm.core.action.DeleteCharacter
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.character.Character
 import at.orchaldir.gm.core.model.economy.business.Business
 import at.orchaldir.gm.core.model.organization.Organization
 import at.orchaldir.gm.core.model.realm.Realm
 import at.orchaldir.gm.core.model.util.*
+import at.orchaldir.gm.core.model.world.building.Building
 import at.orchaldir.gm.core.model.world.street.StreetTemplate
 import at.orchaldir.gm.core.model.world.town.StreetTile
 import at.orchaldir.gm.core.model.world.town.Town
 import at.orchaldir.gm.core.model.world.town.TownTile
+import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
 import at.orchaldir.gm.utils.map.MapSize2d
 import at.orchaldir.gm.utils.map.TileMap2d
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class OwnerTest {
@@ -56,192 +60,212 @@ class OwnerTest {
         HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1),
     )
 
-    @Test
-    fun `Owner is an unknown business`() {
-        val state = STATE.removeStorage(BUSINESS_ID_0)
+    @Nested
+    inner class CanDeleteOwnerTest {
+        private val action = DeleteCharacter(CHARACTER_ID_2)
 
-        assertIllegalArgument("Cannot use an unknown Business 0 as owner!") {
-            checkOwnership(state, OWNED_BY_BUSINESS, DAY0)
+        @Test
+        fun `Owns a building`() {
+            val ownership = History<Owner>(OwnedByCharacter(CHARACTER_ID_2))
+            val building = Building(BUILDING_ID_0, ownership = ownership)
+            val newState = STATE.updateStorage(Storage(building))
+
+            assertIllegalArgument("Cannot delete Character 2, because of owned elements (Building)!") {
+                REDUCER.invoke(newState, action)
+            }
         }
     }
 
-    @Test
-    fun `Owner is an unknown character`() {
-        val state = STATE.removeStorage(CHARACTER_ID_0)
+    @Nested
+    inner class CheckOwnershipTest {
 
-        assertIllegalArgument("Cannot use an unknown Character 2 as owner!") {
-            checkOwnership(state, OWNED_BY_CHARACTER, DAY0)
+        @Test
+        fun `Owner is an unknown business`() {
+            val state = STATE.removeStorage(BUSINESS_ID_0)
+
+            assertIllegalArgument("Cannot use an unknown Business 0 as owner!") {
+                checkOwnership(state, OWNED_BY_BUSINESS, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Owner is an unknown organization`() {
-        val state = STATE.removeStorage(ORGANIZATION_ID_0)
+        @Test
+        fun `Owner is an unknown character`() {
+            val state = STATE.removeStorage(CHARACTER_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Organization 0 as owner!") {
-            checkOwnership(state, OWNED_BY_ORGANIZATION, DAY0)
+            assertIllegalArgument("Cannot use an unknown Character 2 as owner!") {
+                checkOwnership(state, OWNED_BY_CHARACTER, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Owner is an unknown Realm`() {
-        val state = STATE.removeStorage(REALM_ID_0)
+        @Test
+        fun `Owner is an unknown organization`() {
+            val state = STATE.removeStorage(ORGANIZATION_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Realm 0 as owner!") {
-            checkOwnership(state, OWNED_BY_REALM, DAY0)
+            assertIllegalArgument("Cannot use an unknown Organization 0 as owner!") {
+                checkOwnership(state, OWNED_BY_ORGANIZATION, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Owner is an unknown town`() {
-        val state = STATE.removeStorage(TOWN_ID_0)
+        @Test
+        fun `Owner is an unknown Realm`() {
+            val state = STATE.removeStorage(REALM_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Town 0 as owner!") {
-            checkOwnership(state, OWNED_BY_TOWN, DAY0)
+            assertIllegalArgument("Cannot use an unknown Realm 0 as owner!") {
+                checkOwnership(state, OWNED_BY_REALM, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Previous owner is an unknown character`() {
-        val state = STATE.removeStorage(CHARACTER_ID_0)
+        @Test
+        fun `Owner is an unknown town`() {
+            val state = STATE.removeStorage(TOWN_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Character 2 as 1.previous owner!") {
-            checkOwnership(state, CHARACTER_AS_PREVIOUS, DAY0)
+            assertIllegalArgument("Cannot use an unknown Town 0 as owner!") {
+                checkOwnership(state, OWNED_BY_TOWN, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Previous owner is an unknown organization`() {
-        val state = STATE.removeStorage(ORGANIZATION_ID_0)
+        @Test
+        fun `Previous owner is an unknown character`() {
+            val state = STATE.removeStorage(CHARACTER_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Organization 0 as 1.previous owner!") {
-            checkOwnership(state, ORGANIZATION_AS_PREVIOUS, DAY0)
+            assertIllegalArgument("Cannot use an unknown Character 2 as 1.previous owner!") {
+                checkOwnership(state, CHARACTER_AS_PREVIOUS, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Previous owner is an unknown town`() {
-        val state = STATE.removeStorage(TOWN_ID_0)
+        @Test
+        fun `Previous owner is an unknown organization`() {
+            val state = STATE.removeStorage(ORGANIZATION_ID_0)
 
-        assertIllegalArgument("Cannot use an unknown Town 0 as 1.previous owner!") {
-            checkOwnership(state, TOWN_AS_PREVIOUS, DAY0)
+            assertIllegalArgument("Cannot use an unknown Organization 0 as 1.previous owner!") {
+                checkOwnership(state, ORGANIZATION_AS_PREVIOUS, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `First Previous ownership ended before the construction`() {
-        assertIllegalArgument("1.previous owner's until is too early!") {
-            checkOwnership(
-                STATE,
-                CHARACTER_AS_PREVIOUS,
-                DAY2
+        @Test
+        fun `Previous owner is an unknown town`() {
+            val state = STATE.removeStorage(TOWN_ID_0)
+
+            assertIllegalArgument("Cannot use an unknown Town 0 as 1.previous owner!") {
+                checkOwnership(state, TOWN_AS_PREVIOUS, DAY0)
+            }
+        }
+
+        @Test
+        fun `First Previous ownership ended before the construction`() {
+            assertIllegalArgument("1.previous owner's until is too early!") {
+                checkOwnership(
+                    STATE,
+                    CHARACTER_AS_PREVIOUS,
+                    DAY2
+                )
+            }
+        }
+
+        @Test
+        fun `A previous ownership ended before the one before it`() {
+            val ownership = History(
+                OwnedByTown(TOWN_ID_0),
+                listOf(
+                    HistoryEntry(OwnedByCharacter(CHARACTER_ID_2), DAY2),
+                    HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1)
+                )
             )
+
+            assertIllegalArgument("2.previous owner's until is too early!") {
+                checkOwnership(STATE, ownership, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `A previous ownership ended before the one before it`() {
-        val ownership = History(
-            OwnedByTown(TOWN_ID_0),
-            listOf(
-                HistoryEntry(OwnedByCharacter(CHARACTER_ID_2), DAY2),
-                HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1)
-            )
-        )
+        @Test
+        fun `Character owns a building before his birth`() {
+            val state = STATE.updateStorage(Storage(Character(CHARACTER_ID_2, birthDate = DAY1)))
 
-        assertIllegalArgument("2.previous owner's until is too early!") {
-            checkOwnership(STATE, ownership, DAY0)
+            assertIllegalArgument("The owner didn't exist at the start of their ownership!") {
+                checkOwnership(state, OWNED_BY_CHARACTER, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `Character owns a building before his birth`() {
-        val state = STATE.updateStorage(Storage(Character(CHARACTER_ID_2, birthDate = DAY1)))
+        @Test
+        fun `First owner didn't exist yet`() {
+            val state = STATE.updateStorage(Storage(Town(TOWN_ID_0, foundingDate = DAY1)))
 
-        assertIllegalArgument("The owner didn't exist at the start of their ownership!") {
-            checkOwnership(state, OWNED_BY_CHARACTER, DAY0)
+            assertIllegalArgument("The 1.previous owner didn't exist at the start of their ownership!") {
+                checkOwnership(state, TOWN_AS_PREVIOUS, DAY0)
+            }
         }
-    }
 
-    @Test
-    fun `First owner didn't exist yet`() {
-        val state = STATE.updateStorage(Storage(Town(TOWN_ID_0, foundingDate = DAY1)))
-
-        assertIllegalArgument("The 1.previous owner didn't exist at the start of their ownership!") {
-            checkOwnership(state, TOWN_AS_PREVIOUS, DAY0)
-        }
-    }
-
-    @Test
-    fun `Second owner didn't exist yet`() {
-        val ownership = History(
-            NoOwner,
-            listOf(
-                HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1),
-                HistoryEntry(OwnedByCharacter(CHARACTER_ID_2), DAY2)
-            )
-        )
-        val state = STATE.updateStorage(Storage(Character(CHARACTER_ID_2, birthDate = DAY2)))
-
-        assertIllegalArgument("The 2.previous owner didn't exist at the start of their ownership!") {
-            checkOwnership(state, ownership, DAY0)
-        }
-    }
-
-    @Test
-    fun `Successfully updated with business as owner`() {
-        testSuccess(OWNED_BY_BUSINESS)
-    }
-
-    @Test
-    fun `Successfully updated with character as owner`() {
-        testSuccess(OWNED_BY_CHARACTER)
-    }
-
-    @Test
-    fun `Successfully updated with organization as owner`() {
-        testSuccess(OWNED_BY_ORGANIZATION)
-    }
-
-    @Test
-    fun `Successfully updated with realm as owner`() {
-        testSuccess(OWNED_BY_REALM)
-    }
-
-    @Test
-    fun `Successfully updated with town as owner`() {
-        testSuccess(OWNED_BY_TOWN)
-    }
-
-    @Test
-    fun `Successfully updated with character as previous owner`() {
-        testSuccess(CHARACTER_AS_PREVIOUS)
-    }
-
-    @Test
-    fun `Successfully updated with organization as previous owner`() {
-        testSuccess(ORGANIZATION_AS_PREVIOUS)
-    }
-
-    @Test
-    fun `Successfully updated with town as previous owner`() {
-        testSuccess(TOWN_AS_PREVIOUS)
-    }
-
-    @Test
-    fun `Successfully updated with 2 previous owners`() {
-        testSuccess(
-            History(
+        @Test
+        fun `Second owner didn't exist yet`() {
+            val ownership = History(
                 NoOwner,
                 listOf(
                     HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1),
                     HistoryEntry(OwnedByCharacter(CHARACTER_ID_2), DAY2)
                 )
             )
-        )
-    }
+            val state = STATE.updateStorage(Storage(Character(CHARACTER_ID_2, birthDate = DAY2)))
 
-    private fun testSuccess(ownership: History<Owner>) {
-        checkOwnership(STATE, ownership, DAY0)
+            assertIllegalArgument("The 2.previous owner didn't exist at the start of their ownership!") {
+                checkOwnership(state, ownership, DAY0)
+            }
+        }
+
+        @Test
+        fun `Successfully updated with business as owner`() {
+            testSuccess(OWNED_BY_BUSINESS)
+        }
+
+        @Test
+        fun `Successfully updated with character as owner`() {
+            testSuccess(OWNED_BY_CHARACTER)
+        }
+
+        @Test
+        fun `Successfully updated with organization as owner`() {
+            testSuccess(OWNED_BY_ORGANIZATION)
+        }
+
+        @Test
+        fun `Successfully updated with realm as owner`() {
+            testSuccess(OWNED_BY_REALM)
+        }
+
+        @Test
+        fun `Successfully updated with town as owner`() {
+            testSuccess(OWNED_BY_TOWN)
+        }
+
+        @Test
+        fun `Successfully updated with character as previous owner`() {
+            testSuccess(CHARACTER_AS_PREVIOUS)
+        }
+
+        @Test
+        fun `Successfully updated with organization as previous owner`() {
+            testSuccess(ORGANIZATION_AS_PREVIOUS)
+        }
+
+        @Test
+        fun `Successfully updated with town as previous owner`() {
+            testSuccess(TOWN_AS_PREVIOUS)
+        }
+
+        @Test
+        fun `Successfully updated with 2 previous owners`() {
+            testSuccess(
+                History(
+                    NoOwner,
+                    listOf(
+                        HistoryEntry(OwnedByTown(TOWN_ID_0), DAY1),
+                        HistoryEntry(OwnedByCharacter(CHARACTER_ID_2), DAY2)
+                    )
+                )
+            )
+        }
+
+        private fun testSuccess(ownership: History<Owner>) {
+            checkOwnership(STATE, ownership, DAY0)
+        }
     }
 }
