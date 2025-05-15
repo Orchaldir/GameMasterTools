0205a2cc244c692d92c2ad223ed1db1d57d9df21
Orchaldir <orchaldir@gmail.com>
2025-05-15 16:22:30 +0200

    Add War

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/realm/War.kt
@@ -0,0 +1,37 @@
+package at.orchaldir.gm.core.model.realm
+
+import at.orchaldir.gm.core.model.name.Name
+import at.orchaldir.gm.core.model.source.DataSourceId
+import at.orchaldir.gm.core.model.source.HasDataSources
+import at.orchaldir.gm.core.model.time.date.Date
+import at.orchaldir.gm.core.model.util.*
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val WAR_TYPE = "War"
+
+@JvmInline
+@Serializable
+value class WarId(val value: Int) : Id<WarId> {
+
+    override fun next() = WarId(value + 1)
+    override fun type() = WAR_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class War(
+    val id: WarId,
+    val name: Name = Name.init("War ${id.value}"),
+    val startDate: Date? = null,
+    val endDate: Date? = null,
+    val sources: Set<DataSourceId> = emptySet(),
+) : ElementWithSimpleName<WarId>, HasDataSources, HasStartDate {
+
+    override fun id() = id
+    override fun name() = name.text
+    override fun sources() = sources
+    override fun startDate() = startDate
+
+}
