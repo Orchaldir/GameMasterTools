5abadd8ba8f39b95ddad21cf1950fe41fcb24f90
Orchaldir <orchaldir@gmail.com>
2025-05-16 09:01:49 +0200

    tests

--- src/main/kotlin/at/orchaldir/gm/core/selector/realm/Catastrophe.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/realm/Catastrophe.kt
@@ -3,6 +3,6 @@ package at.orchaldir.gm.core.selector.realm
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.realm.CatastropheId
 
-fun State.canDeleteCatastrophe(catastrophe: CatastropheId) = false
+fun State.canDeleteCatastrophe(catastrophe: CatastropheId) = true
 
 
