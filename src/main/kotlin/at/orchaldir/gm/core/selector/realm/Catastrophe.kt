5a0c628c307e36e28be58f3368889507c5be1e88
Orchaldir <orchaldir@gmail.com>
2025-05-16 09:13:11 +0200

    Add DeathByCatastrophe

--- src/main/kotlin/at/orchaldir/gm/core/selector/realm/Catastrophe.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/realm/Catastrophe.kt
@@ -2,7 +2,9 @@ package at.orchaldir.gm.core.selector.realm
 
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.realm.CatastropheId
+import at.orchaldir.gm.core.model.time.date.Date
+import at.orchaldir.gm.core.selector.util.getExistingElements
 
 fun State.canDeleteCatastrophe(catastrophe: CatastropheId) = true
 
-
+fun State.getExistingCatastrophes(date: Date?) = getExistingElements(getCatastropheStorage().getAll(), date)
