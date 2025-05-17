8287b78622c007562cc884d4136fabb510e43d6f
Orchaldir <orchaldir@gmail.com>
2025-05-17 08:40:46 +0200

    tests

--- src/test/kotlin/at/orchaldir/gm/TestConstants.kt
+++ src/test/kotlin/at/orchaldir/gm/TestConstants.kt
@@ -30,6 +30,7 @@ import at.orchaldir.gm.core.model.quote.QuoteId
 import at.orchaldir.gm.core.model.race.RaceId
 import at.orchaldir.gm.core.model.race.appearance.RaceAppearanceId
 import at.orchaldir.gm.core.model.realm.CatastropheId
+import at.orchaldir.gm.core.model.realm.LegalCodeId
 import at.orchaldir.gm.core.model.realm.RealmId
 import at.orchaldir.gm.core.model.realm.TownId
 import at.orchaldir.gm.core.model.realm.WarId
@@ -93,6 +94,8 @@ val UNKNOWN_JOB_ID = JobId(99)
 val LANGUAGE_ID_0 = LanguageId(0)
 val LANGUAGE_ID_1 = LanguageId(1)
 val UNKNOWN_LANGUAGE_ID = LanguageId(99)
+val LEGAL_CODE_ID_0 = LegalCodeId(0)
+val UNKNOWN_LEGAL_CODE_ID = LegalCodeId(99)
 val MAGIC_TRADITION_ID_0 = MagicTraditionId(0)
 val UNKNOWN_MAGIC_TRADITION_ID = MagicTraditionId(99)
 val MATERIAL_ID_0 = MaterialId(0)
