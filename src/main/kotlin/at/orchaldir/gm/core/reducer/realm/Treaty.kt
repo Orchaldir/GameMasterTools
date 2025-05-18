076c943e99a77a2da7b95c067f96f2455976bae4
Orchaldir <orchaldir@gmail.com>
2025-05-18 21:13:39 +0200

    test

--- src/main/kotlin/at/orchaldir/gm/core/reducer/realm/Treaty.kt
+++ src/main/kotlin/at/orchaldir/gm/core/reducer/realm/Treaty.kt
@@ -5,9 +5,12 @@ import at.orchaldir.gm.core.action.DeleteTreaty
 import at.orchaldir.gm.core.action.UpdateTreaty
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.realm.Treaty
+import at.orchaldir.gm.core.model.realm.TreatyParticipant
+import at.orchaldir.gm.core.model.time.date.Date
 import at.orchaldir.gm.core.reducer.util.checkDate
 import at.orchaldir.gm.core.reducer.util.validateCanDelete
 import at.orchaldir.gm.core.selector.realm.canDeleteTreaty
+import at.orchaldir.gm.core.selector.util.requireExists
 import at.orchaldir.gm.utils.redux.Reducer
 import at.orchaldir.gm.utils.redux.noFollowUps
 
@@ -37,4 +40,13 @@ val UPDATE_TREATY: Reducer<UpdateTreaty, State> = { state, action ->
 fun validateTreaty(state: State, treaty: Treaty) {
     checkDate(state, treaty.date, "Treaty")
     state.getDataSourceStorage().require(treaty.sources)
+    treaty.participants.forEach { validateParticipant(state, it, treaty.date) }
+}
+
+private fun validateParticipant(state: State, participant: TreatyParticipant, date: Date?) {
+    state.requireExists(state.getRealmStorage(), participant.realm, date)
+
+    if (participant.signature != null) {
+        state.requireExists(state.getCharacterStorage(), participant.signature, date)
+    }
 }
