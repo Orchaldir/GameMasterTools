dfe34828837c54eaf693a8ba68ba01fc569ba007
Orchaldir <orchaldir@gmail.com>
2025-05-19 10:47:53 +0200

    fix test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/world/town/TownMapTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/world/town/TownMapTest.kt
@@ -74,13 +74,13 @@ class TownMapTest {
             @Test
             fun `Hill must exist`() {
                 val terrain = HillTerrain(UNKNOWN_MOUNTAIN_ID)
-                testValid("Mountain", TownTile(terrain))
+                testValid("Region", TownTile(terrain))
             }
 
             @Test
             fun `Mountain must exist`() {
                 val terrain = MountainTerrain(UNKNOWN_MOUNTAIN_ID)
-                testValid("Mountain", TownTile(terrain))
+                testValid("Region", TownTile(terrain))
             }
 
             @Test
