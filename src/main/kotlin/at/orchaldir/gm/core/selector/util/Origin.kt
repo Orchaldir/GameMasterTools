02b513ad3fbe38888c31cace76a646f0401cdb66
Orchaldir <orchaldir@gmail.com>
2025-05-21 17:10:18 +0200

    Add getChildrenOf()

--- src/main/kotlin/at/orchaldir/gm/core/selector/util/Origin.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/util/Origin.kt
@@ -1,18 +1,20 @@
 package at.orchaldir.gm.core.selector.util
 
+import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.util.HasOrigin
 import at.orchaldir.gm.utils.Element
 import at.orchaldir.gm.utils.Id
-import at.orchaldir.gm.core.model.State
 
-fun <ID, ELEMENT> State.getChildrenOf(id: ID) where ID : Id<ID>,
+fun <ID, ELEMENT> State.getChildrenOf(id: ID) where
+        ID : Id<ID>,
         ELEMENT : Element<ID>,
-                                                    ELEMENT : HasOrigin<ID> = getStorage<ID, ELEMENT>(id)
+        ELEMENT : HasOrigin<ID> = getStorage<ID, ELEMENT>(id)
     .getAll()
     .filter { it.origin().isChildOf(id) }
 
-fun <ID, ELEMENT> State.hasChildren(id: ID) where ID : Id<ID>,
+fun <ID, ELEMENT> State.hasChildren(id: ID) where
+        ID : Id<ID>,
         ELEMENT : Element<ID>,
-                                                    ELEMENT : HasOrigin<ID> = getStorage<ID, ELEMENT>(id)
+        ELEMENT : HasOrigin<ID> = getStorage<ID, ELEMENT>(id)
     .getAll()
     .any { it.origin().isChildOf(id) }
