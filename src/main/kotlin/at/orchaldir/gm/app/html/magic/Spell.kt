8bf3cc82ccdf74f9990061debd22025eb4395a8b
Orchaldir <orchaldir@gmail.com>
2025-05-21 10:09:10 +0200

    move

--- src/main/kotlin/at/orchaldir/gm/app/html/magic/Spell.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/magic/Spell.kt
@@ -5,6 +5,7 @@ import at.orchaldir.gm.app.LANGUAGE
 import at.orchaldir.gm.app.ORIGIN
 import at.orchaldir.gm.app.REFERENCE
 import at.orchaldir.gm.app.html.*
+import at.orchaldir.gm.app.html.culture.parseOptionalLanguageId
 import at.orchaldir.gm.app.html.util.*
 import at.orchaldir.gm.app.parse.combine
 import at.orchaldir.gm.app.parse.parse
