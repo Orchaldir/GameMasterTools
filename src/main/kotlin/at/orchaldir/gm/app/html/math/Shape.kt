db6c4bbe9b3f4c1b674681b1b9d440b30ebf4555
Orchaldir <orchaldir@gmail.com>
2025-05-23 21:14:19 +0200

    Add RectangularShape html

--- src/main/kotlin/at/orchaldir/gm/app/html/math/Shape.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/math/Shape.kt
@@ -4,6 +4,7 @@ import at.orchaldir.gm.app.html.field
 import at.orchaldir.gm.app.html.selectValue
 import at.orchaldir.gm.app.parse.parse
 import at.orchaldir.gm.utils.math.shape.CircularShape
+import at.orchaldir.gm.utils.math.shape.RectangularShape
 import io.ktor.http.*
 import kotlinx.html.HtmlBlockTag
 
@@ -11,6 +12,13 @@ import kotlinx.html.HtmlBlockTag
 
 fun HtmlBlockTag.showCircularShape(
     shape: CircularShape,
+    label: String = "Shape",
+) {
+    field(label, shape)
+}
+
+fun HtmlBlockTag.showRectangularShape(
+    shape: RectangularShape,
     label: String = "Shape",
 ) {
     field(label, shape)
@@ -26,10 +34,24 @@ fun HtmlBlockTag.selectCircularShape(
     selectValue(label, param, CircularShape.entries, shape)
 }
 
+fun HtmlBlockTag.selectRectangularShape(
+    shape: RectangularShape,
+    param: String,
+    label: String = "Shape",
+) {
+    selectValue(label, param, RectangularShape.entries, shape)
+}
+
 // parse
 
 fun parseCircularShape(
     parameters: Parameters,
     param: String,
     default: CircularShape = CircularShape.Circle,
+) = parse(parameters, param, default)
+
+fun parseRectangularShape(
+    parameters: Parameters,
+    param: String,
+    default: RectangularShape = RectangularShape.Rectangle,
 ) = parse(parameters, param, default)
