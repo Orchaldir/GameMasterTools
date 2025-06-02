d0d1348ebd2158786623cba3043b97606b34c000
Orchaldir <orchaldir@gmail.com>
2025-06-02 11:50:25 +0200

    Add SwordHilt

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/style/SwordHilt.kt
@@ -0,0 +1,39 @@
+package at.orchaldir.gm.core.model.item.equipment.style
+
+import at.orchaldir.gm.core.model.util.Size
+import at.orchaldir.gm.core.model.util.part.MadeFromParts
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class SwordHiltType {
+    Simple,
+    WithoutGuard,
+}
+
+@Serializable
+sealed class SwordHilt : MadeFromParts {
+
+    fun getType() = when (this) {
+        is HiltWithoutGuard -> SwordHiltType.WithoutGuard
+        is SimpleHilt -> SwordHiltType.Simple
+    }
+
+    override fun parts() = when (this) {
+        is HiltWithoutGuard -> pommel.parts()
+        is SimpleHilt -> pommel.parts()
+    }
+}
+
+@Serializable
+@SerialName("WithoutGuard")
+data class HiltWithoutGuard(
+    val pommel: Ornament = SimpleOrnament(),
+    val pommelSize: Size = Size.Medium,
+) : SwordHilt()
+
+@Serializable
+@SerialName("Simple")
+data class SimpleHilt(
+    val pommel: Ornament = SimpleOrnament(),
+    val pommelSize: Size = Size.Medium,
+) : SwordHilt()
