c31762d81b4759bf69b9fd4a11ab704cfd180a97
Orchaldir <orchaldir@gmail.com>
2025-06-04 14:24:09 +0200

    add createSkullCapPolygon()

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Config.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/Config.kt
@@ -110,6 +110,7 @@ val CHARACTER_CONFIG = CharacterRenderConfig(
             fromPercentage(170),
         ),
         HelmetConfig(
+            fromPercentage(30),
             fromPercentage(80),
             fromPercentage(5),
         ),
--- src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/Helmet.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/Helmet.kt
@@ -2,11 +2,14 @@ package at.orchaldir.gm.visualization.character.equipment
 
 import at.orchaldir.gm.core.model.item.equipment.Helmet
 import at.orchaldir.gm.core.model.item.equipment.style.ChainmailHood
+import at.orchaldir.gm.core.model.item.equipment.style.HelmetShape
 import at.orchaldir.gm.core.model.item.equipment.style.SkullCap
 import at.orchaldir.gm.utils.doNothing
+import at.orchaldir.gm.utils.math.AABB
 import at.orchaldir.gm.utils.math.END
 import at.orchaldir.gm.utils.math.FULL
 import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.Polygon2d
 import at.orchaldir.gm.utils.math.Polygon2dBuilder
 import at.orchaldir.gm.utils.math.START
 import at.orchaldir.gm.utils.renderer.LayerRenderer
@@ -14,33 +17,39 @@ import at.orchaldir.gm.visualization.character.CharacterRenderState
 import at.orchaldir.gm.visualization.character.appearance.HAND_LAYER
 
 data class HelmetConfig(
+    val frontBottomY: Factor,
     val hoodOpeningWidth: Factor,
     val padding: Factor,
-)
+) {
+
+    fun getHelmWidth() = FULL + padding * 2
+
+}
 
 fun visualizeHelmet(
     state: CharacterRenderState,
     helmet: Helmet,
 ) {
     val renderer = state.renderer.getLayer(HAND_LAYER)
+    val config = state.config.equipment.helmet
 
     when (helmet.style) {
-        is ChainmailHood -> visualizeChainmailHood(state, renderer, helmet.style)
-        is SkullCap -> doNothing()
+        is ChainmailHood -> visualizeChainmailHood(state, renderer, config, helmet.style)
+        is SkullCap -> visualizeSkullCap(state, renderer, config, helmet.style)
     }
 }
 
 private fun visualizeChainmailHood(
     state: CharacterRenderState,
     renderer: LayerRenderer,
+    config: HelmetConfig,
     hood: ChainmailHood,
 ) {
     val color = hood.part.getColor(state.state, state.colors)
     val options = state.config.getLineOptions(color)
 
     if (state.renderFront) {
-        val config = state.config.equipment.helmet
-        val hoodWidth = FULL + config.padding * 2
+        val hoodWidth = config.getHelmWidth()
         val hoodOpeningWidth = config.hoodOpeningWidth
         val polygon = Polygon2dBuilder()
             .addMirroredPoints(state.aabb, hoodWidth, START, true)
@@ -53,4 +62,37 @@ private fun visualizeChainmailHood(
     } else {
         renderer.renderRectangle(state.aabb, options)
     }
+}
+
+private fun visualizeSkullCap(
+    state: CharacterRenderState,
+    renderer: LayerRenderer,
+    config: HelmetConfig,
+    cap: SkullCap,
+) {
+    val color = cap.part.getColor(state.state, state.colors)
+    val options = state.config.getLineOptions(color)
+    val polygon = createSkullCapPolygon(state.aabb, config, cap)
+
+    renderer.renderRoundedPolygon(polygon, options)
+}
+
+private fun createSkullCapPolygon(
+    aabb: AABB,
+    config: HelmetConfig,
+    cap: SkullCap,
+): Polygon2d {
+    val helmWidth = config.getHelmWidth()
+    val builder = Polygon2dBuilder()
+        .addMirroredPoints(aabb, helmWidth, config.frontBottomY, true)
+        .addMirroredPoints(aabb, helmWidth, START)
+
+    when (cap.shape) {
+        HelmetShape.Bucket -> doNothing()
+        HelmetShape.Conical -> doNothing()
+        HelmetShape.Onion -> doNothing()
+        HelmetShape.Round -> builder.addMirroredPoints(aabb, helmWidth, -config.frontBottomY)
+    }
+
+    return builder.build()
 }
