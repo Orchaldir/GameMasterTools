ff198aded89b753199e09da3630e87d9de04d22c
Orchaldir <orchaldir@gmail.com>
2025-06-04 14:41:30 +0200

    chainmail-hoods.svg

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/armour/ChainmailHoods.kt
@@ -0,0 +1,43 @@
+package at.orchaldir.gm.prototypes.visualization.character.equipment.armour
+
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.character.appearance.Body
+import at.orchaldir.gm.core.model.character.appearance.BodyShape
+import at.orchaldir.gm.core.model.character.appearance.Head
+import at.orchaldir.gm.core.model.character.appearance.HumanoidBody
+import at.orchaldir.gm.core.model.economy.material.Material
+import at.orchaldir.gm.core.model.economy.material.MaterialId
+import at.orchaldir.gm.core.model.item.equipment.BodyArmour
+import at.orchaldir.gm.core.model.item.equipment.EquipmentMap.Companion.from
+import at.orchaldir.gm.core.model.item.equipment.Helmet
+import at.orchaldir.gm.core.model.item.equipment.style.ChainmailHood
+import at.orchaldir.gm.core.model.item.equipment.style.HoodShape
+import at.orchaldir.gm.core.model.item.equipment.style.OuterwearLength
+import at.orchaldir.gm.core.model.item.equipment.style.SegmentedArmour
+import at.orchaldir.gm.core.model.util.render.Color
+import at.orchaldir.gm.prototypes.visualization.addNames
+import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
+import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTableWithoutColorScheme
+import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.math.unit.Distance
+
+fun main() {
+    renderCharacterTableWithoutColorScheme(
+        State(Storage(Material(MaterialId(0), color = Color.Silver))),
+        "chainmail-hoods.svg",
+        CHARACTER_CONFIG,
+        addNames(BodyShape.entries),
+        addNames(HoodShape.entries),
+    ) { distance, hoodShape, bodyShape ->
+        val armour = Helmet(ChainmailHood(hoodShape))
+
+        Pair(createAppearance(distance, bodyShape), from(armour))
+    }
+}
+
+private fun createAppearance(height: Distance, bodyShape: BodyShape) =
+    HumanoidBody(
+        Body(bodyShape),
+        Head(),
+        height,
+    )
