cf02fb0c48e4c186f7234e37d33d73c97691fba4
Orchaldir <orchaldir@gmail.com>
2025-06-10 09:01:32 +0200

    improve UI

--- src/main/kotlin/at/orchaldir/gm/core/reducer/item/Equipment.kt
+++ src/main/kotlin/at/orchaldir/gm/core/reducer/item/Equipment.kt
@@ -120,8 +120,8 @@ private fun checkPolearmHead(head: PolearmHead) = when (head) {
     NoPolearmHead -> doNothing()
     RoundedPolearmHead -> doNothing()
     SharpenedPolearmHead -> doNothing()
-    is PolearmHeadWithSegments -> TODO()
-    is PolearmHeadWithAxeHead -> TODO()
+    is PolearmHeadWithSegments -> doNothing()
+    is PolearmHeadWithAxeHead -> checkPolearmFixation(head.fixation)
     is PolearmHeadWithSpearHead -> {
         checkPolearmFixation(head.fixation)
         checkSpearHead(head.spear)
