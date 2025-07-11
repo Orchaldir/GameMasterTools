d666d272b9c6443870c997020b271d9418ea2868
Orchaldir <orchaldir@gmail.com>
2025-07-11 19:18:02 +0200

    add UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/util/Origin.kt
@@ -0,0 +1,161 @@
+package at.orchaldir.gm.app.html.util
+
+import at.orchaldir.gm.app.ORIGIN
+import at.orchaldir.gm.app.REFERENCE
+import at.orchaldir.gm.app.html.field
+import at.orchaldir.gm.app.html.link
+import at.orchaldir.gm.app.html.selectElement
+import at.orchaldir.gm.app.html.selectValue
+import at.orchaldir.gm.app.parse.combine
+import at.orchaldir.gm.app.parse.parse
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.time.date.Date
+import at.orchaldir.gm.core.model.util.Creator
+import at.orchaldir.gm.core.model.util.name.ElementWithSimpleName
+import at.orchaldir.gm.core.model.util.origin.*
+import at.orchaldir.gm.core.selector.util.getExistingElements
+import at.orchaldir.gm.utils.Element
+import at.orchaldir.gm.utils.Id
+import at.orchaldir.gm.utils.doNothing
+import io.ktor.http.*
+import io.ktor.server.application.*
+import kotlinx.html.HtmlBlockTag
+
+// show
+
+fun <ID : Id<ID>> HtmlBlockTag.fieldOrigin(
+    call: ApplicationCall,
+    state: State,
+    origin: Origin<ID>,
+) {
+    field("Origin") {
+        showOrigin(call, state, origin, true)
+    }
+}
+
+fun <ID : Id<ID>> HtmlBlockTag.showOrigin(
+    call: ApplicationCall,
+    state: State,
+    origin: Origin<ID>,
+    showUndefined: Boolean = false,
+) {
+    when (origin) {
+        is CreatedElement -> {
+            +"Created by "
+            showCreator(call, state, origin.creator)
+        }
+
+        is EvolvedElement -> {
+            +"Evolved from "
+            link(call, state, origin.parent)
+        }
+
+        is ModifiedElement -> showCreatorAndParent(call, state, origin.modifier, origin.parent, "modified")
+        is OriginalElement -> +"Original"
+        is UndefinedOrigin -> if (showUndefined) {
+            +"Undefined"
+        } else {
+            doNothing()
+        }
+    }
+}
+
+private fun <ID : Id<ID>> HtmlBlockTag.showCreatorAndParent(
+    call: ApplicationCall,
+    state: State,
+    creator: Creator,
+    original: ID,
+    verb: String,
+) {
+    link(call, state, original)
+    +" $verb by "
+    showCreator(call, state, creator)
+}
+
+// edit
+
+fun <ID : Id<ID>, ELEMENT : ElementWithSimpleName<ID>> HtmlBlockTag.editOrigin(
+    state: State,
+    id: ID,
+    origin: Origin<ID>,
+    date: Date?,
+) {
+    val availableParents = state.getExistingElements(state.getStorage(id), date)
+        .filter { it.id() != id }
+
+    selectValue("Origin Origin", ORIGIN, OriginType.entries, origin.getType()) { type ->
+        when (type) {
+            OriginType.Evolved, OriginType.Modified -> availableParents.isEmpty()
+            else -> false
+        }
+    }
+
+    when (origin) {
+        is CreatedElement -> selectCreator(state, id, origin.creator, date)
+        is EvolvedElement -> selectParent(state, availableParents, origin.parent)
+        is ModifiedElement -> selectInventorAndOriginal(
+            state,
+            id,
+            availableParents,
+            origin.modifier,
+            origin.parent,
+            date,
+        )
+
+        is OriginalElement -> doNothing()
+        is UndefinedOrigin -> doNothing()
+    }
+}
+
+private fun <ID : Id<ID>, ELEMENT : Element<ID>> HtmlBlockTag.selectInventorAndOriginal(
+    state: State,
+    id: ID,
+    availableParents: List<ELEMENT>,
+    creator: Creator,
+    parent: ID,
+    date: Date?,
+) {
+    selectCreator(state, id, creator, date)
+    selectParent(state, availableParents, parent)
+}
+
+private fun <ID : Id<ID>, ELEMENT : Element<ID>> HtmlBlockTag.selectParent(
+    state: State,
+    availableParents: List<ELEMENT>,
+    parent: ID,
+) {
+    selectElement(
+        state,
+        "Parent",
+        combine(ORIGIN, REFERENCE),
+        availableParents,
+        parent,
+    )
+}
+
+private fun <ID : Id<ID>> HtmlBlockTag.selectCreator(
+    state: State,
+    id: ID,
+    creator: Creator,
+    date: Date?,
+) {
+    selectCreator(state, creator, id, date, "Creator")
+}
+
+// parse
+
+fun <ID : Id<ID>> parseOrigin(parameters: Parameters, parseParent: (Parameters, String) -> ID) =
+    when (parse(parameters, ORIGIN, OriginType.Undefined)) {
+        OriginType.Created -> CreatedElement(parseCreator(parameters))
+        OriginType.Evolved -> EvolvedElement(
+            parseParent(parameters, combine(ORIGIN, REFERENCE)),
+        )
+
+        OriginType.Modified -> ModifiedElement(
+            parseParent(parameters, combine(ORIGIN, REFERENCE)),
+            parseCreator(parameters),
+        )
+
+        OriginType.Original -> OriginalElement()
+        OriginType.Undefined -> UndefinedOrigin()
+    }
