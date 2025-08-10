24eecb43dcc64132c589c2af90ed6f460dfcd01d
Orchaldir <orchaldir@gmail.com>
2025-08-10 17:25:18 +0200

    Add test

--- src/test/kotlin/at/orchaldir/gm/core/reducer/util/PopulationTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/util/PopulationTest.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.core.reducer.util
 import at.orchaldir.gm.RACE_ID_0
 import at.orchaldir.gm.RACE_ID_1
 import at.orchaldir.gm.RACE_ID_2
+import at.orchaldir.gm.UNKNOWN_RACE_ID
 import at.orchaldir.gm.assertIllegalArgument
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.race.Race
@@ -10,6 +11,7 @@ import at.orchaldir.gm.core.model.util.population.Population
 import at.orchaldir.gm.core.model.util.population.PopulationPerRace
 import at.orchaldir.gm.core.model.util.population.TotalPopulation
 import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.math.HALF
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -40,6 +42,13 @@ class PopulationTest {
         @Test
         fun `The total population must be greater than 0`() {
             assertTotalPopulation(PopulationPerRace(0, emptyMap()))
+        }
+
+        @Test
+        fun `With an unknown race`() {
+            assertIllegalArgument("Requires unknown Race 99!") {
+                state.validatePopulation(PopulationPerRace(100, mapOf(UNKNOWN_RACE_ID to HALF)))
+            }
         }
 
     }
