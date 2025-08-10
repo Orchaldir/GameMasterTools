3c50d3ff067d6f78da43197db4b7640240e3ffa2
Orchaldir <orchaldir@gmail.com>
2025-08-10 09:49:16 +0200

    Show a races' total population

--- src/main/kotlin/at/orchaldir/gm/core/selector/util/HasPopulation.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/util/HasPopulation.kt
@@ -1,5 +1,6 @@
 package at.orchaldir.gm.core.selector.util
 
+import at.orchaldir.gm.core.logger
 import at.orchaldir.gm.core.model.race.RaceId
 import at.orchaldir.gm.core.model.util.population.HasPopulation
 import at.orchaldir.gm.core.model.util.population.PopulationPerRace
@@ -37,12 +38,12 @@ fun <ID : Id<ID>, ELEMENT> getPopulationEntries(
 fun State.getTotalPopulation(race: RaceId): Int {
     val towns = getTownStorage()
         .getAll()
-        .filter { it.owner.current != null }
+        .filter { it.owner.current == null }
         .sumOf { it.population.getPopulation(race) ?: 0 }
 
     val realms = getRealmStorage()
         .getAll()
-        .filter { it.owner.current != null }
+        .filter { it.owner.current == null }
         .sumOf { it.population.getPopulation(race) ?: 0 }
 
     return towns + realms
