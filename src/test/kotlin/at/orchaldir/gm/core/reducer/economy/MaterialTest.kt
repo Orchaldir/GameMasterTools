3b286f48d440329512fb65fbcef9263cc01296fb
Orchaldir <orchaldir@gmail.com>
2025-08-15 09:24:00 +0200

    tests

--- src/test/kotlin/at/orchaldir/gm/core/reducer/economy/MaterialTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/economy/MaterialTest.kt
@@ -15,8 +15,12 @@ import at.orchaldir.gm.core.model.item.text.Text
 import at.orchaldir.gm.core.model.item.text.book.Hardcover
 import at.orchaldir.gm.core.model.util.part.FillLookupItemPart
 import at.orchaldir.gm.core.model.util.render.Color
+import at.orchaldir.gm.core.model.world.moon.Moon
 import at.orchaldir.gm.core.model.world.street.StreetTemplate
+import at.orchaldir.gm.core.model.world.terrain.Region
 import at.orchaldir.gm.core.reducer.REDUCER
+import at.orchaldir.gm.utils.Element
+import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -26,74 +30,57 @@ class MaterialTest {
 
     @Nested
     inner class DeleteTest {
+        val action = DeleteMaterial(MATERIAL_ID_0)
 
         @Test
         fun `Can delete an existing material`() {
             val state = State(Storage(Material(MATERIAL_ID_0)))
-            val action = DeleteMaterial(MATERIAL_ID_0)
 
             assertEquals(0, REDUCER.invoke(state, action).first.getMaterialStorage().getSize())
         }
 
         @Test
         fun `Cannot delete unknown id`() {
-            val action = DeleteMaterial(MATERIAL_ID_0)
-
             assertIllegalArgument("Requires unknown Material 0!") { REDUCER.invoke(State(), action) }
         }
 
         @Test
         fun `Cannot delete a material used by a currency unit`() {
-            val unit = CurrencyUnit(CURRENCY_UNIT_ID_0, format = Coin())
-            val state = State(
-                listOf(
-                    Storage(unit),
-                    Storage(Material(MATERIAL_ID_0)),
-                )
-            )
-            val action = DeleteMaterial(MATERIAL_ID_0)
-
-            assertIllegalArgument("Cannot delete Material 0, because it is used!") { REDUCER.invoke(state, action) }
+            failDeleting(CurrencyUnit(CURRENCY_UNIT_ID_0, format = Coin()))
         }
 
         @Test
         fun `Cannot delete a material used by an equipment`() {
-            val equipment = Equipment(EQUIPMENT_ID_0, data = Shirt(main = FillLookupItemPart(MATERIAL_ID_0)))
-            val state = State(
-                listOf(
-                    Storage(equipment),
-                    Storage(Material(MATERIAL_ID_0)),
-                )
-            )
-            val action = DeleteMaterial(MATERIAL_ID_0)
-
-            assertIllegalArgument("Cannot delete Material 0, because it is used!") { REDUCER.invoke(state, action) }
+            failDeleting(Equipment(EQUIPMENT_ID_0, data = Shirt(main = FillLookupItemPart(MATERIAL_ID_0))))
         }
 
         @Test
         fun `Cannot delete a material used by a street template`() {
-            val template = StreetTemplate(STREET_TYPE_ID_0, materialCost = MaterialCost(MATERIAL_ID_0))
-            val state = State(
-                listOf(
-                    Storage(template),
-                    Storage(Material(MATERIAL_ID_0)),
-                )
-            )
-            val action = DeleteMaterial(MATERIAL_ID_0)
-
-            assertIllegalArgument("Cannot delete Material 0, because it is used!") { REDUCER.invoke(state, action) }
+            failDeleting(StreetTemplate(STREET_TYPE_ID_0, materialCost = MaterialCost(MATERIAL_ID_0)))
         }
 
         @Test
         fun `Cannot delete a material used by a book`() {
-            val book = Text(TEXT_ID_0, format = Book(Hardcover(), 100))
+            failDeleting(Text(TEXT_ID_0, format = Book(Hardcover(), 100)))
+        }
+
+        @Test
+        fun `Cannot delete a material contained in a region`() {
+            failDeleting(Region(REGION_ID_0, resources = setOf(MATERIAL_ID_0)))
+        }
+
+        @Test
+        fun `Cannot delete a material contained in a moon`() {
+            failDeleting(Moon(MOON_ID_0, resources = setOf(MATERIAL_ID_0)))
+        }
+
+        fun <ID : Id<ID>, ELEMENT : Element<ID>> failDeleting(element: ELEMENT) {
             val state = State(
                 listOf(
-                    Storage(book),
+                    Storage(element),
                     Storage(Material(MATERIAL_ID_0)),
                 )
             )
-            val action = DeleteMaterial(MATERIAL_ID_0)
 
             assertIllegalArgument("Cannot delete Material 0, because it is used!") { REDUCER.invoke(state, action) }
         }
