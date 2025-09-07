2492b52473d4fed778dc1244acc1c1a9f447f968
Orchaldir <orchaldir@gmail.com>
2025-09-07 10:46:13 +0200

    Improve

--- src/main/kotlin/at/orchaldir/gm/core/model/DeleteResult.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/DeleteResult.kt
@@ -1,20 +1,24 @@
 package at.orchaldir.gm.core.model
 
+import at.orchaldir.gm.utils.Element
 import at.orchaldir.gm.utils.Id
 
 data class DeleteResult(
     val id: Id<*>,
     val elements: MutableMap<String, MutableSet<Id<*>>> = mutableMapOf(),
 ) {
-    fun <ID : Id<ID>> addElements(newElements: Collection<ID>): DeleteResult {
-        val first = newElements.firstOrNull() ?: return this
+    fun <ID : Id<ID>> addIds(ids: Collection<ID>): DeleteResult {
+        val first = ids.firstOrNull() ?: return this
 
         elements.computeIfAbsent(first.type()) { _ ->
             mutableSetOf()
-        }.addAll(newElements)
+        }.addAll(ids)
 
         return this
     }
+
+    fun <ID : Id<ID>, ELEMENT : Element<ID>> addElements(elements: Collection<ELEMENT>) =
+        addIds(elements.map { it.id() })
 
     fun countElements(): Int {
         var count = 0
--- src/main/kotlin/at/orchaldir/gm/core/selector/world/River.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/world/River.kt
@@ -6,7 +6,7 @@ import at.orchaldir.gm.core.model.world.terrain.RiverId
 import at.orchaldir.gm.core.model.world.town.TownMapId
 
 fun State.canDeleteRiver(river: RiverId) = DeleteResult(river)
-    .addElements(getTowns(river).map { it.id })
+    .addElements(getTowns(river))
 
 fun State.getRivers(town: TownMapId) = getRiverIds(town)
     .map { getRiverStorage().getOrThrow(it) }
