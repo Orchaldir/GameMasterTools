7d5966a42286221046a6aa327573200f80e97317
Orchaldir <orchaldir@gmail.com>
2025-09-07 10:14:59 +0200

    Add CannotDeleteException

--- src/main/kotlin/at/orchaldir/gm/core/model/DeleteResult.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/DeleteResult.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.core.model
 import at.orchaldir.gm.utils.Id
 
 data class DeleteResult(
+    val id: Id<*>,
     val elements: MutableMap<String, MutableSet<Id<*>>> = mutableMapOf(),
 ) {
     fun <ID : Id<ID>> addElements(newElements: Set<ID>) {
@@ -11,5 +12,21 @@ data class DeleteResult(
         elements.computeIfAbsent(first.type()) { _ ->
             mutableSetOf()
         }.addAll(newElements)
+    }
+
+    fun countElements(): Int {
+        var count = 0
+
+        elements.forEach { (_, ids) ->
+            count += ids.size
+        }
+
+        return count
+    }
+
+    fun validateCanDelete() {
+        if (elements.isNotEmpty()) {
+            throw CannotDeleteException(this)
+        }
     }
 }
