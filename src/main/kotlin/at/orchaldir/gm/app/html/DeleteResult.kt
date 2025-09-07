aa2c4adbabc2d56a4430f25f010e751c96e8b382
Orchaldir <orchaldir@gmail.com>
2025-09-07 11:08:08 +0200

    Add showDeleteResult()

--- src/main/kotlin/at/orchaldir/gm/app/html/DeleteResult.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/DeleteResult.kt
@@ -4,6 +4,7 @@ import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
 import io.ktor.server.application.*
 import kotlinx.html.HTML
+import kotlinx.html.h2
 
 fun HTML.showDeleteResult(
     call: ApplicationCall,
@@ -11,7 +12,9 @@ fun HTML.showDeleteResult(
     result: DeleteResult,
 ) {
     simpleHtml("Cannot delete ${result.id.print()}") {
-        fieldLink("Element", call, state, result.id)
+        fieldLink("Name", call, state, result.id)
+
+        h2 { +"Blocking Elements" }
 
         result.elements.forEach { (_, ids) ->
             fieldIdList(call, state, ids)
