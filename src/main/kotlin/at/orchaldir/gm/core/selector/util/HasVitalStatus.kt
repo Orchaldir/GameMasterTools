8a9290793128fac8a3f5c514b1efb2d5f8f15671
Orchaldir <orchaldir@gmail.com>
2025-09-11 20:23:41 +0200

    Cleanup

--- src/main/kotlin/at/orchaldir/gm/core/selector/util/HasVitalStatus.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/util/HasVitalStatus.kt
@@ -12,10 +12,6 @@ fun <ID : Id<ID>> State.canDeleteDestroyer(id: ID, result: DeleteResult) = resul
     .addElements(getDestroyedBy(getRealmStorage(), id))
     .addElements(getDestroyedBy(getTownStorage(), id))
 
-fun <ID : Id<ID>> State.isDestroyer(destroyer: ID) = isDestroyer(getCharacterStorage(), destroyer)
-        || isDestroyer(getRealmStorage(), destroyer)
-        || isDestroyer(getTownStorage(), destroyer)
-
 fun <ID : Id<ID>, ELEMENT, DESTROYER : Id<DESTROYER>> isDestroyer(
     storage: Storage<ID, ELEMENT>,
     destroyer: DESTROYER,
@@ -33,28 +29,3 @@ fun <ID : Id<ID>, ELEMENT, DESTROYER : Id<DESTROYER>> getDestroyedBy(
         ELEMENT : HasVitalStatus = storage
     .getAll()
     .filter { it.vitalStatus().isDestroyedBy(destroyer) }
-
-fun <ID : Id<ID>> checkIfDestroyerCanBeDeleted(
-    state: State,
-    destroyer: ID,
-) {
-    val noun = destroyer.type()
-
-    checkDestroyer(state.getCharacterStorage(), noun, destroyer)
-    checkDestroyer(state.getRealmStorage(), noun, destroyer)
-    checkDestroyer(state.getTownStorage(), noun, destroyer)
-}
-
-private fun <ID0, ID1, ELEMENT> checkDestroyer(
-    storage: Storage<ID0, ELEMENT>,
-    destroyerNoun: String,
-    id: ID1,
-) where ID0 : Id<ID0>,
-        ID1 : Id<ID1>,
-        ELEMENT : Element<ID0>,
-        ELEMENT : HasVitalStatus {
-    val destroyedNoun = storage.getType()
-    require(!isDestroyer(storage, id)) {
-        "Cannot delete $destroyerNoun ${id.value()}, because of destroyed elements ($destroyedNoun)!"
-    }
-}
