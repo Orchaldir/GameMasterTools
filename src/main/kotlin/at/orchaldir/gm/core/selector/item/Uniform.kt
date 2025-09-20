fc396ad11d94ace1bddddd996f946216c97a2a11
Orchaldir <orchaldir@gmail.com>
2025-09-20 08:48:54 +0200

    Update canDeleteUniform()

--- src/main/kotlin/at/orchaldir/gm/core/selector/item/Uniform.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/item/Uniform.kt
@@ -3,9 +3,11 @@ package at.orchaldir.gm.core.selector.item
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.item.UniformId
+import at.orchaldir.gm.core.selector.character.getCharacterTemplates
 import at.orchaldir.gm.core.selector.economy.getJobs
 
 fun State.canDeleteUniform(uniform: UniformId) = DeleteResult(uniform)
+    .addElements(getCharacterTemplates(uniform))
     .addElements(getJobs(uniform))
 
 
--- src/test/kotlin/at/orchaldir/gm/core/selector/item/UniformTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/item/UniformTest.kt
@@ -1,9 +1,13 @@
 package at.orchaldir.gm.core.selector.item
 
+import at.orchaldir.gm.CHARACTER_TEMPLATE_ID_0
+import at.orchaldir.gm.CULTURE_ID_0
 import at.orchaldir.gm.JOB_ID_0
+import at.orchaldir.gm.RACE_ID_0
 import at.orchaldir.gm.UNIFORM_ID_0
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.character.CharacterTemplate
 import at.orchaldir.gm.core.model.economy.job.Job
 import at.orchaldir.gm.core.model.item.Uniform
 import at.orchaldir.gm.core.model.util.GenderMap
@@ -30,6 +34,14 @@ class UniformTest {
             val newState = state.updateStorage(Storage(job))
 
             failCanDelete(newState, JOB_ID_0)
+        }
+
+        @Test
+        fun `Cannot delete a culture used by a character template`() {
+            val template = CharacterTemplate(CHARACTER_TEMPLATE_ID_0, race = RACE_ID_0, uniform = UNIFORM_ID_0)
+            val newState = state.updateStorage(Storage(template))
+
+            failCanDelete(newState, CHARACTER_TEMPLATE_ID_0)
         }
 
         private fun <ID : Id<ID>> failCanDelete(state: State, blockingId: ID) {
