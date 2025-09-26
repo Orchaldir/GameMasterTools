29cc12eabe300895811f6bba64fdaf1b92bd8bc8
Orchaldir <orchaldir@gmail.com>
2025-09-26 16:33:26 +0200

    Add tests

---
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/CreateTest.kt
@@ -0,0 +1,31 @@
+package at.orchaldir.gm.core.reducer
+
+import at.orchaldir.gm.*
+import at.orchaldir.gm.core.action.CreateAction
+import at.orchaldir.gm.core.action.DeleteRiver
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.world.terrain.River
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CreateTest {
+    val action = DeleteRiver(RIVER_ID_0)
+    val state = State(Storage(River(RIVER_ID_0)))
+
+    @Test
+    fun `Can create an element with the correct id`() {
+        val storage = REDUCER.invoke(State(), CreateAction(TEXT_ID_0)).first.getTextStorage()
+
+        assertEquals(1, storage.getSize())
+        assertEquals(TEXT_ID_0, storage.get(TEXT_ID_0)?.id)
+    }
+
+    @Test
+    fun `Cannot create an element with the wrong id`() {
+        val action = CreateAction(UNKNOWN_RIVER_ID)
+
+        assertIllegalArgument("Added River 99 doesn't have the next free id!") { REDUCER.invoke(state, action) }
+    }
+
+}
