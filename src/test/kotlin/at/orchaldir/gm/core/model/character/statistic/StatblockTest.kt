e6af8afbaf4d268395157bd9ed112a2c46da9230
Orchaldir <orchaldir@gmail.com>
2025-09-26 12:43:49 +0200

    Add StatblockTest

---
+++ src/test/kotlin/at/orchaldir/gm/core/model/character/statistic/StatblockTest.kt
@@ -0,0 +1,33 @@
+package at.orchaldir.gm.core.model.character.statistic
+
+import at.orchaldir.gm.STATISTIC_ID_0
+import at.orchaldir.gm.UNKNOWN_STATISTIC_ID
+import at.orchaldir.gm.assertIllegalArgument
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNull
+
+class StatblockTest {
+
+    val attribute = Statistic(STATISTIC_ID_0, data = Attribute(FixedNumber(10)))
+    private val state = State(
+        listOf(
+            Storage(attribute),
+        )
+    )
+
+    @Nested
+    inner class ResolveTest {
+
+        @Test
+        fun `Unknown statistic`() {
+            val statblock = Statblock()
+
+            assertIllegalArgument("Requires unknown Statistic 99!") { statblock.resolve(state, UNKNOWN_STATISTIC_ID) }
+        }
+
+    }
+
+}
