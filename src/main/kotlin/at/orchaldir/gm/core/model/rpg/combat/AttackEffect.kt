9e938b34c0815c3da5c1320b5d49ebfbc06a660a
Orchaldir <orchaldir@gmail.com>
2025-10-26 15:34:10 +0100

    Add AttackEffect

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/combat/AttackEffect.kt
@@ -0,0 +1,29 @@
+package at.orchaldir.gm.core.model.rpg.combat
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class AttackEffectType {
+    Damage,
+    Undefined,
+}
+
+@Serializable
+sealed class AttackEffect {
+
+    fun getType() = when (this) {
+        is Damage -> AttackEffectType.Damage
+        is UndefinedAttackEffect -> AttackEffectType.Undefined
+    }
+}
+
+@Serializable
+@SerialName("Damage")
+data class Damage(
+    val amount: AttackEffect,
+    val type: DamageTypeId,
+) : AttackEffect()
+
+@Serializable
+@SerialName("Undefined")
+data object UndefinedAttackEffect : AttackEffect()
--- src/main/kotlin/at/orchaldir/gm/core/model/rpg/combat/MeleeAttack.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/combat/MeleeAttack.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MeleeAttack(
-    val amount: DamageAmount,
+    val effect: AttackEffect = UndefinedAttackEffect,
 )
