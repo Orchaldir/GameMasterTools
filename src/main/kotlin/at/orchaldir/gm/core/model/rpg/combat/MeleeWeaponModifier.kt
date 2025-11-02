2dabe21246ae9a46ec5d65d52ec480001adc0dff
Orchaldir <orchaldir@gmail.com>
2025-11-02 15:24:00 +0100

    Data

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/combat/MeleeWeaponModifier.kt
@@ -0,0 +1,28 @@
+package at.orchaldir.gm.core.model.rpg.combat
+
+import at.orchaldir.gm.core.model.util.name.ElementWithSimpleName
+import at.orchaldir.gm.core.model.util.name.Name
+import at.orchaldir.gm.utils.Id
+import kotlinx.serialization.Serializable
+
+const val MELEE_WEAPON_MODIFIER_TYPE = "Melee Weapon Modifier"
+
+@JvmInline
+@Serializable
+value class MeleeWeaponModifierId(val value: Int) : Id<MeleeWeaponModifierId> {
+
+    override fun next() = MeleeWeaponModifierId(value + 1)
+    override fun type() = MELEE_WEAPON_MODIFIER_TYPE
+    override fun value() = value
+
+}
+
+@Serializable
+data class MeleeWeaponModifier(
+    val id: MeleeWeaponModifierId,
+    val name: Name = Name.init("$MELEE_WEAPON_MODIFIER_TYPE ${id.value}"),
+) : ElementWithSimpleName<MeleeWeaponModifierId> {
+
+    override fun id() = id
+    override fun name() = name.text
+}
