d599ddf71ef74c7743226f3aa1fa91a478e6df29
Orchaldir <orchaldir@gmail.com>
2025-11-08 19:17:56 +0100

    Damage Resistance must be greater than 0

---
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/rpg/AmorTypeTest.kt
@@ -0,0 +1,34 @@
+package at.orchaldir.gm.core.reducer.rpg
+
+import at.orchaldir.gm.*
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.rpg.combat.*
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class AmorTypeTest {
+
+    private val STATE = State(
+        listOf(
+            Storage(listOf(DamageType(DAMAGE_TYPE_ID_0))),
+        )
+    )
+
+    @Nested
+    inner class DamageResistanceTest {
+        @Test
+        fun `Damage Resistance must be greater than 0`() {
+            val protection = DamageResistance(0)
+
+            assertInvalidType(protection, "Damage Resistance needs to be greater 0!")
+        }
+    }
+
+    private fun assertInvalidType(protection: Protection, message: String) {
+        val type = ArmorType(ARMOR_TYPE_ID_0, protection = protection)
+
+        assertIllegalArgument(message) { type.validate(STATE) }
+    }
+
+}
