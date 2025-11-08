7eda575b3cc0a6898b79bfaf4afa83e02171d635
Orchaldir <orchaldir@gmail.com>
2025-11-08 19:06:50 +0100

    Cannot delete an armor type used by an equipment

---
+++ src/test/kotlin/at/orchaldir/gm/core/selector/rpg/ArmorTypeTest.kt
@@ -0,0 +1,47 @@
+package at.orchaldir.gm.core.selector.rpg
+
+import at.orchaldir.gm.EQUIPMENT_ID_0
+import at.orchaldir.gm.ARMOR_TYPE_ID_0
+import at.orchaldir.gm.core.model.DeleteResult
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.item.equipment.BodyArmour
+import at.orchaldir.gm.core.model.item.equipment.Equipment
+import at.orchaldir.gm.core.model.item.equipment.OneHandedAxe
+import at.orchaldir.gm.core.model.item.equipment.style.ScaleArmour
+import at.orchaldir.gm.core.model.rpg.combat.ArmorStats
+import at.orchaldir.gm.core.model.rpg.combat.ArmorType
+import at.orchaldir.gm.utils.Id
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ArmorTypeTest {
+
+    @Nested
+    inner class CanDeleteTest {
+        private val type = ArmorType(ARMOR_TYPE_ID_0)
+        private val state = State(
+            listOf(
+                Storage(type),
+            )
+        )
+
+        @Test
+        fun `Cannot delete an armor type used by an equipment`() {
+            val data = BodyArmour(ScaleArmour(), stats = ArmorStats(ARMOR_TYPE_ID_0))
+            val element = Equipment(EQUIPMENT_ID_0, data = data)
+            val newState = state.updateStorage(Storage(element))
+
+            failCanDelete(newState, EQUIPMENT_ID_0)
+        }
+
+        private fun <ID : Id<ID>> failCanDelete(state: State, blockingId: ID) {
+            assertEquals(
+                DeleteResult(ARMOR_TYPE_ID_0).addId(blockingId),
+                state.canDeleteArmorType(ARMOR_TYPE_ID_0)
+            )
+        }
+    }
+
+}
