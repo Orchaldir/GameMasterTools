8b1c936f557bccaabe98a0fa77c1efc418595e78
Orchaldir <orchaldir@gmail.com>
2025-11-09 11:21:00 +0100

    Implement visualizeChainMailBody()

--- src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/part/ChainMail.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/character/equipment/part/ChainMail.kt
@@ -3,34 +3,14 @@ package at.orchaldir.gm.visualization.character.equipment.part
 import at.orchaldir.gm.core.model.character.appearance.Body
 import at.orchaldir.gm.core.model.item.equipment.BodyArmour
 import at.orchaldir.gm.core.model.item.equipment.style.ChainMail
-import at.orchaldir.gm.utils.renderer.LayerRenderer
 import at.orchaldir.gm.utils.renderer.model.FillAndBorder
+import at.orchaldir.gm.utils.renderer.model.RenderOptions
 import at.orchaldir.gm.visualization.character.CharacterRenderState
 import at.orchaldir.gm.visualization.character.appearance.JACKET_LAYER
+import at.orchaldir.gm.visualization.character.equipment.createOuterwearBuilder
+import at.orchaldir.gm.visualization.renderBuilder
 
 fun visualizeChainMail(
-    state: CharacterRenderState,
-    body: Body,
-    armour: BodyArmour,
-    style: ChainMail,
-) {
-    val renderer = state.renderer.getLayer(JACKET_LAYER)
-
-    visualizeChainMailBody(state, renderer, body, armour, style)
-    visualizeArmourSleeves(state, body, armour, style)
-}
-
-private fun visualizeChainMailBody(
-    state: CharacterRenderState,
-    renderer: LayerRenderer,
-    body: Body,
-    armour: BodyArmour,
-    style: ChainMail,
-) {
-
-}
-
-private fun visualizeArmourSleeves(
     state: CharacterRenderState,
     body: Body,
     armour: BodyArmour,
@@ -39,5 +19,26 @@ private fun visualizeArmourSleeves(
     val fill = style.chain.getColor(state.state, state.colors)
     val options = FillAndBorder(fill.toRender(), state.config.line)
 
+    visualizeChainMailBody(state, options, body, armour)
+    visualizeArmourSleeves(state, options, body, armour)
+}
+
+private fun visualizeChainMailBody(
+    state: CharacterRenderState,
+    options: RenderOptions,
+    body: Body,
+    armour: BodyArmour,
+) {
+    val builder = createOuterwearBuilder(state, body, armour.length)
+
+    renderBuilder(state.renderer, builder, options, JACKET_LAYER)
+}
+
+private fun visualizeArmourSleeves(
+    state: CharacterRenderState,
+    options: RenderOptions,
+    body: Body,
+    armour: BodyArmour,
+) {
     visualizeSleeves(state, options, body, armour.sleeveStyle, JACKET_LAYER)
 }
