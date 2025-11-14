a94541251964d10a472a57c6e4c307c12a2964fe
Orchaldir <orchaldir@gmail.com>
2025-11-14 17:57:17 +0100

    Add axe example

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/weapons/Axes.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/weapons/Axes.kt
@@ -4,37 +4,39 @@ import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.economy.material.Material
 import at.orchaldir.gm.core.model.economy.material.MaterialId
 import at.orchaldir.gm.core.model.item.equipment.EquipmentMap.Companion.from
-import at.orchaldir.gm.core.model.item.equipment.Polearm
-import at.orchaldir.gm.core.model.item.equipment.style.PolearmHeadWithSpearHead
-import at.orchaldir.gm.core.model.item.equipment.style.SimpleShaft
-import at.orchaldir.gm.core.model.item.equipment.style.SpearHead
-import at.orchaldir.gm.core.model.item.equipment.style.SpearShape
+import at.orchaldir.gm.core.model.item.equipment.OneHandedAxe
+import at.orchaldir.gm.core.model.item.equipment.style.*
+import at.orchaldir.gm.core.model.util.Size.Medium
 import at.orchaldir.gm.core.model.util.part.FillLookupItemPart
 import at.orchaldir.gm.core.model.util.render.Color
-import at.orchaldir.gm.prototypes.visualization.addNames
 import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
 import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTableWithoutColorScheme
 import at.orchaldir.gm.utils.Storage
 
 fun main() {
+    val axeHeads = listOf<Pair<String,AxeHead>>(
+        Pair("Dagger", SingleBitAxeHead(DaggerAxeBlade(Medium)))
+    ).toMutableList()
+    axeHeads.addAll(BroadAxeShape.entries.map {
+        Pair("$it", SingleBitAxeHead(BroadAxeBlade(it, Medium, Medium)))
+    })
+    axeHeads.addAll(SymmetricAxeShape.entries.map {
+        Pair("$it", DoubleBitAxeHead(SymmetricAxeBlade(it, Medium)))
+    })
+
     renderCharacterTableWithoutColorScheme(
         State(Storage(Material(MaterialId(0), color = Color.Gray))),
         "axes.svg",
         CHARACTER_CONFIG,
-        addNames(SpearShape.entries),
+        axeHeads,
         FIXATION,
-    ) { distance, fixation, shape ->
-        val withSpearHead = PolearmHeadWithSpearHead(
-            SpearHead(
-                shape,
-            ),
+    ) { distance, fixation, head ->
+        val polearm = OneHandedAxe(
+            head,
             fixation,
-        )
-        val polearm = Polearm(
-            withSpearHead,
             SimpleShaft(
                 FillLookupItemPart(Color.SaddleBrown)
-            )
+            ),
         )
         Pair(createAppearance(distance), from(polearm))
     }
