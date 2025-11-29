b8c85573c76577c24d7720402c744b3b66af784f
Orchaldir <orchaldir@gmail.com>
2025-11-29 16:34:51 +0100

    Fix tests

--- src/test/kotlin/at/orchaldir/gm/core/reducer/character/EquippedTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/character/EquippedTest.kt
@@ -4,68 +4,99 @@ import at.orchaldir.gm.*
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.character.Character
 import at.orchaldir.gm.core.model.character.EquippedEquipment
+import at.orchaldir.gm.core.model.character.EquippedUniform
+import at.orchaldir.gm.core.model.item.Uniform
 import at.orchaldir.gm.core.model.item.equipment.*
 import at.orchaldir.gm.core.model.util.render.ColorScheme
 import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class EquippedTest {
-
-    private val equipmentMap = EquipmentMap
-        .fromId(EQUIPMENT_ID_0, COLOR_SCHEME_ID_0, BodySlot.Head)
-    private val equippedEquipment = EquippedEquipment(equipmentMap)
     val state = State(
         listOf(
-            Storage(listOf(Character(CHARACTER_ID_0))),
-            Storage(listOf(ColorScheme(COLOR_SCHEME_ID_0))),
-            Storage(listOf(Equipment(EQUIPMENT_ID_0, data = Hat()))),
+            Storage(Character(CHARACTER_ID_0)),
+            Storage(ColorScheme(COLOR_SCHEME_ID_0)),
+            Storage(Equipment(EQUIPMENT_ID_0, data = Hat())),
+            Storage(Uniform(UNIFORM_ID_0)),
         )
     )
 
-    @Test
-    fun `Update equipment`() {
-        validateEquipped(state, equippedEquipment)
-    }
+    @Nested
+    inner class EquippedEquipmentTest {
 
-    @Test
-    fun `Cannot use unknown equipment`() {
-        val state = state.removeStorage(EQUIPMENT_ID_0)
+        private val equipmentMap = EquipmentMap
+            .fromId(EQUIPMENT_ID_0, COLOR_SCHEME_ID_0, BodySlot.Head)
+        private val equipped = EquippedEquipment(equipmentMap)
 
-        assertIllegalArgument("Requires unknown Equipment 0!") { validateEquipped(state, equippedEquipment) }
-    }
+        @Test
+        fun `Update equipment`() {
+            validateEquipped(state, equipped)
+        }
 
-    @Test
-    fun `Cannot use unknown color scheme`() {
-        val state = state.removeStorage(COLOR_SCHEME_ID_0)
+        @Test
+        fun `Cannot use unknown equipment`() {
+            val state = state.removeStorage(EQUIPMENT_ID_0)
 
-        assertIllegalArgument("Requires unknown Color Scheme 0!") { validateEquipped(state, equippedEquipment) }
-    }
+            assertIllegalArgument("Requires unknown Equipment 0!") { validateEquipped(state, equipped) }
+        }
 
-    @Test
-    fun `Cannot use equipment with wrong slots`() {
-        val state = state.updateStorage(Storage(listOf(Equipment(EQUIPMENT_ID_0, data = Dress()))))
+        @Test
+        fun `Cannot use unknown color scheme`() {
+            val state = state.removeStorage(COLOR_SCHEME_ID_0)
 
-        assertIllegalArgument("Equipment 0 uses wrong slots!") { validateEquipped(state, equippedEquipment) }
-    }
+            assertIllegalArgument("Requires unknown Color Scheme 0!") { validateEquipped(state, equipped) }
+        }
 
-    @Test
-    fun `Cannot occupy equipment slot twice`() {
-        val state = state.updateStorage(
-            Storage(
-                listOf(
-                    Equipment(EQUIPMENT_ID_0, data = Dress()),
-                    Equipment(EQUIPMENT_ID_1, data = Shirt())
+        @Test
+        fun `Cannot use equipment with wrong slots`() {
+            val state = state.updateStorage(Storage(listOf(Equipment(EQUIPMENT_ID_0, data = Dress()))))
+
+            assertIllegalArgument("Equipment 0 uses wrong slots!") { validateEquipped(state, equipped) }
+        }
+
+        @Test
+        fun `Cannot occupy equipment slot twice`() {
+            val state = state.updateStorage(
+                Storage(
+                    listOf(
+                        Equipment(EQUIPMENT_ID_0, data = Dress()),
+                        Equipment(EQUIPMENT_ID_1, data = Shirt())
+                    )
                 )
             )
-        )
-        val equipmentMap = EquipmentMap.fromSlotAsValueMap<EquipmentIdPair>(
-            mapOf(
-                EQUIPMENT_ID_0 to setOf(setOf(BodySlot.Bottom, BodySlot.InnerTop)),
-                EQUIPMENT_ID_1 to setOf(setOf(BodySlot.InnerTop)),
+            val equipmentMap = EquipmentMap.fromSlotAsValueMap<EquipmentIdPair>(
+                mapOf(
+                    EQUIPMENT_ID_0 to setOf(setOf(BodySlot.Bottom, BodySlot.InnerTop)),
+                    EQUIPMENT_ID_1 to setOf(setOf(BodySlot.InnerTop)),
+                )
+                    .mapKeys { Pair(it.key, COLOR_SCHEME_ID_0) }
             )
-                .mapKeys { Pair(it.key, COLOR_SCHEME_ID_0) }
-        )
 
-        assertIllegalArgument("Body slot InnerTop is occupied multiple times!") { validateEquipped(state, EquippedEquipment(equipmentMap)) }
+            assertIllegalArgument("Body slot InnerTop is occupied multiple times!") {
+                validateEquipped(
+                    state,
+                    EquippedEquipment(equipmentMap)
+                )
+            }
+        }
+    }
+
+    @Nested
+    inner class EquippedUniformTest {
+
+        private val equipped = EquippedUniform(UNIFORM_ID_0)
+
+        @Test
+        fun `Update equipment`() {
+            validateEquipped(state, equipped)
+        }
+
+        @Test
+        fun `Cannot use unknown equipment`() {
+            val state = state.removeStorage(UNIFORM_ID_0)
+
+            assertIllegalArgument("Requires unknown Uniform 0!") { validateEquipped(state, equipped) }
+        }
     }
 }
