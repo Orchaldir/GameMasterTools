69f16f8890949552d82b104eeb16b167ec9810cf
Orchaldir <orchaldir@gmail.com>
2025-11-29 15:52:01 +0100

    Fix ProtectionTest.kt

--- src/test/kotlin/at/orchaldir/gm/core/model/rpg/combat/ResolveEquipmentStats.kt
+++ src/test/kotlin/at/orchaldir/gm/core/model/rpg/combat/ResolveEquipmentStats.kt
@@ -4,15 +4,15 @@ import at.orchaldir.gm.DAMAGE_TYPE_ID_0
 import at.orchaldir.gm.STATISTIC_ID_0
 import at.orchaldir.gm.core.model.rpg.SimpleModifiedDice
 import at.orchaldir.gm.core.selector.rpg.resolveMeleeAttack
-import at.orchaldir.gm.core.selector.rpg.resolveMeleeAttacks
+import at.orchaldir.gm.core.selector.rpg.resolveProtection
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class EquipmentModifierEffectTest {
+class ResolveEquipmentStats {
 
     @Nested
-    inner class ApplyEffectToMeleeAttackTest {
+    inner class MeleeAttackTest {
         val simple = SimpleRandomDamage(SimpleModifiedDice(1, 2))
         val simpleAttack = createAttack(simple)
         val based = StatisticBasedDamage(STATISTIC_ID_0, SimpleModifiedDice(1, 2))
@@ -52,6 +52,45 @@ class EquipmentModifierEffectTest {
         }
 
         fun createAttack(amount: DamageAmount) = MeleeAttack(Damage(amount, DAMAGE_TYPE_ID_0))
+
+    }
+
+    @Nested
+    inner class ProtectionTest {
+        val damageResistance = DamageResistance(4)
+        val damageResistances = DamageResistances(4, mapOf(DAMAGE_TYPE_ID_0 to 6))
+        val defenseBonus = DefenseBonus(7)
+
+        @Test
+        fun `Test Modify Damage`() {
+            val effect = ModifyDamage(SimpleModifiedDice(1, 2))
+
+            assertEquals(resolveProtection(effect, damageResistance), damageResistance)
+            assertEquals(resolveProtection(effect, damageResistances), damageResistances)
+            assertEquals(resolveProtection(effect, defenseBonus), defenseBonus)
+            assertEquals(resolveProtection(effect, UndefinedProtection), UndefinedProtection)
+        }
+
+        @Test
+        fun `Test Modify Damage Resistance`() {
+            val effect = ModifyDamageResistance(1)
+            val updated = DamageResistances(5, mapOf(DAMAGE_TYPE_ID_0 to 6))
+
+            assertEquals(resolveProtection(effect, damageResistance), DamageResistance(5))
+            assertEquals(resolveProtection(effect, damageResistances), updated)
+            assertEquals(resolveProtection(effect, defenseBonus), defenseBonus)
+            assertEquals(resolveProtection(effect, UndefinedProtection), UndefinedProtection)
+        }
+
+        @Test
+        fun `Test Modify Defense Bonus`() {
+            val effect = ModifyDefenseBonus(2)
+
+            assertEquals(resolveProtection(effect, damageResistance), damageResistance)
+            assertEquals(resolveProtection(effect, damageResistances), damageResistances)
+            assertEquals(resolveProtection(effect, defenseBonus), DefenseBonus(9))
+            assertEquals(resolveProtection(effect, UndefinedProtection), UndefinedProtection)
+        }
 
     }
 
