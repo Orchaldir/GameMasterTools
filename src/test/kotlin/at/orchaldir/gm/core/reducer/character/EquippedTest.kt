62a07178c3605e38d7f234921b87a635f771644d
Orchaldir <orchaldir@gmail.com>
2025-11-29 16:32:30 +0100

    Fix tests

--- src/test/kotlin/at/orchaldir/gm/core/reducer/character/EquippedTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/character/EquippedTest.kt
@@ -1,23 +1,19 @@
 package at.orchaldir.gm.core.reducer.character
 
 import at.orchaldir.gm.*
-import at.orchaldir.gm.core.action.UpdateAction
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.character.Character
 import at.orchaldir.gm.core.model.character.EquippedEquipment
 import at.orchaldir.gm.core.model.item.equipment.*
 import at.orchaldir.gm.core.model.util.render.ColorScheme
-import at.orchaldir.gm.core.reducer.REDUCER
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class EquippedTest {
 
     private val equipmentMap = EquipmentMap
         .fromId(EQUIPMENT_ID_0, COLOR_SCHEME_ID_0, BodySlot.Head)
     private val equippedEquipment = EquippedEquipment(equipmentMap)
-    private val action = UpdateAction(Character(CHARACTER_ID_0, equipped = equippedEquipment))
     val state = State(
         listOf(
             Storage(listOf(Character(CHARACTER_ID_0))),
@@ -28,37 +24,28 @@ class EquippedTest {
 
     @Test
     fun `Update equipment`() {
-        val result = REDUCER.invoke(state, action).first
-
-        assertEquals(equippedEquipment, result.getCharacterStorage().getOrThrow(CHARACTER_ID_0).equipped)
-    }
-
-    @Test
-    fun `Cannot update unknown character`() {
-        val state = state.removeStorage(CHARACTER_ID_0)
-
-        assertIllegalArgument("Requires unknown Character 0!") { REDUCER.invoke(state, action) }
+        validateEquipped(state, equippedEquipment)
     }
 
     @Test
     fun `Cannot use unknown equipment`() {
         val state = state.removeStorage(EQUIPMENT_ID_0)
 
-        assertIllegalArgument("Requires unknown Equipment 0!") { REDUCER.invoke(state, action) }
+        assertIllegalArgument("Requires unknown Equipment 0!") { validateEquipped(state, equippedEquipment) }
     }
 
     @Test
     fun `Cannot use unknown color scheme`() {
         val state = state.removeStorage(COLOR_SCHEME_ID_0)
 
-        assertIllegalArgument("Requires unknown Color Scheme 0!") { REDUCER.invoke(state, action) }
+        assertIllegalArgument("Requires unknown Color Scheme 0!") { validateEquipped(state, equippedEquipment) }
     }
 
     @Test
     fun `Cannot use equipment with wrong slots`() {
         val state = state.updateStorage(Storage(listOf(Equipment(EQUIPMENT_ID_0, data = Dress()))))
 
-        assertIllegalArgument("Equipment 0 uses wrong slots!") { REDUCER.invoke(state, action) }
+        assertIllegalArgument("Equipment 0 uses wrong slots!") { validateEquipped(state, equippedEquipment) }
     }
 
     @Test
@@ -78,8 +65,7 @@ class EquippedTest {
             )
                 .mapKeys { Pair(it.key, COLOR_SCHEME_ID_0) }
         )
-        val action = UpdateAction(Character(CHARACTER_ID_0, equipped = EquippedEquipment(equipmentMap)))
 
-        assertIllegalArgument("Body slot InnerTop is occupied multiple times!") { REDUCER.invoke(state, action) }
+        assertIllegalArgument("Body slot InnerTop is occupied multiple times!") { validateEquipped(state, EquippedEquipment(equipmentMap)) }
     }
 }
