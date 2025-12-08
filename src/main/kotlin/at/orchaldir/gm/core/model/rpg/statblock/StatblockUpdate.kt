3ea6afd25bc884b61b10a33c62230e1f046df6e4
Orchaldir <orchaldir@gmail.com>
2025-12-08 12:36:50 +0100

    Add StatblockUpdate

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/statblock/StatblockUpdate.kt
@@ -0,0 +1,27 @@
+package at.orchaldir.gm.core.model.rpg.statblock
+
+import at.orchaldir.gm.core.model.rpg.statistic.StatisticId
+import at.orchaldir.gm.core.model.rpg.trait.CharacterTraitId
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StatblockUpdate(
+    val statistics: Map<StatisticId, Int> = emptyMap(),
+    val addedTraits: Set<CharacterTraitId> = emptySet(),
+    val removedTraits: Set<CharacterTraitId> = emptySet(),
+) {
+
+    fun resolve(statblock: Statblock): Statblock {
+        val newStatistics = statblock.statistics.toMutableMap()
+
+        statistics.forEach { (id, modifier) ->
+            newStatistics[id] = newStatistics.getOrDefault(id, 0) + modifier
+        }
+
+        return Statblock(
+            newStatistics,
+            statblock.traits - removedTraits + addedTraits,
+        )
+    }
+
+}
