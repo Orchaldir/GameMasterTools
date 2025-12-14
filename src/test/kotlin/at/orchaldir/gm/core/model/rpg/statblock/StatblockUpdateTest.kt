34ae47b2baab8a0988bc16846ac626f1026e98d0
Orchaldir <orchaldir@gmail.com>
2025-12-14 19:49:47 +0100

    Add StatblockUpdateTest

---
+++ src/test/kotlin/at/orchaldir/gm/core/model/rpg/statblock/StatblockUpdateTest.kt
@@ -0,0 +1,80 @@
+package at.orchaldir.gm.core.model.rpg.statblock
+
+import at.orchaldir.gm.*
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.rpg.statistic.*
+import at.orchaldir.gm.core.model.rpg.trait.CharacterTrait
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class StatblockUpdateTest {
+
+    private val data0 = Attribute(FixedNumber(10), FixedStatisticCost(5))
+    private val data1 = Attribute(cost = FixedStatisticCost(10))
+    private val attribute0 = Statistic(STATISTIC_ID_0, data = data0)
+    private val attribute1 = Statistic(STATISTIC_ID_1, data = data1)
+    private val trait0 = CharacterTrait(CHARACTER_TRAIT_ID_0, cost = 15)
+    private val trait1 = CharacterTrait(CHARACTER_TRAIT_ID_1, cost = -5)
+    private val state = State(
+        listOf(
+            Storage(listOf(attribute0, attribute1)),
+            Storage(listOf(trait0, trait1)),
+        )
+    )
+
+    @Nested
+    inner class CalculateCostTest {
+
+        @Test
+        fun `Cost of unknown statistic`() {
+            val update = StatblockUpdate(mapOf(UNKNOWN_STATISTIC_ID to 5))
+
+            assertIllegalArgument("Requires unknown Statistic 99!") { update.calculateCost(state) }
+        }
+
+        @Test
+        fun `Cost of unknown added character trait`() {
+            val update = StatblockUpdate(addedTraits = setOf(UNKNOWN_CHARACTER_TRAIT_ID))
+
+            assertIllegalArgument("Requires unknown Character Trait 99!") { update.calculateCost(state) }
+        }
+
+        @Test
+        fun `Cost of unknown removed character trait`() {
+            val update = StatblockUpdate(addedTraits = setOf(UNKNOWN_CHARACTER_TRAIT_ID))
+
+            assertIllegalArgument("Requires unknown Character Trait 99!") { update.calculateCost(state) }
+        }
+
+        @Test
+        fun `Cost of empty update`() {
+            val update = StatblockUpdate()
+
+            assertEquals(0, update.calculateCost(state))
+        }
+
+        @Test
+        fun `Cost of multiple statistics`() {
+            val update = StatblockUpdate(mapOf(STATISTIC_ID_0 to 3, STATISTIC_ID_1 to 4))
+
+            assertEquals(55, update.calculateCost(state))
+        }
+
+        @Test
+        fun `Cost of multiple added traits`() {
+            val update = StatblockUpdate(addedTraits = setOf(CHARACTER_TRAIT_ID_0, CHARACTER_TRAIT_ID_1))
+
+            assertEquals(10, update.calculateCost(state))
+        }
+
+        @Test
+        fun `Cost of multiple removed traits`() {
+            val update = StatblockUpdate(removedTraits = setOf(CHARACTER_TRAIT_ID_0, CHARACTER_TRAIT_ID_1))
+
+            assertEquals(-10, update.calculateCost(state))
+        }
+    }
+
+}
