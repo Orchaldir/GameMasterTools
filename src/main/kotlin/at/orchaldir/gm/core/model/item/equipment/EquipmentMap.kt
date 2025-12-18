841e2200dffa9d7de8fa119e25bd1b4b9071ba5a
Orchaldir <orchaldir@gmail.com>
2025-12-18 19:29:05 +0100

    Split

--- src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentMap.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/EquipmentMap.kt
@@ -5,41 +5,6 @@ import at.orchaldir.gm.core.model.util.render.Colors
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class EquipmentEntry<T>(val data: T, val sets: Set<Set<BodySlot>>) {
-    constructor(value: T, slot: BodySlot) : this(value, setOf(setOf(slot)))
-
-    companion object {
-        fun <T> from(value: T, slots: Set<BodySlot>) = EquipmentEntry(value, setOf(slots))
-        fun <T> from(value: T, data: EquipmentData) = from(value, data.slots().getAllBodySlotCombinations().first())
-        fun fromId(equipment: EquipmentId, scheme: ColorSchemeId, slot: BodySlot) =
-            EquipmentEntry(EquipmentIdPair(equipment, scheme), slot)
-    }
-
-    fun <U> convert(function: (T) -> U): EquipmentEntry<U> = EquipmentEntry(
-        function(data),
-        sets,
-    )
-
-    fun getMaxIounStoneSlot(): BodySlot? {
-        var maxIndex = -1
-        var maxSlot: BodySlot? = null
-
-        sets.forEach { slots ->
-            slots.forEach { slot ->
-                val index = slot.getOptionalIounStoneIndex() ?: return@forEach
-
-                if (index > maxIndex) {
-                    maxIndex = index
-                    maxSlot = slot
-                }
-            }
-        }
-
-        return maxSlot
-    }
-}
-
-@Serializable
 data class EquipmentMap<T>(private val list: List<EquipmentEntry<T>>) {
 
     constructor() : this(emptyList())
