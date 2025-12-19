74f0a820a2dc2d99bca8c9b7aa0e462f780af7d7
Orchaldir <orchaldir@gmail.com>
2025-12-19 15:13:34 +0100

    Add WeightLookup

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/util/WeightLookup.kt
@@ -0,0 +1,34 @@
+package at.orchaldir.gm.core.model.util
+
+import at.orchaldir.gm.utils.math.unit.Weight
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class WeightLookupType {
+    Calculated,
+    Fixed,
+    Undefined,
+}
+
+@Serializable
+sealed class WeightLookup {
+    fun getType() = when (this) {
+        is CalculatedWeight -> WeightLookupType.Calculated
+        is FixedWeight -> WeightLookupType.Fixed
+        UndefinedWeight -> WeightLookupType.Undefined
+    }
+}
+
+@Serializable
+@SerialName("Calculated")
+data object CalculatedWeight : WeightLookup()
+
+@Serializable
+@SerialName("Fixed")
+data class FixedWeight(
+    val weight: Weight,
+) : WeightLookup()
+
+@Serializable
+@SerialName("Undefined")
+data object UndefinedWeight : WeightLookup()
