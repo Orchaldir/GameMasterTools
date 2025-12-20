c62b77759ed6c71f28920a3435d3e22d558d20b0
Orchaldir <orchaldir@gmail.com>
2025-12-20 14:48:10 +0100

    Test WeightPerMaterial

--- src/test/kotlin/at/orchaldir/gm/utils/math/unit/WeightPerMaterialTest.kt
+++ src/test/kotlin/at/orchaldir/gm/utils/math/unit/WeightPerMaterialTest.kt
@@ -1,6 +1,7 @@
 package at.orchaldir.gm.utils.math.unit
 
 import at.orchaldir.gm.MATERIAL_ID_0
+import at.orchaldir.gm.MATERIAL_ID_1
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -11,7 +12,26 @@ class WeightPerMaterialTest {
 
     @Test
     fun `Add new material`() {
-        assertEquals(weightA, WeightPerMaterial().add(MATERIAL_ID_0, weightA).get(MATERIAL_ID_0))
+        val wpm = WeightPerMaterial().add(MATERIAL_ID_0, weightA)
+
+        assertEquals(weightA, wpm.get(MATERIAL_ID_0))
+        assertNull(wpm.get(MATERIAL_ID_1))
+    }
+
+    @Test
+    fun `Add 2 materials`() {
+        val wpm = WeightPerMaterial().add(MATERIAL_ID_0, weightA).add(MATERIAL_ID_1, weightB)
+
+        assertEquals(weightA, wpm.get(MATERIAL_ID_0))
+        assertEquals(weightB, wpm.get(MATERIAL_ID_1))
+    }
+
+    @Test
+    fun `Add material again`() {
+        val wpm = WeightPerMaterial().add(MATERIAL_ID_0, weightA).add(MATERIAL_ID_0, weightB)
+
+        assertEquals(weightAB, wpm.get(MATERIAL_ID_0))
+        assertNull(wpm.get(MATERIAL_ID_1))
     }
 
 }
