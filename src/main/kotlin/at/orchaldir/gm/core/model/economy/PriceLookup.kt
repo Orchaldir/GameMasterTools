00cf69c34dbc15fa1a2e537713e5884829978d41
Orchaldir <orchaldir@gmail.com>
2025-12-22 15:55:02 +0100

    Add PriceLookup

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/economy/PriceLookup.kt
@@ -0,0 +1,29 @@
+package at.orchaldir.gm.core.model.economy
+
+import at.orchaldir.gm.core.model.economy.money.Price
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class PriceLookupType {
+    Calculated,
+    Fixed,
+}
+
+@Serializable
+sealed class PriceLookup {
+    fun getType() = when (this) {
+        is CalculatedPrice -> PriceLookupType.Calculated
+        is FixedPrice -> PriceLookupType.Fixed
+    }
+}
+
+@Serializable
+@SerialName("Calculated")
+data object CalculatedPrice : PriceLookup()
+
+@Serializable
+@SerialName("Fixed")
+data class FixedPrice(
+    val price: Price,
+) : PriceLookup()
+
