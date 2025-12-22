2e9ad2c9383e84616737a2ca6d9ad7d12bbc3b4b
Orchaldir <orchaldir@gmail.com>
2025-12-22 22:39:02 +0100

    Add getPrice()

--- src/main/kotlin/at/orchaldir/gm/utils/math/unit/VolumePerMaterial.kt
+++ src/main/kotlin/at/orchaldir/gm/utils/math/unit/VolumePerMaterial.kt
@@ -2,6 +2,7 @@ package at.orchaldir.gm.utils.math.unit
 
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.economy.material.MaterialId
+import at.orchaldir.gm.core.model.economy.money.Price
 
 class VolumePerMaterial(
     private val map: MutableMap<MaterialId, Volume> = mutableMapOf(),
@@ -22,6 +23,18 @@ class VolumePerMaterial(
     fun get(material: MaterialId) = map[material]
 
     fun getMap(): Map<MaterialId, Volume> = map
+
+    fun getPrice(state: State): Price {
+        var total = Price(0)
+
+        getWeightPerMaterial(state).forEach { (id, weight) ->
+            val material = state.getMaterialStorage().getOrThrow(id)
+
+            total += Price.fromWeight(weight, material.pricePerKilogram)
+        }
+
+        return total
+    }
 
     fun getWeightPerMaterial(state: State) = map.mapValues { (id, volume) ->
         val material = state.getMaterialStorage().getOrThrow(id)
