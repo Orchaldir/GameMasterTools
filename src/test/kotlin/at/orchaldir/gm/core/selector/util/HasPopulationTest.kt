0eb2d9a6200a915429177ebb51c7665c465a86b4
Orchaldir <orchaldir@gmail.com>
2025-12-24 19:42:04 +0100

    Add HasPopulationTest

--- src/test/kotlin/at/orchaldir/gm/core/selector/util/HasPopulationTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/util/HasPopulationTest.kt
@@ -3,7 +3,11 @@ package at.orchaldir.gm.core.selector.util
 import at.orchaldir.gm.*
 import at.orchaldir.gm.core.model.realm.Realm
 import at.orchaldir.gm.core.model.util.population.AbstractPopulation
+import at.orchaldir.gm.core.model.util.population.Population
+import at.orchaldir.gm.core.model.util.population.PopulationPerRace
+import at.orchaldir.gm.core.model.util.population.TotalPopulation
 import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.math.HALF
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -14,13 +18,26 @@ class HasPopulationTest {
     inner class GetPopulationsTest {
 
         @Test
-        fun `Test abstract population `() {
-            val population = AbstractPopulation(races = setOf(RACE_ID_0))
+        fun `Test abstract population`() {
+            assertGetPopulations(AbstractPopulation(races = setOf(RACE_ID_0)))
+        }
+
+        @Test
+        fun `Test population per race`() {
+            assertGetPopulations(PopulationPerRace(100, mapOf(RACE_ID_0 to HALF)))
+        }
+
+        @Test
+        fun `Test total population`() {
+            assertGetPopulations(TotalPopulation(100, setOf(RACE_ID_0)))
+        }
+
+        private fun assertGetPopulations(population: Population) {
             val realm0 = Realm(REALM_ID_0, population = population)
             val realm1 = Realm(REALM_ID_1)
             val storage = Storage(listOf(realm0, realm1))
 
-            assertEquals(listOf(realm0), getPopulations(storage,RACE_ID_0))
+            assertEquals(listOf(realm0), getPopulations(storage, RACE_ID_0))
         }
     }
 
