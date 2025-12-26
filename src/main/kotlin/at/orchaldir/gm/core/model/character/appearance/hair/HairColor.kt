7d81182706e9dd4392c46c851a3fa805a6e02c40
Orchaldir <orchaldir@gmail.com>
2025-12-26 10:37:44 +0100

    Add HairColor

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/character/appearance/hair/HairColor.kt
@@ -0,0 +1,14 @@
+package at.orchaldir.gm.core.model.character.appearance.hair
+
+enum class HairColor {
+    VeryLightBlond,
+    LightBlond,
+    Blond,
+    DarkBlond,
+    MediumBrown,
+    DarkBrown,
+    Black,
+    // Reddish-brown
+    Auburn,
+    Red,
+}
