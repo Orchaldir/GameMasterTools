54a4fb4d1e8e27df573b36103e0406fe2b044a11
Orchaldir <orchaldir@gmail.com>
2025-12-28 21:02:23 +0100

    Cannot delete a business template used by a business

--- src/main/kotlin/at/orchaldir/gm/core/selector/economy/BusinessTemplate.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/economy/BusinessTemplate.kt
@@ -2,7 +2,7 @@ package at.orchaldir.gm.core.selector.economy
 
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.economy.business.BusinessId
 import at.orchaldir.gm.core.model.economy.business.BusinessTemplateId
 
-fun State.canDeleteBusinessTemplate(business: BusinessTemplateId) = DeleteResult(business)
+fun State.canDeleteBusinessTemplate(template: BusinessTemplateId) = DeleteResult(template)
+    .addElements(getBusinesses(template))
---
+++ src/test/kotlin/at/orchaldir/gm/core/selector/economy/BusinessTemplateTest.kt
@@ -0,0 +1,49 @@
+package at.orchaldir.gm.core.selector.economy
+
+import at.orchaldir.gm.*
+import at.orchaldir.gm.core.model.DeleteResult
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.character.Character
+import at.orchaldir.gm.core.model.character.Employed
+import at.orchaldir.gm.core.model.character.EmploymentStatus
+import at.orchaldir.gm.core.model.character.Unemployed
+import at.orchaldir.gm.core.model.economy.business.Business
+import at.orchaldir.gm.core.model.economy.business.BusinessTemplate
+import at.orchaldir.gm.core.model.economy.job.JobId
+import at.orchaldir.gm.core.model.item.text.Text
+import at.orchaldir.gm.core.model.util.*
+import at.orchaldir.gm.core.model.world.building.Building
+import at.orchaldir.gm.utils.Id
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BusinessTemplateTest {
+
+    @Nested
+    inner class CanDeleteTest {
+        private val template = BusinessTemplate(BUSINESS_TEMPLATE_ID_0)
+        private val state = State(
+            listOf(
+                Storage(template),
+            )
+        )
+
+        @Test
+        fun `Cannot delete a business template used by a business`() {
+            val business = Business(BUSINESS_ID_0, templates = setOf(BUSINESS_TEMPLATE_ID_0))
+            val newState = state.updateStorage(Storage(business))
+
+            failCanDelete(newState, BUSINESS_ID_0)
+        }
+
+        private fun <ID : Id<ID>> failCanDelete(state: State, blockingId: ID) {
+            assertEquals(
+                DeleteResult(BUSINESS_TEMPLATE_ID_0).addId(blockingId),
+                state.canDeleteBusinessTemplate(BUSINESS_TEMPLATE_ID_0)
+            )
+        }
+    }
+
+}
