82c132c2b5705fd884a0f7516cccc81857f31727
Orchaldir <orchaldir@gmail.com>
2025-12-28 12:26:29 +0100

    Add UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/economy/BusinessTemplate.kt
@@ -0,0 +1,50 @@
+package at.orchaldir.gm.app.html.economy
+
+import at.orchaldir.gm.app.html.parseName
+import at.orchaldir.gm.app.html.parseSimpleOptionalInt
+import at.orchaldir.gm.app.html.selectName
+import at.orchaldir.gm.app.html.util.source.editDataSources
+import at.orchaldir.gm.app.html.util.source.parseDataSources
+import at.orchaldir.gm.app.html.util.source.showDataSources
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.economy.business.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import kotlinx.html.HtmlBlockTag
+
+// show
+
+fun HtmlBlockTag.showBusinessTemplate(
+    call: ApplicationCall,
+    state: State,
+    business: BusinessTemplate,
+) {
+    showDataSources(call, state, business.sources)
+}
+
+// edit
+
+fun HtmlBlockTag.editBusinessTemplate(
+    call: ApplicationCall,
+    state: State,
+    business: BusinessTemplate,
+) {
+    selectName(business.name)
+    editDataSources(state, business.sources)
+}
+
+// parse
+
+fun parseBusinessTemplateId(parameters: Parameters, param: String) = parseOptionalBusinessTemplateId(parameters, param) ?: BusinessTemplateId(0)
+fun parseOptionalBusinessTemplateId(parameters: Parameters, param: String) =
+    parseSimpleOptionalInt(parameters, param)?.let { BusinessTemplateId(it) }
+
+fun parseBusinessTemplate(
+    state: State,
+    parameters: Parameters,
+    id: BusinessTemplateId,
+) = BusinessTemplate(
+    id,
+    parseName(parameters),
+    parseDataSources(parameters),
+)
