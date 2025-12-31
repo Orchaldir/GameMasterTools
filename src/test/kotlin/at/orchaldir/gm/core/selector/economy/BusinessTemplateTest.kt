01296888239eb5ff2cb6f6628239b5ad78e304bf
Orchaldir <orchaldir@gmail.com>
2025-12-31 20:55:37 +0100

    Cannot delete a business template used by an economy

--- src/test/kotlin/at/orchaldir/gm/core/selector/economy/BusinessTemplateTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/selector/economy/BusinessTemplateTest.kt
@@ -2,10 +2,13 @@ package at.orchaldir.gm.core.selector.economy
 
 import at.orchaldir.gm.BUSINESS_ID_0
 import at.orchaldir.gm.BUSINESS_TEMPLATE_ID_0
+import at.orchaldir.gm.REALM_ID_0
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.economy.CommonBusinesses
 import at.orchaldir.gm.core.model.economy.business.Business
 import at.orchaldir.gm.core.model.economy.business.BusinessTemplate
+import at.orchaldir.gm.core.model.realm.Realm
 import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Nested
@@ -29,6 +32,15 @@ class BusinessTemplateTest {
             val newState = state.updateStorage(Storage(business))
 
             failCanDelete(newState, BUSINESS_ID_0)
+        }
+
+        @Test
+        fun `Cannot delete a business template used by an economy`() {
+            val economy = CommonBusinesses(setOf(BUSINESS_TEMPLATE_ID_0))
+            val realm = Realm(REALM_ID_0, economy = economy)
+            val newState = state.updateStorage(Storage(realm))
+
+            failCanDelete(newState, REALM_ID_0)
         }
 
         private fun <ID : Id<ID>> failCanDelete(state: State, blockingId: ID) {
