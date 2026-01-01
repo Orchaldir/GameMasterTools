e0ba7a6571f54a6c3aa9b8d3bde21d32749f27ed
Orchaldir <orchaldir@gmail.com>
2026-01-01 08:40:34 +0100

    Add LifeStageId

--- src/main/kotlin/at/orchaldir/gm/core/model/race/aging/LifeStage.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/race/aging/LifeStage.kt
@@ -3,9 +3,22 @@ package at.orchaldir.gm.core.model.race.aging
 import at.orchaldir.gm.core.model.character.appearance.hair.HairColor
 import at.orchaldir.gm.core.model.character.appearance.hair.NoHairColor
 import at.orchaldir.gm.core.model.util.name.Name
+import at.orchaldir.gm.utils.Id
 import at.orchaldir.gm.utils.math.FULL
 import at.orchaldir.gm.utils.math.Factor
 import kotlinx.serialization.Serializable
+
+const val LIFE_STAGE_TYPE = "Life Stage"
+
+@JvmInline
+@Serializable
+value class LifeStageId(val value: Int) : Id<LifeStageId> {
+
+    override fun next() = LifeStageId(value + 1)
+    override fun type() = LIFE_STAGE_TYPE
+    override fun value() = value
+
+}
 
 @Serializable
 data class LifeStage(
