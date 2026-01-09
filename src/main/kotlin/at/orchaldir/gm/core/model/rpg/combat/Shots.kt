3a326bd2d199f1323a5e7b985883ab4bc2ead962
Orchaldir <orchaldir@gmail.com>
2026-01-09 13:25:21 +0100

    Add Shots

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/combat/Shots.kt
@@ -0,0 +1,36 @@
+package at.orchaldir.gm.core.model.rpg.combat
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class ShotsType {
+    Thrown,
+    SingleShot,
+    Undefined,
+}
+
+@Serializable
+sealed class Shots {
+
+    fun getType() = when (this) {
+        is Thrown -> ShotsType.Thrown
+        is SingleShot -> ShotsType.SingleShot
+        is UndefinedShots -> ShotsType.Undefined
+    }
+}
+
+@Serializable
+@SerialName("Thrown")
+data class Thrown(
+    val rounds: Int = 1,
+) : Shots()
+
+@Serializable
+@SerialName("SingleShot")
+data class SingleShot(
+    val rounds: Int = 1,
+) : Shots()
+
+@Serializable
+@SerialName("Undefined")
+data object UndefinedShots : Shots()
