e9f63f6c40efb7036b98daa342c50cff4042aa07
Orchaldir <orchaldir@gmail.com>
2026-01-10 14:14:23 +0100

    Add Shots UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/rpg/combat/Shots.kt
@@ -0,0 +1,93 @@
+package at.orchaldir.gm.app.html.rpg.combat
+
+import at.orchaldir.gm.app.*
+import at.orchaldir.gm.app.html.*
+import at.orchaldir.gm.core.model.rpg.combat.*
+import at.orchaldir.gm.utils.doNothing
+import io.ktor.http.*
+import kotlinx.html.DETAILS
+import kotlinx.html.HtmlBlockTag
+
+// show
+
+fun HtmlBlockTag.fieldShots(
+    shots: Shots,
+) {
+    field("Shots") {
+        displayShots(shots, true)
+    }
+}
+
+fun HtmlBlockTag.displayShots(
+    shots: Shots,
+    showUndefined: Boolean = false,
+) {
+    when (shots) {
+        is SingleShot -> +"1(${shots.roundsOfReload})"
+        is Thrown -> +"T(${shots.roundsOfReload})"
+        UndefinedShots -> if(showUndefined) {
+            +"Undefined"
+        }
+    }
+}
+
+// edit
+
+fun HtmlBlockTag.editShots(
+    shots: Shots,
+    param: String,
+) {
+    val shotsParam = combine(param, SHOTS)
+
+    showDetails("Shots", true) {
+        selectValue(
+            "Type",
+            combine(shotsParam, TYPE),
+            ShotsType.entries,
+            shots.getType(),
+        )
+
+        when (shots) {
+
+            is SingleShot -> selectRoundsOfReload(shotsParam, shots.roundsOfReload)
+            is Thrown -> selectRoundsOfReload(shotsParam, shots.roundsOfReload)
+            UndefinedShots -> doNothing()
+        }
+    }
+}
+
+private fun DETAILS.selectRoundsOfReload(
+    param: String,
+    roundsOfReload: Int,
+) {
+    selectInt(
+        "Rounds Of Reload",
+        roundsOfReload,
+        1,
+        10,
+        1,
+        combine(param, NUMBER),
+    )
+}
+
+// parse
+
+fun parseShots(
+    parameters: Parameters,
+    param: String,
+): Shots {
+    val shotsParam = combine(param, SHOTS)
+
+    return when (parse(parameters, combine(shotsParam, TYPE), ShotsType.Undefined)) {
+        ShotsType.Thrown -> Thrown(
+            parseRoundsOfReload(parameters, shotsParam),
+        )
+        ShotsType.SingleShot -> SingleShot(
+            parseRoundsOfReload(parameters, shotsParam),
+        )
+        ShotsType.Undefined -> UndefinedShots
+    }
+}
+
+private fun parseRoundsOfReload(parameters: Parameters, shotsParam: String) =
+    parseInt(parameters, combine(shotsParam, NUMBER), 1)
