ed5c5fef7260f1e44aaa7e8ae1b204ce2a08fe1b
Orchaldir <orchaldir@gmail.com>
2026-01-17 16:17:57 +0100

    Add slings.svg

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/weapons/Slings.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/character/equipment/weapons/Slings.kt
@@ -5,39 +5,46 @@ import at.orchaldir.gm.core.model.economy.material.Material
 import at.orchaldir.gm.core.model.economy.material.MaterialId
 import at.orchaldir.gm.core.model.item.equipment.Bow
 import at.orchaldir.gm.core.model.item.equipment.EquipmentMap.Companion.from
+import at.orchaldir.gm.core.model.item.equipment.Sling
 import at.orchaldir.gm.core.model.item.equipment.style.*
-import at.orchaldir.gm.core.model.util.Size
+import at.orchaldir.gm.core.model.util.part.ColorSchemeItemPart
 import at.orchaldir.gm.core.model.util.part.FillLookupItemPart
 import at.orchaldir.gm.core.model.util.render.Color
+import at.orchaldir.gm.core.model.util.render.HorizontalStripesLookup
+import at.orchaldir.gm.core.model.util.render.SolidLookup
 import at.orchaldir.gm.prototypes.visualization.addNames
 import at.orchaldir.gm.prototypes.visualization.character.CHARACTER_CONFIG
 import at.orchaldir.gm.prototypes.visualization.character.renderCharacterTableWithoutColorScheme
 import at.orchaldir.gm.utils.Storage
 import at.orchaldir.gm.utils.math.FULL
+import at.orchaldir.gm.utils.math.HALF
+import at.orchaldir.gm.utils.math.QUARTER
+import at.orchaldir.gm.utils.math.THREE_QUARTER
 
 fun main() {
-    val grip0 = SimpleGrip(GripShape.Oval, FillLookupItemPart(Color.Red))
-    val grip1 = SimpleGrip(GripShape.Straight, FillLookupItemPart(Color.Green))
-    val grip2 = SimpleGrip(GripShape.Waisted, FillLookupItemPart(Color.Blue))
-    val grips = listOf(
-        Pair("No Grip", NoBowGrip),
-        Pair("Small Grip", SimpleBowGrip(Size.Small, grip0)),
-        Pair("Medium Grip", SimpleBowGrip(Size.Medium, grip1)),
-        Pair("Large Grip", SimpleBowGrip(Size.Large, grip2)),
+    val length = listOf(
+        Pair("Short", QUARTER),
+        Pair("Medium", HALF),
+        Pair("Long", THREE_QUARTER),
+    )
+    val cradles = listOf(
+        Pair("Blue", SolidLookup(Color.Blue)),
+        Pair("Green", SolidLookup(Color.Green)),
+        Pair("Stripped", HorizontalStripesLookup(Color.Red, Color.Yellow)),
     )
 
     renderCharacterTableWithoutColorScheme(
         State(Storage(Material(MaterialId(0), color = Color.Gray))),
-        "bows.svg",
+        "slings.svg",
         CHARACTER_CONFIG,
-        addNames(BowShape.entries),
-        grips,
-    ) { distance, grip, shape ->
-        val bow = Bow(
-            shape,
-            FULL,
-            grip,
+        cradles,
+        length,
+    ) { distance, length, cradle ->
+        val polearm = Sling(
+            length,
+            Rope(ColorSchemeItemPart(Color.SaddleBrown)),
+            FillLookupItemPart(MaterialId(0), cradle),
         )
-        Pair(createAppearance(distance), from(bow))
+        Pair(createAppearance(distance), from(polearm))
     }
 }
