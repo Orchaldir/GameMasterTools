cdeae2ef1950a0090fb460f71ca015ac81af3ed1
Orchaldir <orchaldir@gmail.com>
2026-01-23 09:09:36 +0100

    Add Ammunition UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/rpg/combat/AmmunitionType.kt
@@ -0,0 +1,63 @@
+package at.orchaldir.gm.app.html.rpg.combat
+
+import at.orchaldir.gm.app.html.*
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.rpg.combat.AmmunitionType
+import at.orchaldir.gm.core.model.rpg.combat.AmmunitionTypeId
+import at.orchaldir.gm.core.selector.item.equipment.getAmmunitions
+import io.ktor.http.*
+import io.ktor.server.application.*
+import kotlinx.html.HtmlBlockTag
+import kotlinx.html.h2
+
+// show
+
+fun HtmlBlockTag.showAmmunitionType(
+    call: ApplicationCall,
+    state: State,
+    type: AmmunitionType,
+) {
+    showUsages(call, state, type.id)
+}
+
+private fun HtmlBlockTag.showUsages(
+    call: ApplicationCall,
+    state: State,
+    type: AmmunitionTypeId,
+) {
+    val ammunitions = state.getAmmunitions(type)
+
+    if (ammunitions.isEmpty()) {
+        return
+    }
+
+    h2 { +"Usage" }
+
+    fieldElements(call, state, ammunitions)
+}
+
+// edit
+
+fun HtmlBlockTag.editAmmunitionType(
+    call: ApplicationCall,
+    state: State,
+    type: AmmunitionType,
+) {
+    selectName(type.name)
+}
+
+// parse
+
+fun parseAmmunitionTypeId(parameters: Parameters, param: String) = AmmunitionTypeId(parseInt(parameters, param))
+fun parseAmmunitionTypeId(value: String) = AmmunitionTypeId(value.toInt())
+fun parseOptionalAmmunitionTypeId(parameters: Parameters, param: String) =
+    parseSimpleOptionalInt(parameters, param)?.let { AmmunitionTypeId(it) }
+
+fun parseAmmunitionType(
+    state: State,
+    parameters: Parameters,
+    id: AmmunitionTypeId,
+) = AmmunitionType(
+    id,
+    parseName(parameters),
+)
--- src/main/kotlin/at/orchaldir/gm/core/selector/item/equipment/Ammunition.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/item/equipment/Ammunition.kt
@@ -3,5 +3,10 @@ package at.orchaldir.gm.core.selector.item.equipment
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.item.equipment.AmmunitionId
+import at.orchaldir.gm.core.model.rpg.combat.AmmunitionTypeId
 
 fun State.canDeleteAmmunition(type: AmmunitionId) = DeleteResult(type)
+
+fun State.getAmmunitions(type: AmmunitionTypeId) = getAmmunitionStorage()
+    .getAll()
+    .filter { it.type == type }
