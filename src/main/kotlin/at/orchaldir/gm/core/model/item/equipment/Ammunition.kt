c99ff3f670d25b46be2e82dd9caf5c6ee3a9e6c9
Orchaldir <orchaldir@gmail.com>
2026-01-23 09:04:14 +0100

    Improve Ammunition

--- src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/Ammunition.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/item/equipment/Ammunition.kt
@@ -1,11 +1,15 @@
 package at.orchaldir.gm.core.model.item.equipment
 
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.economy.money.CalculatedPrice
+import at.orchaldir.gm.core.model.economy.money.PriceLookup
 import at.orchaldir.gm.core.model.rpg.combat.AmmunitionTypeId
 import at.orchaldir.gm.core.model.rpg.combat.EquipmentModifierId
 import at.orchaldir.gm.core.model.util.name.ElementWithSimpleName
 import at.orchaldir.gm.core.model.util.name.Name
 import at.orchaldir.gm.utils.Id
+import at.orchaldir.gm.utils.math.unit.CalculatedWeight
+import at.orchaldir.gm.utils.math.unit.WeightLookup
 import kotlinx.serialization.Serializable
 
 const val AMMUNITION_TYPE = "Ammunition"
@@ -26,6 +30,8 @@ data class Ammunition(
     val name: Name = Name.init(id),
     val type: AmmunitionTypeId = AmmunitionTypeId(0),
     val modifiers: Set<EquipmentModifierId> = emptySet(),
+    val weight: WeightLookup = CalculatedWeight,
+    val price: PriceLookup = CalculatedPrice,
 ) : ElementWithSimpleName<AmmunitionId> {
 
     override fun id() = id
