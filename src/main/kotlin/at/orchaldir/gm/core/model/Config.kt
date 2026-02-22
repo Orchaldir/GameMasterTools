c15c3004fe459c206e36f88fda46fb18dab49509
Orchaldir <orchaldir@gmail.com>
2026-02-22 14:12:30 +0100

    Delete RealmConfig

--- src/main/kotlin/at/orchaldir/gm/core/model/Config.kt
+++ src/main/kotlin/at/orchaldir/gm/core/model/Config.kt
@@ -1,7 +1,6 @@
 package at.orchaldir.gm.core.model
 
 import at.orchaldir.gm.core.model.economy.EconomyConfig
-import at.orchaldir.gm.core.model.realm.RealmConfig
 import at.orchaldir.gm.core.model.rpg.RpgConfig
 import at.orchaldir.gm.core.model.time.Time
 import at.orchaldir.gm.utils.math.unit.AreaUnit
@@ -10,7 +9,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Config(
     val economy: EconomyConfig = EconomyConfig(),
-    val realm: RealmConfig = RealmConfig(),
     val rpg: RpgConfig = RpgConfig(),
     val time: Time = Time(),
     val largeAreaUnit: AreaUnit = AreaUnit.Hectare,
