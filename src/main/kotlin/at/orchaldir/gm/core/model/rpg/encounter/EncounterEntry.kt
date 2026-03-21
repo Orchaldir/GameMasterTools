fa3f6f304a638d9e6faaf1764230ade7f4ba100a
Orchaldir <orchaldir@gmail.com>
2026-03-21 14:13:51 +0100

    Improve EncounterEntry

---
+++ src/main/kotlin/at/orchaldir/gm/core/model/rpg/encounter/EncounterEntry.kt
@@ -0,0 +1,48 @@
+package at.orchaldir.gm.core.model.rpg.encounter
+
+import at.orchaldir.gm.core.model.character.CharacterTemplateId
+import at.orchaldir.gm.core.model.rpg.dice.RandomNumber
+import at.orchaldir.gm.core.model.util.Lookup
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+enum class EncounterEntryType {
+    None,
+    CharacterTemplate,
+    Combined,
+    Table,
+}
+
+@Serializable
+sealed class EncounterEntry {
+
+    fun getType() = when (this) {
+        NoEncounter -> EncounterEntryType.None
+        is CharacterTemplateEncounter -> EncounterEntryType.CharacterTemplate
+        is CombinedEncounter -> EncounterEntryType.Combined
+        is EncounterTable -> EncounterEntryType.Table
+    }
+}
+
+@Serializable
+@SerialName("None")
+data object NoEncounter : EncounterEntry()
+
+@Serializable
+@SerialName("Template")
+data class CharacterTemplateEncounter(
+    val amount: RandomNumber,
+    val template: CharacterTemplateId,
+) : EncounterEntry()
+
+@Serializable
+@SerialName("Combined")
+data class CombinedEncounter(
+    val list: List<EncounterEntry>,
+) : EncounterEntry()
+
+@Serializable
+@SerialName("Table")
+data class EncounterTable(
+    val table: Lookup<EncounterEntry>,
+) : EncounterEntry()
