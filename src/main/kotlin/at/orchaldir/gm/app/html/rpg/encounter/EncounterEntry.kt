863d01fb2efc2c854d64b86104e7d16c86b9838c
Orchaldir <orchaldir@gmail.com>
2026-03-21 18:31:29 +0100

    Improve UI

--- src/main/kotlin/at/orchaldir/gm/app/html/rpg/encounter/EncounterEntry.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/rpg/encounter/EncounterEntry.kt
@@ -1,10 +1,6 @@
 package at.orchaldir.gm.app.html.rpg.encounter
 
-import at.orchaldir.gm.app.LIST
-import at.orchaldir.gm.app.LOOKUP
-import at.orchaldir.gm.app.NUMBER
-import at.orchaldir.gm.app.TEMPLATE
-import at.orchaldir.gm.app.TYPE
+import at.orchaldir.gm.app.*
 import at.orchaldir.gm.app.html.*
 import at.orchaldir.gm.app.html.character.parseCharacterTemplateId
 import at.orchaldir.gm.app.html.rpg.dice.editRandomNumber
@@ -16,12 +12,7 @@ import at.orchaldir.gm.app.html.util.showLookupDetails
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.rpg.IntRange
 import at.orchaldir.gm.core.model.rpg.dice.ModifiedDiceRange
-import at.orchaldir.gm.core.model.rpg.encounter.CharacterTemplateEncounter
-import at.orchaldir.gm.core.model.rpg.encounter.CombinedEncounter
-import at.orchaldir.gm.core.model.rpg.encounter.EncounterEntry
-import at.orchaldir.gm.core.model.rpg.encounter.EncounterEntryType
-import at.orchaldir.gm.core.model.rpg.encounter.EncounterTable
-import at.orchaldir.gm.core.model.rpg.encounter.NoEncounter
+import at.orchaldir.gm.core.model.rpg.encounter.*
 import at.orchaldir.gm.core.selector.util.sortCharacterTemplates
 import at.orchaldir.gm.utils.doNothing
 import io.ktor.http.*
@@ -40,9 +31,11 @@ fun HtmlBlockTag.showEncounterEntryDetails(
         is CharacterTemplateEncounter -> field("Encounter") {
             showCharacterTemplateEncounter(call, state, encounter)
         }
+
         is CombinedEncounter -> fieldList("Encounter", encounter.list) { entry ->
             showEncounterEntryInternal(call, state, entry)
         }
+
         is EncounterTable -> showLookupDetails(encounter.table, "Encounter") { entry ->
             showEncounterEntryInternal(call, state, entry)
         }
@@ -57,11 +50,12 @@ private fun HtmlBlockTag.showEncounterEntryInternal(
     when (encounter) {
         NoEncounter -> +"None"
         is CharacterTemplateEncounter -> showCharacterTemplateEncounter(call, state, encounter)
-        is CombinedEncounter -> showList( encounter.list) { entry ->
-            showEncounterEntryDetails(call, state, entry)
+        is CombinedEncounter -> showList(encounter.list) { entry ->
+            showEncounterEntryInternal(call, state, entry)
         }
+
         is EncounterTable -> showLookup(encounter.table) { entry ->
-            showEncounterEntryDetails(call, state, entry)
+            showEncounterEntryInternal(call, state, entry)
         }
     }
 }
@@ -83,53 +77,62 @@ fun HtmlBlockTag.editEncounterEntry(
     state: State,
     encounter: EncounterEntry,
     param: String,
+) = showDetails("Encounter", true) {
+    editEncounterEntryIntern(call, state, encounter, param)
+}
+
+fun HtmlBlockTag.editEncounterEntryIntern(
+    call: ApplicationCall,
+    state: State,
+    encounter: EncounterEntry,
+    param: String,
 ) {
     val range = ModifiedDiceRange(IntRange(0, 10), IntRange(0, 10))
-    showDetails("Encounter", true) {
-        selectValue(
-            "Type",
-            combine(param, TYPE),
-            EncounterEntryType.entries,
-            encounter.getType(),
-        )
 
-        when (encounter) {
-            NoEncounter -> doNothing()
-            is CharacterTemplateEncounter -> {
-                editRandomNumber(
-                    range,
-                    encounter.amount,
-                    combine(param, NUMBER),
-                    "Amount",
-                )
-                selectElement(
-                    state,
-                    combine(param, TEMPLATE),
-                    state.sortCharacterTemplates(),
-                    encounter.template,
-                )
-            }
-            is CombinedEncounter -> editList(
-                "Encounter",
-                combine(param, LIST),
-                encounter.list,
-                2,
-                100,
-            ) { _, entryParam, entry ->
-                editEncounterEntry(call, state, entry, entryParam)
-            }
-            is EncounterTable -> editLookupTable(
-                combine(param, LOOKUP),
-                encounter.table,
-                2,
-                100,
-                listOf(
-                    Pair("Encounter") { entryParam, entry ->
-                        editEncounterEntry(call, state, entry, entryParam)
-                    },
-                ),
+    selectValue(
+        "Type",
+        combine(param, TYPE),
+        EncounterEntryType.entries,
+        encounter.getType(),
+    )
+    when (encounter) {
+        NoEncounter -> doNothing()
+        is CharacterTemplateEncounter -> {
+            editRandomNumber(
+                range,
+                encounter.amount,
+                combine(param, NUMBER),
+                "Amount",
+            )
+            selectElement(
+                state,
+                combine(param, TEMPLATE),
+                state.sortCharacterTemplates(),
+                encounter.template,
             )
         }
+
+        is CombinedEncounter -> editList(
+            "Encounter",
+            combine(param, LIST),
+            encounter.list,
+            2,
+            100,
+        ) { _, entryParam, entry ->
+            editEncounterEntryIntern(call, state, entry, entryParam)
+        }
+
+        is EncounterTable -> editLookupTable(
+            combine(param, LOOKUP),
+            encounter.table,
+            2,
+            100,
+            listOf(
+                Pair("Encounter") { entryParam, entry ->
+                    editEncounterEntryIntern(call, state, entry, entryParam)
+                },
+            ),
+        )
     }
 }
 
@@ -145,11 +148,13 @@ fun parseEncounterEntry(
         parseRandomNumber(parameters, combine(param, NUMBER)),
         parseCharacterTemplateId(parameters, combine(param, TEMPLATE)),
     )
+
     EncounterEntryType.Combined -> CombinedEncounter(
         parseList(parameters, combine(param, LIST), 2) { _, entryParam ->
             parseEncounterEntry(parameters, entryParam)
         }
     )
+
     EncounterEntryType.Table -> EncounterTable(
         parseLookup(parameters, combine(param, LOOKUP), 1) { entryParam ->
             parseEncounterEntry(parameters, entryParam)
