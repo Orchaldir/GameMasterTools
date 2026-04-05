c30bb252b60cdfcbc1afe3722bdff833796d6ce5
Orchaldir <orchaldir@gmail.com>
2026-04-05 10:56:58 +0200

    Add Plant UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/ecology/plant/Plant.kt
@@ -0,0 +1,60 @@
+package at.orchaldir.gm.app.html.ecology.plant
+
+import at.orchaldir.gm.app.DATE
+import at.orchaldir.gm.app.html.parseInt
+import at.orchaldir.gm.app.html.parseName
+import at.orchaldir.gm.app.html.selectName
+import at.orchaldir.gm.app.html.util.*
+import at.orchaldir.gm.app.html.util.source.editDataSources
+import at.orchaldir.gm.app.html.util.source.parseDataSources
+import at.orchaldir.gm.app.html.util.source.showDataSources
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.ecology.plant.Plant
+import at.orchaldir.gm.core.model.ecology.plant.PlantId
+import at.orchaldir.gm.core.model.magic.ALLOWED_SPELL_ORIGINS
+import io.ktor.http.*
+import io.ktor.server.application.*
+import kotlinx.html.HtmlBlockTag
+
+// show
+
+fun HtmlBlockTag.showPlant(
+    call: ApplicationCall,
+    state: State,
+    plant: Plant,
+) {
+    optionalField(call, state, "Date", plant.date)
+    fieldOrigin(call, state, plant.origin, ::PlantId)
+    showDataSources(call, state, plant.sources)
+}
+
+// edit
+
+fun HtmlBlockTag.editPlant(
+    call: ApplicationCall,
+    state: State,
+    plant: Plant,
+) {
+    selectName(plant.name)
+    selectOptionalDate(state, "Date", plant.date, DATE)
+    editOrigin(state, plant.id, plant.origin, plant.date, ALLOWED_SPELL_ORIGINS, ::PlantId)
+    editDataSources(state, plant.sources)
+}
+
+// parse
+
+fun parsePlantId(parameters: Parameters, param: String) = PlantId(parseInt(parameters, param))
+
+fun parsePlantId(value: String) = PlantId(value.toInt())
+
+fun parsePlant(
+    state: State,
+    parameters: Parameters,
+    id: PlantId,
+) = Plant(
+    id,
+    parseName(parameters),
+    parseOptionalDate(parameters, state, DATE),
+    parseOrigin(parameters),
+    parseDataSources(parameters),
+)
