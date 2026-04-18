2bab017dbc37eca52b21fd544bca6e38394fd69a
Orchaldir <orchaldir@gmail.com>
2026-04-18 20:11:50 +0200

    Add Ecology.validate()

--- src/test/kotlin/at/orchaldir/gm/core/reducer/ecology/EcologyTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/ecology/EcologyTest.kt
@@ -5,8 +5,10 @@ import at.orchaldir.gm.UNKNOWN_PLANT_ID
 import at.orchaldir.gm.assertIllegalArgument
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.ecology.Ecology
+import at.orchaldir.gm.core.model.ecology.EcologyWithRarity
 import at.orchaldir.gm.core.model.ecology.EcologyWithSets
 import at.orchaldir.gm.core.model.ecology.plant.Plant
+import at.orchaldir.gm.core.model.util.SomeOf
 import at.orchaldir.gm.utils.Storage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -28,6 +30,24 @@ class EcologyTest {
         fun `Has unknown plant`() {
             assertEcology(
                 EcologyWithSets(setOf(UNKNOWN_PLANT_ID)),
+                "Requires unknown Plant 99!",
+            )
+        }
+
+    }
+
+    @Nested
+    inner class EcologyWithRarityTest {
+
+        @Test
+        fun `Ecology is valid`() {
+            EcologyWithRarity(SomeOf(PLANT_ID_0)).validate(state)
+        }
+
+        @Test
+        fun `Has unknown plant`() {
+            assertEcology(
+                EcologyWithRarity(SomeOf(UNKNOWN_PLANT_ID)),
                 "Requires unknown Plant 99!",
             )
         }
