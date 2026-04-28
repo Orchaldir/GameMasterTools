1ef0ef7147eea7dc41742a7bfa17717bcfbc0c3e
Orchaldir <orchaldir@gmail.com>
2026-04-28 21:35:47 +0200

    refactor

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/plant/Trunk.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/plant/Trunk.kt
@@ -1,27 +1,19 @@
 package at.orchaldir.gm.prototypes.visualization.plant
 
+import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.ecology.plant.Tree
-import at.orchaldir.gm.core.model.ecology.plant.appearance.LinearStemThickness
-import at.orchaldir.gm.core.model.ecology.plant.appearance.Stem
-import at.orchaldir.gm.core.model.ecology.plant.appearance.Trunk
+import at.orchaldir.gm.core.model.ecology.plant.appearance.*
 import at.orchaldir.gm.core.model.util.render.Color
+import at.orchaldir.gm.utils.math.FULL
 import at.orchaldir.gm.utils.math.Factor.Companion.fromPercentage
+import at.orchaldir.gm.utils.math.THREE_QUARTER
 import at.orchaldir.gm.utils.math.Variance
 import at.orchaldir.gm.utils.math.unit.Distance
 import at.orchaldir.gm.utils.math.unit.Distribution
-import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.ecology.plant.appearance.BaseSplitting
-import at.orchaldir.gm.core.model.ecology.plant.appearance.CurvedStem
-import at.orchaldir.gm.core.model.ecology.plant.appearance.NoStemSplitting
-import at.orchaldir.gm.core.model.ecology.plant.appearance.SegmentSplitting
-import at.orchaldir.gm.core.model.ecology.plant.appearance.StemShape
-import at.orchaldir.gm.core.model.ecology.plant.appearance.StemSplitting
-import at.orchaldir.gm.core.model.ecology.plant.appearance.StraightStem
-import at.orchaldir.gm.utils.math.FULL
-import at.orchaldir.gm.utils.math.THREE_QUARTER
 import at.orchaldir.gm.utils.math.unit.Orientation.Companion.fromDegrees
 
 fun main() {
+    val curvedStem = CurvedStem(Variance(fromDegrees(20), fromDegrees(20)))
     val baseSplitting = BaseSplitting(FULL, Variance(fromDegrees(30)))
     val segmentSplitting = SegmentSplitting(THREE_QUARTER, Variance(fromDegrees(30)))
 
@@ -29,23 +21,27 @@ fun main() {
         State(),
         "tree-trunk.svg",
         PLANT_CONFIG,
-        listOf(listOf(
-            createTree(StraightStem),
-            createTree(CurvedStem(Variance(fromDegrees(20), fromDegrees(20)))),
-            createTree(StraightStem, baseSplitting),
-            createTree(StraightStem, segmentSplitting),
-        )),
+        listOf(
+            listOf(
+                createTree(StraightStem),
+                createTree(curvedStem),
+                createTree(StraightStem, baseSplitting),
+                createTree(StraightStem, segmentSplitting),
+            )
+        ),
     )
 }
 
-fun createTree(shape: StemShape, splitting: StemSplitting = NoStemSplitting) = Tree(Trunk(
-    Distribution(Distance.fromMeters(1)),
-    Stem(
-        3,
-        shape,
-        LinearStemThickness(fromPercentage(5)),
-        splitting,
-    ),
-    Color.SaddleBrown,
-))
+fun createTree(shape: StemShape, splitting: StemSplitting = NoStemSplitting) = Tree(
+    Trunk(
+        Distribution(Distance.fromMeters(1)),
+        Stem(
+            3,
+            shape,
+            LinearStemThickness(fromPercentage(5)),
+            splitting,
+        ),
+        Color.SaddleBrown,
+    )
+)
 
