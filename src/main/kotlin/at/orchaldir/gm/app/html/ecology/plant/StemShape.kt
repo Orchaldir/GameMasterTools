7f71e923b488a1311b1613455c33125b8d7be15a
Orchaldir <orchaldir@gmail.com>
2026-04-30 22:10:19 +0200

    Add StemShape UI

---
+++ src/main/kotlin/at/orchaldir/gm/app/html/ecology/plant/StemShape.kt
@@ -0,0 +1,79 @@
+package at.orchaldir.gm.app.html.ecology.plant
+
+import at.orchaldir.gm.app.ORIENTATION
+import at.orchaldir.gm.app.SHAPE
+import at.orchaldir.gm.app.html.*
+import at.orchaldir.gm.app.html.util.math.fieldVariance
+import at.orchaldir.gm.app.html.util.math.parseOrientationVariance
+import at.orchaldir.gm.app.html.util.math.selectOrientationVariance
+import at.orchaldir.gm.core.model.ecology.plant.appearance.CurvedStem
+import at.orchaldir.gm.core.model.ecology.plant.appearance.StemShape
+import at.orchaldir.gm.core.model.ecology.plant.appearance.StemShapeType
+import at.orchaldir.gm.core.model.ecology.plant.appearance.StraightStem
+import at.orchaldir.gm.utils.doNothing
+import at.orchaldir.gm.utils.math.unit.QUARTER_CIRCLE
+import at.orchaldir.gm.utils.math.unit.ZERO_ORIENTATION
+import io.ktor.http.*
+import kotlinx.html.HtmlBlockTag
+
+// show
+
+fun HtmlBlockTag.showStemShape(
+    shape: StemShape,
+) {
+    showDetails("Stem Shape", true) {
+        field("Type", shape.getType())
+
+        when (shape) {
+            StraightStem -> doNothing()
+            is CurvedStem -> fieldVariance("Angle", shape.angle)
+        }
+    }
+}
+
+// edit
+
+fun HtmlBlockTag.editStemShape(
+    shape: StemShape,
+    param: String ,
+) {
+    val shapeParam = combine(param, SHAPE)
+
+    showDetails("Stem Shape", true) {
+        selectValue(
+            "Type",
+            shapeParam,
+            StemShapeType.entries,
+            shape.getType(),
+        )
+
+        when (shape) {
+            StraightStem -> doNothing()
+            is CurvedStem -> selectOrientationVariance(
+                "Angle Offset",
+                combine(shapeParam, ORIENTATION),
+                shape.angle,
+                -QUARTER_CIRCLE,
+                QUARTER_CIRCLE,
+                ZERO_ORIENTATION,
+                QUARTER_CIRCLE,
+            )
+        }
+    }
+}
+
+// parse
+
+fun parseStemShape(
+    parameters: Parameters,
+    param: String ,
+): StemShape {
+    val shapeParam = combine(param, SHAPE)
+
+    return when (parse(parameters, shapeParam, StemShapeType.Straight)) {
+        StemShapeType.Straight -> StraightStem
+        StemShapeType.Curved -> CurvedStem(
+            parseOrientationVariance(parameters, combine(shapeParam, ORIENTATION)),
+        )
+    }
+}
