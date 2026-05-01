632590c23823735eebcee985fd23c593a108a087
Orchaldir <orchaldir@gmail.com>
2026-05-01 16:50:59 +0200

    Test Trunk.validate()

--- src/test/kotlin/at/orchaldir/gm/core/reducer/ecology/plant/appearance/TrunkTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/ecology/plant/appearance/TrunkTest.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.core.reducer.ecology.plant.appearance
 import at.orchaldir.gm.*
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.ecology.plant.Tree
+import at.orchaldir.gm.core.model.ecology.plant.appearance.MAX_TRUNK_HEIGHT
 import at.orchaldir.gm.core.model.ecology.plant.appearance.MIN_TRUNK_HEIGHT
 import at.orchaldir.gm.core.model.ecology.plant.appearance.Trunk
 import at.orchaldir.gm.core.model.economy.material.Material
@@ -20,18 +21,36 @@ class TrunkTest {
         )
     )
 
-    @Test
-    fun `Cannot use too small height`() {
-        val tree = Trunk(Distribution(MIN_TRUNK_HEIGHT - Distance.fromMillimeters(1)))
+    @Nested
+    inner class TreeTest {
 
-        assertIllegalArgument("The trunk's height is too small!") { tree.validate(state) }
-    }
+        @Test
+        fun `Cannot use too small height`() {
+            val tree = Trunk(Distribution(MIN_TRUNK_HEIGHT - Distance.fromMillimeters(1)))
 
-    @Test
-    fun `Valid tree`() {
-        val tree = Trunk(Distribution(MIN_TRUNK_HEIGHT))
+            assertIllegalArgument("The trunk's height is too small!") { tree.validate(state) }
+        }
 
-        tree.validate(state)
+        @Test
+        fun `Can use min height`() {
+            val tree = Trunk(Distribution(MIN_TRUNK_HEIGHT))
+
+            tree.validate(state)
+        }
+
+        @Test
+        fun `Cannot use too large height`() {
+            val tree = Trunk(Distribution(MAX_TRUNK_HEIGHT + Distance.fromMillimeters(1)))
+
+            assertIllegalArgument("The trunk's height is too large!") { tree.validate(state) }
+        }
+
+        @Test
+        fun `Can use max height`() {
+            val tree = Trunk(Distribution(MAX_TRUNK_HEIGHT))
+
+            tree.validate(state)
+        }
     }
 
 }
