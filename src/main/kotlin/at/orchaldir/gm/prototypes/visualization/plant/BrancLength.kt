7f0443457d56a0de2987c68deeb5a475d4cc5716
Orchaldir <orchaldir@gmail.com>
2026-05-03 11:35:55 +0200

    Improve

--- src/main/kotlin/at/orchaldir/gm/prototypes/visualization/plant/BrancLength.kt
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/plant/BrancLength.kt
@@ -5,6 +5,7 @@ import at.orchaldir.gm.core.model.ecology.plant.Tree
 import at.orchaldir.gm.core.model.ecology.plant.appearance.*
 import at.orchaldir.gm.core.model.util.render.Color
 import at.orchaldir.gm.prototypes.visualization.addNames
+import at.orchaldir.gm.utils.RandomNumberGenerator
 import at.orchaldir.gm.utils.math.FULL
 import at.orchaldir.gm.utils.math.Factor.Companion.fromPercentage
 import at.orchaldir.gm.utils.math.HALF
@@ -14,28 +15,27 @@ import at.orchaldir.gm.utils.math.Variance
 import at.orchaldir.gm.utils.math.unit.Distance
 import at.orchaldir.gm.utils.math.unit.Distribution
 import at.orchaldir.gm.utils.math.unit.Orientation.Companion.fromDegrees
+import kotlin.random.Random
 
 fun main() {
     val curvedStem = CurvedStem(Variance(fromDegrees(20), fromDegrees(20)))
     val baseSplitting = BaseSplitting(FULL, Variance(fromDegrees(30)))
-    val segmentSplitting = SegmentSplitting(THREE_QUARTER, Variance(fromDegrees(30)))
 
     renderPlantTable(
         State(),
         "branch-length.svg",
         PLANT_CONFIG,
-        addNames(BranchLength.entries),
         listOf(
             Pair("Straight", Pair(StraightStem, NoStemSplitting)),
             Pair("Curved", Pair(curvedStem, NoStemSplitting)),
-            Pair("Base Splitting", Pair(StraightStem, baseSplitting)),
-            Pair("Segment Splitting", Pair(StraightStem, segmentSplitting)),
+            Pair("Splitting", Pair(StraightStem, baseSplitting)),
         ),
+        addNames(BranchLength.entries),
         ::createTree,
     )
 }
 
-private fun createTree(pair: Pair<StemShape,StemSplitting>, length: BranchLength) = Tree(
+private fun createTree(length: BranchLength, pair: Pair<StemShape,StemSplitting>) = Tree(
     Trunk(
         Distribution(Distance.fromMeters(1)),
         Stem(
