6ff5ad7e6ff2f42f9bc101876cd2732fb573c841
Orchaldir <orchaldir@gmail.com>
2026-05-29 12:44:08 +0200

    Add visualizeBrickRows()

--- src/main/kotlin/at/orchaldir/gm/visualization/grammar/SingleBrickGrammar.kt
+++ src/main/kotlin/at/orchaldir/gm/visualization/grammar/SingleBrickGrammar.kt
@@ -4,6 +4,7 @@ import at.orchaldir.gm.core.model.visualization.SingleBrickGrammar
 import at.orchaldir.gm.core.model.visualization.SingleBrickPattern
 import at.orchaldir.gm.utils.doNothing
 import at.orchaldir.gm.utils.math.AABB
+import at.orchaldir.gm.utils.math.Factor
 
 fun visualizeSingleBrickGrammar(
     state: GrammarRenderState,
@@ -15,8 +16,25 @@ fun visualizeSingleBrickGrammar(
     SingleBrickPattern.BasketWeaveDouble -> doNothing()
     SingleBrickPattern.Grid -> visualizeGrid(state, grammar, aabb, layer)
     SingleBrickPattern.Herringbone -> doNothing()
-    SingleBrickPattern.Running -> doNothing()
-    SingleBrickPattern.Stack -> doNothing()
+    SingleBrickPattern.Running -> visualizeBrickRows(
+        state,
+        grammar,
+        aabb,
+        layer,
+    ) { x, y ->
+        if (x == 0 && y % 2 == 0) {
+            1
+        } else {
+            2
+        }
+    }
+    SingleBrickPattern.Stack -> visualizeBrickRows(
+        state,
+        grammar,
+        aabb,
+        layer,
+        { _,_ -> 2 },
+    )
 }
 
 private fun visualizeGrid(
@@ -42,5 +60,37 @@ private fun visualizeGrid(
         }
 
         startOfRow = startOfRow.addHeight(brickSize.height)
+    }
+}
+
+private fun visualizeBrickRows(
+    state: GrammarRenderState,
+    grammar: SingleBrickGrammar,
+    aabb: AABB,
+    layer: Int,
+    calculateLength: (Int, Int) -> Int,
+) = grammar.size.process(aabb) { start, gridSize, blockSize ->
+    var startOfRow = start
+
+    repeat(gridSize.height) { y ->
+        var currentBrick = startOfRow
+        var x = 0
+
+        while(x < gridSize.width) {
+            val length = calculateLength(x, y)
+            val brickSize = blockSize.replaceWidth(Factor.fromNumber(length))
+
+            visualizeGrammar(
+                state,
+                grammar.brick,
+                AABB(currentBrick, brickSize),
+                layer,
+            )
+
+            currentBrick = currentBrick.addWidth(brickSize.width)
+            x += length
+        }
+
+        startOfRow = startOfRow.addHeight(blockSize.height)
     }
 }
