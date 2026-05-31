f01ec0e75039d3cd7dd4b6e9e98d06e98ee8094b
Orchaldir <orchaldir@gmail.com>
2026-05-31 10:25:42 +0200

    GridSizeTest

--- src/test/kotlin/at/orchaldir/gm/core/reducer/visualization/GridSizeTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/visualization/GridSizeTest.kt
@@ -1,22 +1,11 @@
 package at.orchaldir.gm.core.reducer.visualization
 
-import at.orchaldir.gm.MATERIAL_ID_0
-import at.orchaldir.gm.UNKNOWN_MATERIAL_ID
 import at.orchaldir.gm.assertIllegalArgument
 import at.orchaldir.gm.assertInt
-import at.orchaldir.gm.core.model.State
-import at.orchaldir.gm.core.model.economy.material.Material
-import at.orchaldir.gm.core.model.economy.material.MaterialProperties
-import at.orchaldir.gm.core.model.economy.material.Metal
-import at.orchaldir.gm.core.model.util.part.MadeFromMetal
-import at.orchaldir.gm.core.model.visualization.BrickPatternGrammar
 import at.orchaldir.gm.core.model.visualization.GridSize
-import at.orchaldir.gm.core.model.visualization.MAX_BRICK_LENGTH
-import at.orchaldir.gm.core.model.visualization.MIN_BRICK_LENGTH
-import at.orchaldir.gm.core.model.visualization.ShapeGrammar
-import at.orchaldir.gm.core.model.visualization.RectangularShapeGrammar
+import at.orchaldir.gm.core.model.visualization.RowsAndColumns
 import at.orchaldir.gm.core.model.visualization.SquareGrid
-import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.map.MapSize2d
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -25,24 +14,42 @@ private const val MAX = 4
 
 class GridSizeTest {
 
-    @Nested
-    inner class SquareGridTest {
-
         @Test
-        fun `Test the brick length`() {
-            assertInt(
-                "test's size",
-                MIN,
-                MAX,
-                { length, message ->
-                    fail(SquareGrid(length), message)
-                },
-                { length ->
-                    success(SquareGrid(length))
-                },
-            )
+        fun `Test the SquareGridbrick length`() {
+            test("test's size", ::SquareGrid)
         }
 
+    @Nested
+    inner class RowsAndColumnsTest {
+
+        @Test
+        fun `Test the width`() {
+            test("test's width") { size ->
+                RowsAndColumns(MapSize2d(size, MIN))
+            }
+        }
+
+        @Test
+        fun `Test the height`() {
+            test("test's height") { size ->
+                RowsAndColumns(MapSize2d(MIN, size))
+            }
+        }
+
+    }
+
+    private fun test(text: String, create: (Int) -> GridSize) {
+        assertInt(
+            text,
+            MIN,
+            MAX,
+            { size, message ->
+                fail(create(size), message)
+            },
+            { size ->
+                success(create(size))
+            },
+        )
     }
 
     fun fail(size: GridSize, message: String) {
