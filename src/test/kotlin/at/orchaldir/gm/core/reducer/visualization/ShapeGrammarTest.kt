44122b298897e0a36d0e6fe1f6624fcf386f124c
Orchaldir <orchaldir@gmail.com>
2026-06-05 08:04:09 +0200

    add tests

--- src/test/kotlin/at/orchaldir/gm/core/reducer/visualization/ShapeGrammarTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/visualization/ShapeGrammarTest.kt
@@ -2,6 +2,7 @@ package at.orchaldir.gm.core.reducer.visualization
 
 import at.orchaldir.gm.MATERIAL_ID_0
 import at.orchaldir.gm.UNKNOWN_MATERIAL_ID
+import at.orchaldir.gm.assertFactor
 import at.orchaldir.gm.assertIllegalArgument
 import at.orchaldir.gm.assertInt
 import at.orchaldir.gm.core.model.State
@@ -80,6 +81,25 @@ class ShapeGrammarTest {
             val grammar = RectangularShapeGrammar(MadeFromMetal(MATERIAL_ID_0))
 
             success(grammar)
+        }
+    }
+
+    @Nested
+    inner class ShrinkGrammarTest {
+
+        @Test
+        fun `Test the shrink factor`() {
+            assertFactor(
+                "test's shrink factor",
+                MIN_SHRINK_FACTOR,
+                MAX_SHRINK_FACTOR,
+                { factor, message ->
+                    fail(ShrinkGrammar(DoNothingShapeGrammar, factor), message)
+                },
+                { factor ->
+                    success(ShrinkGrammar(DoNothingShapeGrammar, factor))
+                },
+            )
         }
     }
 
