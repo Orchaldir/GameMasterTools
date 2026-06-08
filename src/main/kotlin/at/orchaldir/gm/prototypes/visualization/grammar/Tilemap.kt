7e2c4e3e455273f1718913b3bf6b3d087435b96a
Orchaldir <orchaldir@gmail.com>
2026-06-08 21:37:56 +0200

    Add grammar-tilemap.svg

---
+++ src/main/kotlin/at/orchaldir/gm/prototypes/visualization/grammar/Tilemap.kt
@@ -0,0 +1,44 @@
+package at.orchaldir.gm.prototypes.visualization.grammar
+
+import at.orchaldir.gm.core.model.util.part.MadeFromWood
+import at.orchaldir.gm.core.model.util.render.Color
+import at.orchaldir.gm.core.model.util.render.Color.Red
+import at.orchaldir.gm.core.model.visualization.BrickPattern
+import at.orchaldir.gm.core.model.visualization.BrickPatternGrammar
+import at.orchaldir.gm.core.model.visualization.RectangularShapeGrammar
+import at.orchaldir.gm.core.model.visualization.SquareGrid
+import at.orchaldir.gm.core.model.world.settlement.StreetTile
+import at.orchaldir.gm.utils.map.MapSize2d
+import at.orchaldir.gm.utils.map.TileMap2d
+import at.orchaldir.gm.utils.math.unit.Distance
+import at.orchaldir.gm.utils.math.unit.ZERO_DISTANCE
+import at.orchaldir.gm.utils.renderer.TileMap2dRenderer
+import at.orchaldir.gm.utils.renderer.svg.SvgBuilder
+import at.orchaldir.gm.visualization.grammar.Borders
+import at.orchaldir.gm.visualization.grammar.GrammarRenderState
+import at.orchaldir.gm.visualization.grammar.visualizeShapeGrammar
+import at.orchaldir.gm.core.model.State
+import java.io.File
+
+fun main() {
+    val grammar = BrickPatternGrammar(
+        RectangularShapeGrammar(
+            MadeFromWood(color = Color.Gray),
+        ),
+        SquareGrid(10),
+        BrickPattern.Herringbone,
+        3,
+    )
+    val tileMap = TileMap2d(MapSize2d(3, 2), grammar)
+    val tileMapRenderer = TileMap2dRenderer(Distance.fromMeters(1.0f), ZERO_DISTANCE)
+    val svgBuilder = SvgBuilder(tileMapRenderer.calculateMapSize(tileMap))
+
+    tileMapRenderer.render(tileMap) { index, x, y, aabb, grammar ->
+        val renderState = GrammarRenderState(State(), svgBuilder, LINE_OPTIONS)
+
+        visualizeShapeGrammar(renderState, grammar, aabb, Borders(false))
+    }
+
+    File("grammar-tilemap.svg").writeText(svgBuilder.finish().export())
+}
+
