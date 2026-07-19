92ab81a18566130e561a6ef74f9070e6ea9fd86f
Orchaldir <orchaldir@gmail.com>
2026-07-19 18:29:55 +0200

    Add PythagoreanTilingWithLength2Test

--- src/test/kotlin/at/orchaldir/gm/visualization/grammar/BrickPatternCreationTest.kt
+++ src/test/kotlin/at/orchaldir/gm/visualization/grammar/BrickPatternCreationTest.kt
@@ -55,6 +55,11 @@ class BrickPatternCreationTest {
     private val line_5_ExExSxH3xH3 = listOf(null, null, singleBrick, brickH3, brickH3)
     private val line_5_SxExExW2 = listOf(singleBrick, null, null, brickW2, null)
     private val line_5_SxH2xH2xW2 = listOf(singleBrick, brickH2, brickH2, brickW2, null)
+    private val line_5_Sx22xW2 = listOf(singleBrick, brick22, null, brickW2, null)
+    private val line_5_H2xExExSxH2 = listOf(brickH2, null, null, singleBrick, brickH2)
+    private val line_5_ExSx22xE = listOf(null, singleBrick, brick22, null, null)
+    private val line_5_22xExExS = listOf(brick22, null, null, null, singleBrick)
+    private val line_5_ExExSxW2 = listOf( null, null, singleBrick, brickW2, null)
     private val line_5_W2xExE = listOf(brickW2, null, null, null)
     private val line_5_W2xExExS = line_5_W2xExE + singleBrick
     private val line_5_W2xExExW2 = line_5_W2xExE + brickW2
@@ -792,6 +797,59 @@ class BrickPatternCreationTest {
 
         private fun test(size: MapSize2d, borders: Borders, expected: List<List<Brick?>>) =
             test(size, BrickPattern.PinwheelWithBigCenter, 3, borders, expected)
+    }
+
+    @Nested
+    inner class PythagoreanTilingWithLength2Test {
+
+        @Nested
+        inner class HorizontalAndVerticalRepetitionTest {
+
+            @Test
+            fun `Test origin tile`() {
+                testDefault(0, 0)
+            }
+
+            @Test
+            fun `Alternate Pattern 1 tile to the right`() {
+                testDefault(1, 0)
+            }
+
+            @Test
+            fun `Same Pattern 2 tiles to the right`() {
+                testDefault(2, 0)
+            }
+
+            @Test
+            fun `Same Pattern 1 tile to the bottom`() {
+                testDefault(0, 1)
+                testDefault(1, 1)
+                testDefault(2, 1)
+            }
+
+            @Test
+            fun `Same Pattern 2 tiles to the bottom`() {
+                testDefault(0, 2)
+                testDefault(1, 2)
+                testDefault(2, 2)
+            }
+
+            private fun testDefault(tileX: Int, tileY: Int) {
+                val expected = listOf(line_5_Sx22xW2, line_5_H2xExExSxH2, line_5_ExSx22xE, line_5_22xExExS, line_5_ExExSxW2)
+
+                test(tileX, tileY, expected)
+            }
+        }
+
+        private fun test(tileX: Int, tileY: Int, expected: List<List<Brick?>>) {
+            val size = MapSize2d(5, 5)
+            val borders = Borders(true, tileX, tileY)
+
+            test(size, borders, expected)
+        }
+
+        private fun test(size: MapSize2d, borders: Borders, expected: List<List<Brick?>>) =
+            test(size, BrickPattern.PythagoreanTiling, 2, borders, expected)
     }
 
     @Nested
