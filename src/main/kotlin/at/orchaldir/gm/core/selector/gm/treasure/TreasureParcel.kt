ef0279e28c4a73f2cc66973cabcdad52dfce0c84
Orchaldir <orchaldir@gmail.com>
2026-08-26 22:25:42 +0200

    Add showUsage()

--- src/main/kotlin/at/orchaldir/gm/core/selector/gm/treasure/TreasureParcel.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/gm/treasure/TreasureParcel.kt
@@ -3,11 +3,12 @@ package at.orchaldir.gm.core.selector.gm.treasure
 import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.gm.treasure.TreasureParcelId
+import at.orchaldir.gm.utils.Id
 
 fun State.canDeleteTreasureParcel(encounter: TreasureParcelId) = DeleteResult(encounter)
     .addElements(getTreasureParcelsWith(encounter))
 
-fun State.getTreasureParcelsWith(encounter: TreasureParcelId) = getTreasureParcelStorage()
+fun <ID : Id<ID>> State.getTreasureParcelsWith(id: ID) = getTreasureParcelStorage()
     .getAll()
-    .filter { it.entry.contains(encounter) }
+    .filter { it.entry.contains(id) }
 
