84f81664fcf7390fa3737625a8af78ae06abd134
Orchaldir <orchaldir@gmail.com>
2026-08-29 08:43:36 +0200

    Cannot be based on an unknown ammunition

--- src/test/kotlin/at/orchaldir/gm/core/reducer/gm/TreasureParcelTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/gm/TreasureParcelTest.kt
@@ -3,6 +3,7 @@ package at.orchaldir.gm.core.reducer.gm
 import at.orchaldir.gm.*
 import at.orchaldir.gm.core.action.UpdateAction
 import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.gm.treasure.AmmunitionParcel
 import at.orchaldir.gm.core.model.gm.treasure.EquipmentParcel
 import at.orchaldir.gm.core.model.gm.treasure.TreasureParcel
 import at.orchaldir.gm.core.model.item.equipment.Equipment
@@ -24,7 +25,16 @@ class TreasureParcelTest {
     inner class UpdateTest {
 
         @Test
-        fun `Cannot be based on a unknown equipment`() {
+        fun `Cannot be based on an unknown ammunition`() {
+            val entry = AmmunitionParcel(UNKNOWN_AMMUNITION)
+            val parcel = TreasureParcel(TREASURE_PARCEL_ID_0, entry = entry)
+            val action = UpdateAction(parcel)
+
+            assertIllegalArgument("Requires unknown Ammunition 99!") { REDUCER.invoke(stat, action) }
+        }
+
+        @Test
+        fun `Cannot be based on an unknown equipment`() {
             val entry = EquipmentParcel(UNKNOWN_EQUIPMENT_ID)
             val parcel = TreasureParcel(TREASURE_PARCEL_ID_0, entry = entry)
             val action = UpdateAction(parcel)
