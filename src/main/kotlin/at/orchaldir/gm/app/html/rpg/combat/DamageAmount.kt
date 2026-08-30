ea6346fbf7aca181103e7484f73f0f71e6d30af4
Orchaldir <orchaldir@gmail.com>
2026-08-30 12:26:31 +0200

    Add FencingParrying

--- src/main/kotlin/at/orchaldir/gm/app/html/rpg/combat/DamageAmount.kt
+++ src/main/kotlin/at/orchaldir/gm/app/html/rpg/combat/DamageAmount.kt
@@ -74,7 +74,7 @@ fun HtmlBlockTag.editDamageAmount(
 fun parseDamageAmount(
     parameters: Parameters,
     param: String,
-) = when (parse(parameters, combine(param, TYPE), DamageAmountType.SimpleRandom)) {
+) = when (parse(parameters, combine(param, TYPE), DamageAmountType.StatisticBased)) {
     DamageAmountType.StatisticBased -> StatisticBasedDamage(
         parseStatisticId(parameters, combine(param, BASE)),
         parseQuantity(parameters, param),
