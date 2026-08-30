c9f66ef67c83c3897973b6f8576d64c0e5550228
Orchaldir <orchaldir@gmail.com>
2026-08-30 19:42:18 +0200

    Cannot delete a group used by an equipment

--- src/main/kotlin/at/orchaldir/gm/core/selector/util/ColorSchemeGroup.kt
+++ src/main/kotlin/at/orchaldir/gm/core/selector/util/ColorSchemeGroup.kt
@@ -4,9 +4,11 @@ import at.orchaldir.gm.core.model.DeleteResult
 import at.orchaldir.gm.core.model.State
 import at.orchaldir.gm.core.model.util.render.ColorSchemeGroupId
 import at.orchaldir.gm.core.model.util.render.ColorSchemeId
+import at.orchaldir.gm.core.selector.item.equipment.getEquipment
 
 
 fun State.canDeleteColorSchemeGroup(id: ColorSchemeGroupId) = DeleteResult(id)
+    .addElements(getEquipment(id))
 
 fun State.getColorSchemeGroups(id: ColorSchemeId) = getColorSchemeGroupStorage()
     .getAll()
---
+++ src/test/kotlin/at/orchaldir/gm/core/selector/util/ColorSchemeGroupTest.kt
@@ -0,0 +1,50 @@
+package at.orchaldir.gm.core.selector.util
+
+import at.orchaldir.gm.CHARACTER_ID_0
+import at.orchaldir.gm.COLOR_SCHEME_GROUP_ID_0
+import at.orchaldir.gm.COLOR_SCHEME_ID_0
+import at.orchaldir.gm.EQUIPMENT_ID_0
+import at.orchaldir.gm.core.model.DeleteResult
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.character.Character
+import at.orchaldir.gm.core.model.character.UniqueEquipment
+import at.orchaldir.gm.core.model.item.equipment.BodySlot
+import at.orchaldir.gm.core.model.item.equipment.Equipment
+import at.orchaldir.gm.core.model.item.equipment.EquipmentMap
+import at.orchaldir.gm.core.model.util.render.ColorSchemeGroup
+import at.orchaldir.gm.core.model.util.render.UseColorSchemeGroup
+import at.orchaldir.gm.core.model.util.render.UseColorSchemes
+import at.orchaldir.gm.utils.Id
+import at.orchaldir.gm.utils.Storage
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ColorSchemeGroupTest {
+
+    @Nested
+    inner class CanDeleteTest {
+        private val group = ColorSchemeGroup(COLOR_SCHEME_GROUP_ID_0)
+        private val state = State(
+            listOf(
+                Storage(group),
+            )
+        )
+
+        @Test
+        fun `Cannot delete a group used by an equipment`() {
+            val equipment = Equipment(EQUIPMENT_ID_0, colorSchemes = UseColorSchemeGroup(COLOR_SCHEME_GROUP_ID_0))
+            val newState = state.updateStorage(equipment)
+
+            failCanDelete(newState, EQUIPMENT_ID_0)
+        }
+
+        private fun <ID : Id<ID>> failCanDelete(state: State, blockingId: ID) {
+            assertEquals(
+                DeleteResult(COLOR_SCHEME_GROUP_ID_0).addId(blockingId),
+                state.canDeleteColorSchemeGroup(COLOR_SCHEME_GROUP_ID_0)
+            )
+        }
+    }
+
+}
