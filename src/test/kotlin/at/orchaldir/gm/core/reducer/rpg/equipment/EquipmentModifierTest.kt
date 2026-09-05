bd778e8ddae1915a2c76a5a6c91fe529b0c8b204
Orchaldir <orchaldir@gmail.com>
2026-09-05 22:49:45 +0200

    Move

--- src/test/kotlin/at/orchaldir/gm/core/reducer/rpg/equipment/EquipmentModifierTest.kt
+++ src/test/kotlin/at/orchaldir/gm/core/reducer/rpg/equipment/EquipmentModifierTest.kt
@@ -1,4 +1,4 @@
-package at.orchaldir.gm.core.reducer.rpg
+package at.orchaldir.gm.core.reducer.rpg.equipment
 
 import at.orchaldir.gm.EQUIPMENT_MODIFIER_ID_0
 import at.orchaldir.gm.assertIllegalArgument
