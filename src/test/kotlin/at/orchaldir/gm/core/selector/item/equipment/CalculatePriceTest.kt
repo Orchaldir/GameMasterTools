18de2785be3ce10695c005edf5c0cdc39b4c2cad
Orchaldir <orchaldir@gmail.com>
2026-09-18 14:22:48 +0200

    Add CalculatePriceTest

---
+++ src/test/kotlin/at/orchaldir/gm/core/selector/item/equipment/CalculatePriceTest.kt
@@ -0,0 +1,49 @@
+package at.orchaldir.gm.core.selector.item.equipment
+
+import at.orchaldir.gm.EQUIPMENT_ID_0
+import at.orchaldir.gm.EQUIPMENT_MODIFIER_ID_0
+import at.orchaldir.gm.EQUIPMENT_MODIFIER_ID_1
+import at.orchaldir.gm.EQUIPMENT_TYPE_ID_0
+import at.orchaldir.gm.core.model.State
+import at.orchaldir.gm.core.model.economy.money.Price
+import at.orchaldir.gm.core.model.economy.money.PriceBasedOnType
+import at.orchaldir.gm.core.model.economy.money.UserDefinedPrice
+import at.orchaldir.gm.core.model.item.equipment.Equipment
+import at.orchaldir.gm.core.model.rpg.equipment.EquipmentModifier
+import at.orchaldir.gm.core.model.rpg.equipment.EquipmentStats
+import at.orchaldir.gm.core.model.rpg.equipment.EquipmentType
+import at.orchaldir.gm.utils.Storage
+import at.orchaldir.gm.utils.math.Factor
+import at.orchaldir.gm.utils.math.unit.Weight
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class CalculatePriceTest {
+
+    private val type = EquipmentType(
+        EQUIPMENT_TYPE_ID_0,
+        price = UserDefinedPrice(Price(100))
+    )
+    private val modifier0 = EquipmentModifier(
+        EQUIPMENT_MODIFIER_ID_0,
+        price = Factor.fromPercentage(20)
+    )
+    private val modifier1 = EquipmentModifier(
+        EQUIPMENT_MODIFIER_ID_1,
+        price = Factor.fromPercentage(30)
+    )
+    private val state = State(
+        listOf(
+            Storage(type),
+            Storage(listOf(modifier0, modifier1)),
+        )
+    )
+
+    @Test
+    fun `Calculate the price based on the type`() {
+        val stats = EquipmentStats(EQUIPMENT_TYPE_ID_0)
+        val equipment = Equipment(EQUIPMENT_ID_0, stats = stats, price = PriceBasedOnType)
+
+        assertEquals(Price(100), calculatePrice(state, VOLUME_CONFIG, equipment))
+    }
+}
